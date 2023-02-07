a525c29c46f2cc0974cb445294379ee50b32b0e0
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2023-02-07 00:21:12 +0000

    fix torchmetrics Accuracy

--- software/machop/session/plt_wrapper/vision.py
+++ software/machop/session/plt_wrapper/vision.py
@@ -10,11 +10,13 @@ class VisionModelWrapper(WrapperBase):
     def __init__(
             self,
             model,
+            info=None,
             learning_rate=5e-4,
             epochs=200,
             optimizer=None):
         super().__init__(
             model=model, 
+            info=info,
             learning_rate=learning_rate, 
             epochs=epochs, 
             optimizer=optimizer)
