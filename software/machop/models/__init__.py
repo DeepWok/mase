1e6deccb78bf7f9713b4bb4f7b82e69e7b3f42f6
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2023-02-13 16:48:59 -0800

    models and support of manually designed model

--- software/machop/models/__init__.py
+++ software/machop/models/__init__.py
@@ -3,6 +3,7 @@ from .resnet import (
     ResNet18ImageNet, ResNet50ImageNet)
 from .nlp_models import get_nlp_model
 from .toy import get_toynet
+from .manual.toy_manual import get_toymanualnet
 
 
 model_map = {
@@ -10,14 +11,20 @@ model_map = {
     'resnet50': ResNet50,
     'resnet18-imagenet': ResNet18ImageNet,
     'resnet50-imagenet': ResNet50ImageNet,
+    # this is a normal toynet written purely with pytorch ops
     'toy': get_toynet,
+    # this is a toynet with our custom ops
+    'toy_manual': get_toymanualnet,
     'roberta-base': get_nlp_model,
     'roberta-large': get_nlp_model,
 }
 
+# this is a list of models that are written purely with custom ops
+# this is necessary for cli to find an opportunity to pass the modify config...
+manual_models = ['toy_manual']
 
 vision_models = [
-    'toy',
+    'toy', 'toy_manual',
     'resnet18', 'resnet50', 
     'resnet18-imagenet', 'resnet50-imagenet']
 
