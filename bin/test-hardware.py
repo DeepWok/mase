6c97fbffce4ae31970250d3c306e5a99bb661e6e
Jianyi Cheng <jc9016@ic.ac.uk>
2023-03-08 22:01:42 +0000

    Fixed a bug that test-hardware could not run indvidual cases (#44)

--- bin/test-hardware.py
+++ bin/test-hardware.py
@@ -11,15 +11,15 @@ import maselogger
 # Add more test cases here
 test_cases = {
     # 'common/join2': [],
-    'common/int_mult': [],
-    'common/register_slice': [],
-    'common/adder_tree_layer': [],
-    'common/accumulator': [],
-    'common/adder_tree': ['common'],
-    'common/vector_mult': ['common'],
-    'common/dot_product': ['common'],
+    "common/int_mult": [],
+    "common/register_slice": [],
+    "common/adder_tree_layer": [],
+    "common/accumulator": [],
+    "common/adder_tree": ["common"],
+    "common/vector_mult": ["common"],
+    "common/dot_product": ["common"],
     # 'linear/dataflow_linear': ['common'],
-    'activations/int_relu': ['common'],
+    "activations/int_relu": ["common"],
     # 'activations/int_relu6': ['common'],
 }
 
@@ -34,9 +34,9 @@ class TestHardware:
         self.root = os.path.abspath(
             os.path.join(os.path.dirname(__file__), ".."))
         self.logger = maselogger.getLogger(
-            'test-hardware', os.path.join(self.root, 'test-hardware.log'))
-        self.test_cases = self.args
-        if self.args.run_all: 
+            "test-hardware", os.path.join(self.root, "test-hardware.log"))
+        self.test_cases = self.args.test_cases
+        if self.args.run_all:
             self.test_cases = test_cases.keys()
         check_fail = self.check_files()
         if check_fail:
@@ -48,39 +48,51 @@ class TestHardware:
             base_name = os.path.basename(test_case)
             # Check if the test bench exists
             if not os.path.exists(
-                    os.path.join(self.root, 'hardware', 'testbench', test_case,
-                                 '{}_tb.py'.format(base_name))):
+                    os.path.join(
+                        self.root,
+                        "hardware",
+                        "testbench",
+                        test_case,
+                        "{}_tb.py".format(base_name),
+                    )):
                 self.logger.error(
-                    'Cannot find the testbench! Expected to be {}'.format(
-                        os.path.join(self.root, 'hardware', 'testbench',
-                                     test_case, '{}_tb.py'.format(base_name))))
+                    "Cannot find the testbench! Expected to be {}".format(
+                        os.path.join(
+                            self.root,
+                            "hardware",
+                            "testbench",
+                            test_case,
+                            "{}_tb.py".format(base_name),
+                        )))
                 return 1
             # Check if the design file exists
             if not os.path.isfile(
-                    os.path.join(self.root, 'hardware',
-                                 '{}.sv'.format(test_case))):
+                    os.path.join(self.root, "hardware",
+                                 "{}.sv".format(test_case))):
                 self.logger.error(
-                    'Cannot find the design file! Expected to be {}'.format(
-                        os.path.join(self.root, 'hardware',
-                                     '{}.sv'.format(test_case))))
+                    "Cannot find the design file! Expected to be {}".format(
+                        os.path.join(self.root, "hardware",
+                                     "{}.sv".format(test_case))))
                 return 1
         return 0
 
     def lint(self):
         """Lint all the required design files"""
         cwd = os.path.join(self.root)
-        self.logger.info('Linting all the design files...')
+        self.logger.info("Linting all the design files...")
         for test_case in self.test_cases:
-            design_file = os.path.join(self.root, 'hardware', test_case)
+            design_file = os.path.join(self.root, "hardware", test_case)
             include_files = [
-                '-I{}'.format(
-                    os.path.join(self.root, 'hardware',
-                                 '{}'.format(include_file)))
+                "-I{}".format(
+                    os.path.join(self.root, "hardware",
+                                 "{}".format(include_file)))
                 for include_file in test_cases[test_case]
             ]
             cmd = [
-                'verilator', '--lint-only', '--Wall',
-                '{}.sv'.format(design_file)
+                "verilator",
+                "--lint-only",
+                "--Wall",
+                "{}.sv".format(design_file),
             ] + include_files
             result = self.execute(cmd, log_output=self.isdebug, cwd=cwd)
             if result:
@@ -104,30 +116,30 @@ class TestHardware:
             err += queue.get()
         if err:
             self.logger.error(
-                'Hardware regression test finished. {} errors.'.format(err))
+                "Hardware regression test finished. {} errors.".format(err))
         else:
             self.logger.info(
-                'Hardware regression test finished. {} errors.'.format(err))
+                "Hardware regression test finished. {} errors.".format(err))
 
-        cwd = os.path.join(self.root, 'hardware', 'testbench')
-        pycache_files = os.path.join(cwd, '__pycache__')
+        cwd = os.path.join(self.root, "hardware", "testbench")
+        pycache_files = os.path.join(cwd, "__pycache__")
         if os.path.exists(pycache_files):
             shutil.rmtree(pycache_files)
         return err
 
     def single_test(self, test_case, queue):
-        self.logger.info('Running unit test for {}...'.format(test_case))
+        self.logger.info("Running unit test for {}...".format(test_case))
         base_name = os.path.basename(test_case)
-        cwd = os.path.join(self.root, 'hardware', 'testbench', test_case)
-        cmd = ['python3', '{}_tb.py'.format(base_name)]
+        cwd = os.path.join(self.root, "hardware", "testbench", test_case)
+        cmd = ["python3", "{}_tb.py".format(base_name)]
         result = self.execute(cmd, log_output=self.isdebug, cwd=cwd)
         if result:
-            self.logger.error('FAIL.')
+            self.logger.error("FAIL.")
         else:
             # Clean files
-            pycache_files = os.path.join(cwd, '__pycache__')
-            simbuild_files = os.path.join(cwd, 'sim_build')
-            result_file = os.path.join(cwd, 'results.xml')
+            pycache_files = os.path.join(cwd, "__pycache__")
+            simbuild_files = os.path.join(cwd, "sim_build")
+            result_file = os.path.join(cwd, "results.xml")
             if os.path.exists(pycache_files):
                 shutil.rmtree(pycache_files)
             if os.path.exists(simbuild_files):
@@ -137,7 +149,7 @@ class TestHardware:
         queue.put(result)
         return result
 
-    def execute(self, cmd, log_output: bool = True, log_file=None, cwd='.'):
+    def execute(self, cmd, log_output: bool = True, log_file=None, cwd="."):
         if log_output:
             self.logger.debug(subprocess.list2cmdline(cmd))
             with subprocess.Popen(cmd,
@@ -145,20 +157,24 @@ class TestHardware:
                                   bufsize=1,
                                   universal_newlines=True,
                                   cwd=cwd) as result:
-                if log_file: f = open(log_file, 'w')
+                if log_file:
+                    f = open(log_file, "w")
                 if result.stdout or result.stderr:
-                    self.logger.info('')
+                    self.logger.info("")
                 if result.stdout:
                     for line in result.stdout:
-                        if log_file: f.write(line)
+                        if log_file:
+                            f.write(line)
                         line = line.rstrip("\n")
                         self.logger.trace(line)
                 if result.stderr:
                     for line in result.stderr:
-                        if log_file: f.write(line)
+                        if log_file:
+                            f.write(line)
                         line = line.rstrip("\n")
                         self.logger.trace(line)
-                if log_file: f.close()
+                if log_file:
+                    f.close()
         else:
             result = subprocess.run(cmd, stdout=subprocess.DEVNULL, cwd=cwd)
         return result.returncode
@@ -167,30 +183,41 @@ class TestHardware:
 # ---------- main function --------------
 def main():
     USAGE = """Usage: 
-test-hardware.py common/int_mult ...
+test-hardware.py --test common/int_mult ...
 test-hardware.py -a"""
 
     parser = ArgumentParser(usage=USAGE)
-    parser.add_argument('-a',
-                         '--all',
-                         action='store_true',
-                         dest='run_all',
-                         default=False,
-                         help='Run the whole regression test, Default=False')
-    parser.add_argument('--debug',
-                         action='store_true',
-                         dest='debug',
-                         default=False,
-                         help='Run in debug mode, Default=False')
+    parser.add_argument(
+        "-a",
+        "--all",
+        action="store_true",
+        dest="run_all",
+        default=False,
+        help="Run the whole regression test, Default=False",
+    )
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        dest="debug",
+        default=False,
+        help="Run in debug mode, Default=False",
+    )
+    parser.add_argument("--test",
+                        default="",
+                        nargs="+",
+                        dest="test_cases",
+                        help="Test individual cases")
 
     args = parser.parse_args()
     testrun = TestHardware(args)
     lint = testrun.lint()
-    if lint: sys.exit(lint)
+    if lint:
+        sys.exit(lint)
     run = testrun.test()
-    if run: sys.exit(run)
+    if run:
+        sys.exit(run)
     sys.exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
