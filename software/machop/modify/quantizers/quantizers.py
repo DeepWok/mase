342b31e483369bfe86dff6a1b5b34fe9c73c430c
ChengZhang-98 <102538889+ChengZhang-98@users.noreply.github.com>
2023-04-22 17:03:06 +0100

    Supports unsigned fixed-point quantizer (#168)
    ReLUInteger/relu_integer uses unsigned int by default

--- software/machop/modify/quantizers/quantizers.py
+++ software/machop/modify/quantizers/quantizers.py
@@ -10,7 +10,9 @@ from torch import Tensor
 from .utils import block, my_clamp, my_round, unblock
 
 
-def integer_quantizer(x: Union[Tensor, ndarray], width: int, frac_width: int):
+def integer_quantizer(
+    x: Union[Tensor, ndarray], width: int, frac_width: int, is_signed: bool = True
+):
     """
     - Do linear quantization to input according to a scale and number of bits
     - Note that `bias` can be negative or larger than `bits`
@@ -27,15 +29,21 @@ def integer_quantizer(x: Union[Tensor, ndarray], width: int, frac_width: int):
     For example: 0b101 . 00111, bits = 8, bias = 5
 
     """
-    thresh = 2 ** (width - 1)
+    if is_signed:
+        int_min = -(2 ** (width - 1))
+        int_max = 2 ** (width - 1) - 1
+    else:
+        int_min = 0
+        int_max = 2**width - 1
+    # thresh = 2 ** (width - 1)
     scale = 2**frac_width
 
     if isinstance(x, (Tensor, ndarray)):
-        return my_clamp(my_round(x.mul(scale)), -thresh, thresh - 1).div(scale)
+        return my_clamp(my_round(x.mul(scale)), int_min, int_max).div(scale)
     elif isinstance(x, int):
         return x
     else:
-        return my_clamp(my_round(x * scale), -thresh, thresh - 1) / scale
+        return my_clamp(my_round(x * scale), int_min, int_max) / scale
 
 
 def integer_fraction(
