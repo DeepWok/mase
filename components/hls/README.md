00a09e5e7c53a84f052c1d1d3d67d25ec7223c62
Jianyi Cheng <jianyi.cheng@cl.cam.ac.uk>
2023-07-28 16:56:24 +0100

    Added Regression modelling for int linear2d (#335)
    * Updated the vhls scripts
    
    * Clear dir
    
    * Added init regression model template
    
    * Added init int_linear2d DSE script
    
    * Added doc
    
    * Added design space
    
    * format
    
    * Updated config
    
    ---------
    
    Co-authored-by: JianyiCheng <jc9016@ic.ac.uk>

---
+++ components/hls/README.md
@@ -0,0 +1,8 @@
+# Regression Model Generation from HLS Components
+
+This folder contains a Python-based code generator for emitting HLS components in C.
+The exploration toolchain explores an HLS component with different design constraints and returns a regression model for performance and area estimation. 
+
+```sh
+python3 hls_regression.py --op=int_linear2d --mode all --dir dse_linear2d
+```
---
+++ components/hls/hls_regression.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python3
+# ---------------------------------------
+# This script runs the regression model for hls
+# ---------------------------------------
+
+from argparse import ArgumentParser
+import os
+
+from regression_gen import int_linear2d_dse
+
+
+def run(args):
+    op = args.op
+    if not os.path.exists(args.dir):
+        os.mkdir(args.dir)
+
+    mode = args.mode
+    top = args.dir
+    if op == "int_linear2d":
+        int_linear2d_dse(mode=mode, top=top)
+    else:
+        assert False, f"Unsupported op = {op}"
+
+
+# ---------- main function --------------
+def main():
+    USAGE = """Usage:
+mase_hls  ...
+"""
+
+    parser = ArgumentParser(usage=USAGE)
+    parser.add_argument(
+        "--op",
+        dest="op",
+        default=None,
+        help="Op name to explore",
+    )
+    parser.add_argument(
+        "--dir",
+        dest="dir",
+        default=os.path.join(os.getcwd(), "dse"),
+        help="Directory to store files",
+    )
+    parser.add_argument(
+        "--mode",
+        dest="mode",
+        default=None,
+        help="Mode to run: codegen, synth, report, all",
+    )
+    args = parser.parse_args()
+    run(args)
+
+
+if __name__ == "__main__":
+    main()
