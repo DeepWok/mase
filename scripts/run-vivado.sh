f621a50b2eb209effbec4d427e571e7f51356e2a
Jianyi Cheng <jianyi.cheng@cl.cam.ac.uk>
2023-07-28 12:52:32 +0100

    Updated the vhls scripts (#334)
    Co-authored-by: JianyiCheng <jc9016@ic.ac.uk>

--- scripts/run-vivado.sh
+++ scripts/run-vivado.sh
@@ -7,7 +7,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-source $VHLS/Vivado/2020.2/settings64.sh
-source $VHLS/Vitis_HLS/2020.2/settings64.sh
+source $VHLS/Vivado/$XLNX_VERSION/settings64.sh
+source $VHLS/Vitis_HLS/$XLNX_VERSION/settings64.sh
 LD_PRELOAD=/lib/x86_64-linux-gnu/libudev.so.1 vivado -mode batch -source $1
 
