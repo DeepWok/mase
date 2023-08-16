ded8f5a72d12a80151a00a15a8ccdfea02cd3225
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2023-08-16 17:35:36 +0100

    Integrate conv-bn fusion pass into the transform action (#374)

---
+++ machop/chop/passes/transforms/utils/conv_bn_fusion.py
@@ -0,0 +1,77 @@
+# A pass to fuse batch normalisation layers with the preceeding convolutional layers
+# NOTE: This implementation is a derivative of the following:
+# https://github.com/pytorch/pytorch/blob/main/torch/fx/experimental/optimization.py
+
+import tqdm
+import torch.nn as nn
+import torch.fx as fx
+from tqdm.contrib.logging import tqdm_logging_redirect
+from torch.nn.utils.fusion import fuse_conv_bn_eval
+from torch.fx.experimental.optimization import (
+    matches_module_pattern,
+    replace_node_module,
+)
+
+from chop.tools.logger import getLogger
+
+# Housekeeping -------------------------------------------------------------------------
+logger = getLogger(__file__)
+logger.propagate = False  # Avoid duplicate logs
+
+
+def conv_bn_fusion_transform_pass(graph, **_):
+    PATTERNS = [
+        (nn.Conv1d, nn.BatchNorm1d),
+        (nn.Conv2d, nn.BatchNorm2d),
+        (nn.Conv3d, nn.BatchNorm3d),
+    ]
+
+    modules = dict(graph.model.named_modules())
+    logger.debug(f"Found {len(modules)} modules in the model:\n{list(modules.keys())}")
+
+    # Modify the graph in place.
+    total = len(graph.fx_graph.nodes) * len(PATTERNS)
+    with tqdm_logging_redirect(total=total, loggers=[logger]) as pbar:
+        for pattern in PATTERNS:
+            fst, snd = pattern[0].__name__, pattern[1].__name__
+            pbar.set_description(f"Looking for pattern {fst} -> {snd}")
+
+            # Iterate over the graph and fuse the nodes that match the patterns
+            for node in graph.fx_graph.nodes:
+                if matches_module_pattern(pattern, node, modules):
+                    # There may be architectures where such a pattern exits. In these
+                    # cases, fusion isn't trivial. For now, we'll just skip these cases.
+                    if len(node.args[0].users) > 1:
+                        logger.warning("Conv output used by other nodes. Skipped!")
+                        continue
+                    conv = modules[node.args[0].target]
+                    bn = modules[node.target]
+                    if not bn.track_running_stats:
+                        # When track_running_stats is False, the batch norm module's
+                        # running mean and variance buffers are set to None.
+                        logger.warning("Batchnorm not tracking stats. Skipped!")
+                        continue
+                    # Set both modules to eval mode
+                    conv_prev_mode, bn_prev_mode = conv.training, bn.training
+                    conv.train(False)
+                    bn.train(False)
+                    # Fuse!
+                    fused_conv = fuse_conv_bn_eval(conv, bn)
+                    # Restore the previous modes
+                    conv.train(conv_prev_mode)
+                    bn.train(bn_prev_mode)
+                    # NOTE: We may need to update metadata here. Currently unclear.
+                    # Replace conv with the fused module and erase the batchnorm node
+                    replace_node_module(node.args[0], modules, fused_conv)
+                    node.replace_all_uses_with(node.args[0])
+                    graph.fx_graph.erase_node(node)
+                    pbar.update(1)  # Account for removed node :)
+                pbar.update(1)
+
+        # Update the model to reflect the changes in the graph
+        graph.model = fx.GraphModule(graph.model, graph.fx_graph)
+        pbar.set_description("Done")
+
+    modules = dict(graph.model.named_modules())
+    logger.debug(f"Found {len(modules)} modules in the model:\n{list(modules.keys())}")
+    return graph
---
+++ machop/test/passes/transforms/utils/conv_bn_fusion.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# NOTE: This is not really a test, but a script to just informally validate
+# functionality via trial and error. Feel free to modify this file as needed.
+
+import logging
+import os
+import sys
+from pathlib import Path
+
+import toml
+
+# Housekeeping -------------------------------------------------------------------------
+os.environ["PYTHONBREAKPOINT"] = "ipdb.set_trace"
+sys.path.append(Path(__file__).resolve().parents[4].as_posix())
+
+import chop.models as models
+from chop.passes import (
+    add_common_metadata_analysis_pass,
+    init_metadata_analysis_pass,
+    conv_bn_fusion_transform_pass,
+    add_software_metadata_analysis_pass,
+)
+from chop.passes.graph.mase_graph import MaseGraph
+from chop.tools.logger import getLogger
+from chop.tools.get_input import get_dummy_input
+from chop.dataset import MyDataModule, get_dataset_info
+
+logger = getLogger("chop")
+logger.setLevel(logging.DEBUG)
+
+
+def main():
+    MODEL = "resnet18"
+    DATASET = "cifar10"
+
+    # NOTE: We're only concerned with pre-trained vision models
+    dataset_info = get_dataset_info(DATASET)
+    datamodule = MyDataModule(
+        model_name=MODEL,
+        dataset_name=DATASET,
+        batch_size=32,
+        workers=os.cpu_count(),
+        tokenizer=None,
+        max_token_len=None,
+    )
+    datamodule.prepare_data()
+    datamodule.setup()
+    # NOTE: We only support vision classification models for now.
+    dummy_input = get_dummy_input(datamodule, "cls", is_nlp_model=False)
+
+    model_inst_fn = models.model_map[MODEL]
+    model = model_inst_fn(dataset_info, pretrained=True)
+    model.eval()  # NOTE: This is a requirement for fusion to work
+
+    graph = MaseGraph(model=model)
+    # NOTE: Both functions have pass arguments that are not used in this example
+    graph = init_metadata_analysis_pass(graph, None)
+    graph = add_common_metadata_analysis_pass(graph, dummy_input)
+    graph = add_software_metadata_analysis_pass(graph, None)
+    logger.debug(graph.fx_graph)
+
+    # The default save directory is specified as the current working directory
+    graph = conv_bn_fusion_transform_pass(graph)
+
+
+if __name__ == "__main__":
+    main()
