d01a3750a6f2b0ade980c36a803b756467b5b38e
Jianyi Cheng <jianyi.cheng@cl.cam.ac.uk>
2023-09-15 15:09:19 +0100

    Fixed CI temporarily (#438)
    * Fixed CI temporarilyt
    
    * roll back to erwei's commit
    
    * Updated script for building
    
    * Added make clean
    
    ---------
    
    Co-authored-by: Jianyi Cheng <jc9016@ee-kraken.ee.ic.ac.uk>

--- scripts/build-aie.sh
+++ scripts/build-aie.sh
@@ -13,8 +13,9 @@ MASE=$SCRIPT_DIR/..
 LLVM_DIR="${MASE}/llvm"
 
 # Build MLIR-AIE
+# export PATH=$PATH:${VHLS}/Vitis/2023.1/aietools/bin:${VHLS}/Vitis/2023.1/bin
 bash ${MASE}/mlir-air/utils/build-mlir-aie-local.sh \
   ${LLVM_DIR} \
-  ${MASE}/cmakeModules/cmakeModulesXilinx \
+  ${MASE}/mlir-aie/cmake/modulesXilinx \
+  ${MASE}/aienginev2/install \
   ${MASE}/mlir-aie
-
