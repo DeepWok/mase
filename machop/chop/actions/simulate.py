84826ab059ebf4d8f13e6fa1a2f791a7416512b0
pgimenes <pgimenes@outlook.com>
2024-01-24 21:31:25 +0000

    simulate action changes

--- machop/chop/actions/simulate.py
+++ machop/chop/actions/simulate.py
@@ -1,15 +1,32 @@
-from os import getenv
+from os import getenv, PathLike
+import torch
 from cocotb.runner import get_runner, get_results
 from pathlib import Path
 import mase_components
 from mase_components import get_modules
 
+from .emit import emit
 
-def simulate(skip_build: bool = False, skip_test: bool = False):
+
+def simulate(
+    model: torch.nn.Module,
+    model_info,
+    task: str,
+    dataset_info,
+    data_module,
+    load_name: PathLike = None,
+    load_type: str = None,
+    run_emit: bool = False,
+    skip_build: bool = False,
+    skip_test: bool = False,
+):
     SIM = getenv("SIM", "verilator")
     runner = get_runner(SIM)
 
     project_dir = Path.home() / ".mase" / "top"
+
+    if run_emit:
+        emit(model, model_info, task, dataset_info, data_module, load_name, load_type)
 
     if not skip_build:
         # To do: extract from mz checkpoint
@@ -28,11 +45,7 @@ def simulate(skip_build: bool = False, skip_test: bool = False):
                 for module in get_modules()
             ],
             hdl_toplevel="top",
-            build_args=[
-                "-Wno-fatal",
-                "-Wno-lint",
-                "-Wno-style",
-            ],
+            build_args=["-Wno-fatal", "-Wno-lint", "-Wno-style", "--trace"],
             parameters=[],  # use default parameters,
         )
 
