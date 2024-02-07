596e74d522da5583ec8be33bb79832314699aa13
pgimenes <pgimenes@outlook.com>
2024-02-07 14:53:10 +0000

    hw documentation for linear layer
    formatting

--- machop/mase_components/linear/test/fixed_linear_tb.py
+++ machop/mase_components/linear/test/fixed_linear_tb.py
@@ -22,7 +22,7 @@ logger.setLevel(logging.DEBUG)
 
 
 class LinearTB(Testbench):
-    def __init__(self, dut) -> None:
+    def __init__(self, dut, in_features=4, out_features=4) -> None:
         super().__init__(dut, dut.clk, dut.rst)
 
         if not hasattr(self, "log"):
@@ -49,8 +49,8 @@ class LinearTB(Testbench):
         )
         # Model
         self.model = LinearInteger(
-            in_features=4,
-            out_features=4,
+            in_features=in_features,
+            out_features=out_features,
             bias=False,
             config={
                 "data_in_width": 16,
@@ -67,41 +67,59 @@ class LinearTB(Testbench):
 
     def preprocess_tensor(self, tensor, quantizer, config, parallelism):
         tensor = quantizer(tensor)
-        tensor = (tensor * 2**config["frac_width"]).int()
+        tensor = (tensor * 2 ** config["frac_width"]).int()
         logger.info(f"Tensor in int format: {tensor}")
         tensor = tensor.reshape(-1, parallelism).tolist()
         return tensor
 
+    async def run_test(self):
+        await self.reset()
+        logger.info(f"Reset finished")
+        self.data_out_0_monitor.ready.value = 1
+
+        inputs = self.generate_inputs()
+        exp_out = self.model(inputs)
+
+        # Load the inputs driver
+        logger.info(f"Processing inputs")
+        inputs = self.preprocess_tensor(
+            inputs,
+            self.model.x_quantizer,
+            {"widht": 16, "frac_width": 3},
+            int(self.dut.DATA_IN_0_PARALLELISM_DIM_0),
+        )
+        self.data_in_0_driver.load_driver(inputs)
+
+        # Load the weights driver
+        logger.info(f"Processing weights")
+        weights = self.preprocess_tensor(
+            self.model.weight,
+            self.model.w_quantizer,
+            {"widht": 16, "frac_width": 3},
+            int(self.dut.WEIGHT_PARALLELISM_DIM_0)
+            * int(self.dut.DATA_IN_0_PARALLELISM_DIM_0),
+        )
+        self.weight_driver.load_driver(weights)
+
+        # Load the output monitor
+        logger.info(f"Processing outputs: {exp_out}")
+        # To do: need to quantize output to a different precision
+        outs = self.preprocess_tensor(
+            exp_out,
+            self.model.x_quantizer,
+            {"widht": 16, "frac_width": 3},
+            int(self.dut.DATA_OUT_0_PARALLELISM_DIM_0),
+        )
+        self.data_out_0_monitor.load_monitor(outs)
+
+        await Timer(1000, units="us")
+        assert self.data_out_0_monitor.exp_queue.empty()
+
+
 @cocotb.test()
-async def test(dut):
-    tb = LinearTB(dut)
-    await tb.reset()
-    logger.info(f"Reset finished")
-    tb.data_out_0_monitor.ready.value = 1
-
-    inputs = tb.generate_inputs()
-    exp_out = tb.model(inputs)
-
-    # Load the inputs driver
-    logger.info(f"Processing inputs")
-    inputs = tb.preprocess_tensor(inputs, tb.model.x_quantizer, {"widht": 16, "frac_width": 3}, int(dut.DATA_IN_0_PARALLELISM_DIM_0))
-    tb.data_in_0_driver.load_driver(inputs)
-
-    # Load the weights driver
-    logger.info(f"Processing weights")
-    weights = tb.preprocess_tensor(tb.model.weight, tb.model.w_quantizer, {"widht": 16, "frac_width": 3}, int(dut.WEIGHT_PARALLELISM_DIM_0) * int(dut.DATA_IN_0_PARALLELISM_DIM_0))
-    tb.weight_driver.load_driver(weights)
-
-    # Load the output monitor
-    logger.info(f"Processing outputs: {exp_out}")
-    # To do: need to quantize output to a different precision
-    outs = tb.preprocess_tensor(exp_out, tb.model.x_quantizer, {"widht": 16, "frac_width": 3}, int(dut.DATA_OUT_0_PARALLELISM_DIM_0))
-    tb.data_out_0_monitor.load_monitor(outs)
-
-    # To do: replace with tb.run()
-    await Timer(1000, units="us")
-    # To do: replace with tb.monitors_done() --> for monitor, call monitor_done()
-    assert tb.data_out_0_monitor.exp_queue.empty()
+async def test_20x20(dut):
+    tb = LinearTB(dut, in_features=20, out_features=20)
+    await tb.run_test()
 
 
 if __name__ == "__main__":
@@ -109,15 +127,34 @@ if __name__ == "__main__":
         trace=True,
         module_param_list=[
             {
-                "DATA_IN_0_TENSOR_SIZE_DIM_0": 4,
-                "DATA_IN_0_TENSOR_SIZE_DIM_1": 1,
-                "WEIGHT_TENSOR_SIZE_DIM_0": 4,
-                "WEIGHT_TENSOR_SIZE_DIM_1": 4,
-                "WEIGHT_PARALLELISM_DIM_0": 4,
-                "DATA_OUT_0_TENSOR_SIZE_DIM_0": 4,
-                "DATA_OUT_0_PARALLELISM_DIM_0": 4,
-                "BIAS_TENSOR_SIZE_DIM_0": 4,
-                "BIAS_PARALLELISM_DIM_0": 1,
-            }
+                "DATA_IN_0_TENSOR_SIZE_DIM_0": 20,
+                "DATA_IN_0_PARALLELISM_DIM_0": 2,
+                "WEIGHT_TENSOR_SIZE_DIM_0": 20,
+                "WEIGHT_TENSOR_SIZE_DIM_1": 20,
+                "WEIGHT_PARALLELISM_DIM_0": 20,
+                "DATA_OUT_0_TENSOR_SIZE_DIM_0": 20,
+                "DATA_OUT_0_PARALLELISM_DIM_0": 20,
+                "BIAS_TENSOR_SIZE_DIM_0": 20,
+            },
+            {
+                "DATA_IN_0_TENSOR_SIZE_DIM_0": 20,
+                "DATA_IN_0_PARALLELISM_DIM_0": 4,
+                "WEIGHT_TENSOR_SIZE_DIM_0": 20,
+                "WEIGHT_TENSOR_SIZE_DIM_1": 20,
+                "WEIGHT_PARALLELISM_DIM_0": 20,
+                "DATA_OUT_0_TENSOR_SIZE_DIM_0": 20,
+                "DATA_OUT_0_PARALLELISM_DIM_0": 20,
+                "BIAS_TENSOR_SIZE_DIM_0": 20,
+            },
+            {
+                "DATA_IN_0_TENSOR_SIZE_DIM_0": 20,
+                "DATA_IN_0_PARALLELISM_DIM_0": 5,
+                "WEIGHT_TENSOR_SIZE_DIM_0": 20,
+                "WEIGHT_TENSOR_SIZE_DIM_1": 20,
+                "WEIGHT_PARALLELISM_DIM_0": 20,
+                "DATA_OUT_0_TENSOR_SIZE_DIM_0": 20,
+                "DATA_OUT_0_PARALLELISM_DIM_0": 20,
+                "BIAS_TENSOR_SIZE_DIM_0": 20,
+            },
         ],
     )
--- machop/sphinx_docs/source/conf.py
+++ machop/sphinx_docs/source/conf.py
@@ -43,6 +43,22 @@ extensions = [
     "myst_parser",
 ]
 
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
