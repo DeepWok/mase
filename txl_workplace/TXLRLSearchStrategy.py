8b12470dc407ff051110c2e54a9e1aeccbcdb149
ih8gin <erictxl@outlook.com>
2024-03-04 00:49:48 +0000

    init txl rl modules

--- txl_workplace/TXLRLSearchStrategy.py
+++ txl_workplace/TXLRLSearchStrategy.py
@@ -1,49 +1,41 @@
-import optuna
 import torch
-import pandas as pd
 import logging
-from tabulate import tabulate
-import joblib
 
-from functools import partial
 from chop.actions.search.strategies.base import SearchStrategyBase
-
-from chop.passes.module.analysis import calculate_avg_bits_module_analysis_pass
+from stable_baselines3 import A2C, PPO
+from stable_baselines3.common.callbacks import (
+    CallbackList,
+    CheckpointCallback,
+    EvalCallback,
+)
+from txl_workplace.TXLenv import MixedPrecisionEnv
 
 logger = logging.getLogger(__name__)
 
-
-def callback_save_study(
-    study: optuna.study.Study,
-    frozen_trial: optuna.trial.FrozenTrial,
-    save_dir,
-    save_every_n_trials=1,
-):
-    if (frozen_trial.number + 1) % save_every_n_trials == 0:
-        study_path = save_dir / f"study_trial_{frozen_trial.number}.pkl"
-        if not study_path.parent.exists():
-            study_path.parent.mkdir(parents=True)
-
-        with open(study_path, "wb") as f:
-            joblib.dump(study, f)
-
-
+algorithm_map = {
+    "ppo": PPO,
+    "a2c": A2C,
+}
+env_map = {
+    "MixedPrecisionEnv": MixedPrecisionEnv
+}
 class SearchStrategyRL(SearchStrategyBase):
     is_iterative = False
 
     def _post_init_setup(self):
-        # self.sum_scaled_metrics = self.config["setup"]["sum_scaled_metrics"]
         self.metric_names = list(sorted(self.config["metrics"].keys()))
-        self.bf_result = {}
-        self.best_config = None
-        self.highest_score = None
-        self.counter = 0
-        # if not self.sum_scaled_metrics:
-        #     self.directions = [
-        #         self.config["metrics"][k]["direction"] for k in self.metric_names
-        #     ]
-        # else:
-        #     self.direction = self.config["setup"]["direction"]
+        setup = self.config['setup']
+        self.device = setup.get('device', default='cpu')
+        self.total_timesteps = setup["total_timesteps"]
+        algorithm_name = setup.get('algorithm', default='ppo')
+        env_name = setup.get('env', default='MixedPrecisionEnv')
+        if algorithm_name not in algorithm_map:
+            raise ValueError(f"Unsupported algorithm name: {algorithm_name}")
+        if env_name not in env_map:
+            raise ValueError(f"Unsupported env name: {env_name}")
+        self.algorithm = algorithm_map[algorithm_name]
+        self.env = env_map[env_name]
+        self.search_space = None
 
     def compute_software_metrics(self, model, sampled_config: dict, is_eval_mode: bool):
         # note that model can be mase_graph or nn.Module
@@ -68,37 +60,11 @@ class SearchStrategyRL(SearchStrategyBase):
                 metrics |= runner(self.data_module, model, sampled_config)
         return metrics
 
-    def search(self, search_space):
-        sampled_indexes = {}
-        name_list = list(search_space.choice_lengths_flattened.keys())
-        # TODO replace dynamic_bf to model learn in stable baseline3
-        # Complexity of Bruce-Force is Cartesian product of the numbers of possible choices of each component
-        # using recursion to implement dynamic components Bruce-Force
-        def dynamic_bf(index = 0):
-            if index == len(name_list):
-                score = self.run_trial(search_space, sampled_indexes)
-                self.counter += 1
-                print(f'For proposal-{self.counter}, score: {score}')
-                # print(score)
-                self.bf_result[str(sampled_indexes)] = score
-                if self.highest_score is None or score > self.highest_score:
-                    self.highest_score = score
-                    self.best_config = sampled_indexes
-                return
-            for i in range(search_space.choice_lengths_flattened[name_list[index]]):
-                sampled_indexes[name_list[index]] = i
-                dynamic_bf(index + 1)
-        dynamic_bf()
-        self.counter = 0
-        # print(self.bf_result)
-        print("best config: ", self.best_config)
-        print("highest score: ", self.highest_score)
-
-    def run_trial(self, search_space, sampled_indexes):
-        sampled_config = search_space.flattened_indexes_to_config(sampled_indexes)
+    def run_trial(self, sampled_indexes):
+        sampled_config = self.search_space.flattened_indexes_to_config(sampled_indexes)
 
         is_eval_mode = self.config.get("eval_mode", True)
-        model = search_space.rebuild_model(sampled_config, is_eval_mode)
+        model = self.search_space.rebuild_model(sampled_config, is_eval_mode)
         software_metrics = self.compute_software_metrics(
             model, sampled_config, is_eval_mode
         )
@@ -112,3 +78,34 @@ class SearchStrategyRL(SearchStrategyBase):
                     self.config["metrics"][metric_name]["scale"] * metrics[metric_name]
             )
         return sum(scaled_metrics.values())
+
+    def search(self, search_space):
+        self.search_space = search_space
+        env = self.env(config={"search_space": self.search_space, "run_trial": self.run_trial})
+
+        checkpoint_callback = CheckpointCallback(save_freq=1000, save_path="./logs/")
+        eval_callback = EvalCallback(
+            env,
+            best_model_save_path="./logs/best_model",
+            log_path="./logs/results",
+            eval_freq=500,
+        )
+        callback = CallbackList([checkpoint_callback, eval_callback])
+
+        # possible extension is to allow custom policy network
+        # https://stable-baselines3.readthedocs.io/en/master/guide/custom_policy.html
+
+        model = self.algorithm(
+            "MultiInputPolicy",
+            env,
+            verbose=1,
+            device=self.device,
+            tensorboard_log="./logs/",
+        )
+
+        vec_env = model.get_env()
+        model.learn(
+            total_timesteps=int(self.total_timesteps),
+            progress_bar=True,
+            callback=callback,
+        )
---
+++ txl_workplace/TXLenv.py
@@ -0,0 +1,81 @@
+import gymnasium as gym
+
+from gymnasium.spaces import Box, Dict, Discrete, MultiDiscrete
+
+
+class MixedPrecisionEnv(gym.Env):
+    def __init__(self, config):
+        # Make the space (for actions and observations) configurable.
+        # Since actions should repeat observations, their spaces must be the
+        # same.
+        self.search_space = config.get("search_space", None)
+        self.run_trial = config.get("run_trial", None)
+
+        # observation space definition
+        self.observation_space = Dict({"loss": Box(0.0, 10e4, shape=(1,))})
+        for k, v in self.search_space.search_space_flattened.items():
+            k1, k2 = k.split(".")
+            new_k = "_and_".join([k1, k2])
+            self.observation_space[new_k] = Discrete(v)
+        my_list = list(self.search_space.per_layer_search_space.values())
+
+        # action space definition
+        self.action_space = MultiDiscrete((self.search_space.num_layers, *my_list))
+
+        self.cur_obs = None
+        self.episode_len = 0
+
+    def reset(self, *, seed=None, options=None):
+        """Resets the episode and returns the initial observation of the new one."""
+        # Reset the episode len.
+        self.episode_len = 0
+        # Sample a random number from our observation space.
+        self.cur_obs = self.observation_space.sample()
+        # Return initial observation.
+        return self.cur_obs, {}
+
+    def step(self, action):
+        """Takes a single step in the episode given `action`
+
+        Returns:
+            New observation, reward, done-flag, info-dict (empty).
+        """
+        loss = self.cur_obs.pop("loss")
+        cur_obs = {k: v for k, v in self.cur_obs.items()}
+        sample, sample_idx = self.search_space.build_sample(cur_obs, "_and_")
+
+        # We use the first discrete value in action to pick layer
+        # The rest of the action discrete values
+        # are used to pick quantisation choices in that layer
+        i = 0
+        for k, v in sample.items():
+            if i == action[0]:
+                j = 1
+                for k2, v in v["config"].items():
+                    if k2 == "name":
+                        sample[k]["config"][k2] = v
+                    else:
+                        choices = self.search_space.choices[k][k2]
+                        sample[k]["config"][k2] = choices[action[j]]
+                        sample_idx[k][k2] = action[j]
+                        j += 1
+
+        # get performance
+        metric = self.run_trial(sample)
+
+        # Set `truncated` flag after 10 steps.
+        self.episode_len += 1
+        terminated = False
+        truncated = self.episode_len >= 10
+
+        # TODO: this reward needs redesign, apparently
+        reward = -metric
+        # Set a new observation (random sample).
+        rebuild_sample = {}
+        for k, v in sample_idx.items():
+            for k2, v2 in v.items():
+                rebuild_sample[f"{k}_and_{k2}"] = v2
+
+        rebuild_sample["loss"] = [metric]
+        self.cur_obs = rebuild_sample
+        return self.cur_obs, reward, terminated, truncated, {"loss": metric}
