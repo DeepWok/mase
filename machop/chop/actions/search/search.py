dd0ae41e293768a2c33b2c9be4ac2342a8bdad71
HonAnson <hon.anson@gmail.com>
2024-03-09 18:35:48 +0000

    still buggy

--- machop/chop/actions/search/search.py
+++ machop/chop/actions/search/search.py
@@ -3,7 +3,7 @@ from os import PathLike
 
 import toml
 import torch
-
+from fvcore.common.config import CfgNode
 from ...tools.checkpoint_load import load_model
 from ...tools.config_load import load_config
 from ...tools.get_input import get_dummy_input
@@ -12,29 +12,39 @@ from .strategies import get_search_strategy_cls
 from chop.tools.utils import device
 from chop.tools.utils import parse_accelerator
 
-logger = logging.getLogger(__name__)
-IS_NAS=False
 
+# For nas proxy search
+import numpy as np
+from chop.NASLib.naslib.search_spaces import NasBench201SearchSpace
+from chop.NASLib.naslib.utils import get_zc_benchmark_api,get_dataset_api
+from chop.NASLib.naslib.utils import get_train_val_loaders, get_project_root
+from chop.NASLib.naslib.predictors import ZeroCost
+from chop.NASLib.naslib.search_spaces.core import Metric
+
+
+logger = logging.getLogger(__name__)
+### Function made for nas config
+def parse_nas_config(config):
+    search_config = config["search"]   #p arse into search config
+    nas_config = search_config['nas']  # parse into nas
+    op_config = nas_config['op_config']
+    proxy_config = nas_config['proxy_config']
+    return op_config, proxy_config
+    
+
+    
 def parse_search_config(config):
     """
     Parse search config from a dict or a toml file and do sanity check.
 
     ---
     The search config must consist of two parts: strategy and search_space.
-    """
-    
-    if isinstance(config['nas'],dict):
-        search_config = config["search"]
-        n_config =config[]
-
-        return
-    
+    """        
 
     if not isinstance(config, dict):
         search_config = load_config(config)
 
     search_config = config["search"]  # the actual config for action search
-    
     strategy_config = search_config["strategy"]
     search_space_config = search_config["search_space"]
         
@@ -60,6 +70,59 @@ def search(
     """
     # search preparation
     accelerator = parse_accelerator(accelerator)
+    
+    ### Our contribution here:
+    if isinstance(search_config['search']['nas'],dict):
+        op_config, proxy_config = parse_nas_config(search_config)   # type(op_config) = list of tuple , type(proxy_config) = list of strings
+        # Prepare dict for recording scores
+        scores = {}
+        for proxy_name in proxy_config:
+            scores[proxy_name] = []
+        train_accuries = []
+        val_accuries = []
+
+        for op in op_config:
+            # config_dict is config from nas-bench
+            config_dict = {
+                'dataset': 'cifar10', # Dataset to loader: can be cifar10, cifar100, ImageNet16-120
+                'data': str(get_project_root()) + '/data', # path to naslib/data where cifar is saved
+                'search': {
+                    'seed': 9001, # Seed to use in the train, validation and test dataloaders
+                    'train_portion': 0.7, # Portion of train dataset to use as train dataset. The rest is used as validation dataset.
+                    'batch_size': 32, # batch size of the dataloaders
+                }
+            }
+            config = CfgNode(config_dict)
+            train_loader, val_loader, test_loader, train_transform, valid_transform = get_train_val_loaders(config)
+
+            # Generate models
+            graph = NasBench201SearchSpace(n_classes=10)
+            graph.sample_architecture(op_indces=op)
+            graph.parse()
+            graph.get_hash()
+            dataset_apis={}
+            dataset_apis["NASBench201-cifar10"] = get_dataset_api(search_space='nasbench201', dataset='cifar10')
+            train_acc_parent = graph.query(metric=Metric.TRAIN_ACCURACY, dataset='cifar10', dataset_api=dataset_apis["NASBench201-cifar10"])
+            val_acc_parent = graph.query(metric=Metric.VAL_ACCURACY, dataset='cifar10', dataset_api=dataset_apis["NASBench201-cifar10"])
+            
+            train_accuries.append(train_acc_parent)
+            val_accuries.append(val_acc_parent)
+
+
+            for zc_proxy in proxy_config:
+                zc_predictor = ZeroCost(method_type=zc_proxy)
+                score = zc_predictor.query(graph=graph, dataloader=train_loader)
+                scores[proxy_name].append(score)
+
+        np.save(scores)
+        np.save(train_accuries)
+        np.save(val_accuries)
+
+        return
+    
+    ### End of our contribution
+
+        
     strategy_config, search_space_config = parse_search_config(search_config)
     save_path.mkdir(parents=True, exist_ok=True)
 
