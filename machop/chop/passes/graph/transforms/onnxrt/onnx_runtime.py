c725d4ca11cbbe2f53f9a883dd787216064365b5
Will Powell <me@willpowell.uk>
2024-03-10 21:51:17 +0000

    devised onnxruntime & transform pass structure

--- machop/chop/passes/graph/transforms/onnxrt/onnx_runtime.py
+++ machop/chop/passes/graph/transforms/onnxrt/onnx_runtime.py
@@ -1,4 +1,30 @@
+import ...
+
+def onnx_runtime_transform_pass(graph, pass_args="None"):
+    onnx_runtime_session = ONNXRuntime()
+
+    do_test = pass_args['do_test']
+    if do_test == 'before' or do_test == 'both':
+        ... #test pl performances
+
+    if do_test == 'after' or do_test == 'both':
+        ... #test onnx performances
+
+    return graph, {}
 
 
-def onnx_runtime_transform_pass():
-    ...
+class ONNXRuntime:
+    def __init__():
+        ...
+
+    def pytorch_to_onnx(self, ):
+        ...
+
+    def load_onnx(self, ):
+        '''Load .onnx model'''
+        ...
+
+    def test_performances(self, how="NA"):
+        ...
+
+        
