3771ff0af1c50fc6a32a89d5c0e1d30fd153af4a
Will Powell <me@willpowell.uk>
2024-03-10 22:54:52 +0000

    modify requirement from ort to ort-gpu

--- machop/chop/passes/graph/transforms/onnxrt/onnx_runtime.py
+++ machop/chop/passes/graph/transforms/onnxrt/onnx_runtime.py
@@ -10,6 +10,12 @@ def onnx_runtime_transform_pass(graph, pass_args="None"):
     if do_test == 'after' or do_test == 'both':
         ... #test onnx performances
 
+    elif do_test == 'NA':
+        pass
+
+    else:
+        raise Exception(f"Test argument not recognized; expected one in ['before','after','both','NA'], but {do_test} was received.")
+    
     return graph, {}
 
 
