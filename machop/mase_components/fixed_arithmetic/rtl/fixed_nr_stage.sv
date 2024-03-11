7e0bfe6611ad60ade9cedd0f7a7270118a426169
J-u1i0 <Julio.Castillejo.Motta@gmail.com>
2024-03-11 14:22:59 +0000

    Fixed testbench and reduced register sizes of nr stage

--- machop/mase_components/fixed_arithmetic/rtl/fixed_nr_stage.sv
+++ machop/mase_components/fixed_arithmetic/rtl/fixed_nr_stage.sv
@@ -4,16 +4,18 @@ module fixed_nr_stage #(
     localparam THREEHALFS = 3 << (WIDTH - 2)
 ) (
     // Input x.
-    input logic[2*WIDTH-1:0] data_a,    // FORMAT: Q1.(WIDTH-1).
+    input logic[WIDTH-1:0] data_a,    // FORMAT: Q1.(WIDTH-1).
     // Initial LUT guess.
-    input logic[2*WIDTH-1:0] data_b,    // FORMAT: Q1.(WIDTH-1).
-    output logic[2*WIDTH-1:0] data_out  // FORMAT: Q1.(WIDTH-1)
+    input logic[WIDTH-1:0] data_b,    // FORMAT: Q1.(WIDTH-1).
+    output logic[WIDTH-1:0] data_out  // FORMAT: Q1.(WIDTH-1)
 );
     logic[2*WIDTH-1:0] yy;
+    logic[2*WIDTH-1:0] temp;
     logic[2*WIDTH-1:0] mult;
 
     assign yy = (data_b * data_b) >> (WIDTH - 1);
     assign mult = ((data_a >> 1) * yy) >> (WIDTH - 1);
-    assign data_out = (data_b * (THREEHALFS - mult)) >> (WIDTH - 1);
+    assign temp = (data_b * (THREEHALFS - mult)) >> (WIDTH - 1);
+    assign data_out = temp;
 
 endmodule
--- machop/mase_components/fixed_arithmetic/test/fixed_nr_stage_tb.py
+++ machop/mase_components/fixed_arithmetic/test/fixed_nr_stage_tb.py
@@ -5,77 +5,61 @@ import random, os
 
 import cocotb
 from cocotb.triggers import Timer
+from mase_cocotb.testbench import Testbench
 from mase_cocotb.runner import mase_runner
 import math
+from isqrt_sw import nr_stage_sw, float_to_int, int_to_float, fixed_lut_index_sw, make_lut, range_reduction_sw
 
 
-def nr_stage_sw(x: int, lut: int) -> int:
-    """model of newton raphson stage"""
-    x = int_to_float(x, 1, 15)
-    lut = int_to_float(lut, 1, 15)
-    res = lut * (1.5 - 0.5 * x * lut * lut)
-    return float_to_int(res, 1, 15)
+class VerificationCase(Testbench):
+    def __init__(self, dut):
+        super().__init__(dut)
+        self.assign_self_params([
+            "WIDTH"
+        ])
 
-def float_to_int(x: float, int_width: int, frac_width: int) -> int:
-    integer = int(x)
-    x -= integer
-    res = integer * (2 ** frac_width)
-    for i in range(1, frac_width+1):
-        power = 2 ** (-i)
-        if power <= x:
-            x -= power
-            res += 2 ** (frac_width - i)
-    return res
+    def generate_inputs(self, lut_pow):
+        samples = 2 ** self.WIDTH
+        int_width = 1
+        frac_width = self.WIDTH - 1
+        data_x = []
+        initial_guesses = []
+        lut_size = 2 ** lut_pow
+        lut = make_lut(lut_size, self.WIDTH)
+        # NOTE: since negative values are not supported by fixed formats since
+        # isqrt only outputs positive results we cannot test every single com-
+        # bination of x and initial guesses.
+        for x in range(samples):
+            # Create inputs.
+            x_red = range_reduction_sw(x, self.WIDTH)
+            lut_index = fixed_lut_index_sw(x_red, self.WIDTH, lut_pow)
+            lut_value = lut[lut_index]
+            # Add inputs.
+            data_x.append(x_red)
+            initial_guesses.append(lut_value)
+        return data_x, initial_guesses, samples
 
-def int_to_float(x: int, int_width: int, frac_width: int) -> float:
-    integer = x / (2 ** frac_width)
-    fraction = x - integer * 2 ** frac_width
-    res = integer
-
-    for i in range(1, frac_width+1):
-        power = 2 ** (frac_width - i)
-        if power < fraction:
-            res += 2 ** (-i)
-            fraction -= power
-    return res
-
-# TODO: this is just for the Q8.0 format. Need to generalise to other formats.
-class VerificationCase:
-    def __init__(self, samples=1):
-        self.data_in_width = 16
-        self.int_width = 1
-        self.frac_width = 15
-        self.data_in_x = [float_to_int(random.random(), self.int_width, self.frac_width) for _ in range(samples)]
-        self.data_in_lut = [float_to_int(random.random(), self.int_width, self.frac_width) for _ in range(samples)]
-        self.samples = samples
-        self.ref = self.sw_compute()
-
-    def get_dut_parameters(self):
-        return {
-            "IN_WIDTH": self.data_in_width,
-            "INT_WIDTH": self.int_width,
-            "FRAC_WIDTH": self.frac_width
-        }
-
-    def sw_compute(self):
+    def model(self, data_x, initial_guesses):
         ref = []
-        for x, lut in zip(self.data_in_x, self.data_in_lut):
-            expected = nr_stage_sw(x, lut) 
+        for x, lut in zip(data_x, initial_guesses):
+            expected = nr_stage_sw(x, self.WIDTH, lut)
             ref.append(expected)
         return ref
 
 
 @cocotb.test()
 async def test_fixed_nr_stage(dut):
-    """Test for adding 2 random numbers multiple times"""
-    samples = 100
-    testcase = VerificationCase(samples=samples)
+    """Test for the Newton Raphson stage for isqrt"""
+    testcase = VerificationCase(dut)
+    lut_pow = 5
+    data_x, initial_guesses, samples = testcase.generate_inputs(lut_pow)
+    ref = testcase.model(data_x, initial_guesses)
+    width = testcase.WIDTH
 
-    #for i in range(samples):
     for i in range(samples):
         # Set up module data.
-        data_a = testcase.data_in_x[i]
-        data_b = testcase.data_in_lut[i]
+        data_a = data_x[i]
+        data_b = initial_guesses[i]
 
         # Force module data.
         dut.data_a.value = data_a
@@ -84,29 +68,38 @@ async def test_fixed_nr_stage(dut):
         await Timer(10, units="ns")
 
         # Exepected result.
-        expected = testcase.ref[i]
+        expected = ref[i]
 
         # Error 
-        error = abs(int_to_float(dut.data_out.value.integer, 1, 15) - int_to_float(expected, 1, 15))
+        error = abs(int_to_float(dut.data_out.value.integer, 1, width) - int_to_float(expected, 1, width))
 
         # Check the output.
         assert (
-                error < 0.1
+                error == 0
             ), f"""
             <<< --- Test failed --- >>>
             Input: 
-            X  : {int_to_float(data_a, 1, 15)}
-            LUT: {int_to_float(data_b, 1, 15)}
+            X  : {int_to_float(data_a, 1, width-1)}
+            LUT: {int_to_float(data_b, 1, width-1)}
 
             Output:
-            Out: {int_to_float(dut.data_out.value.integer, 1, 15)}
+            Out: {int_to_float(dut.data_out.value.integer, 1, width-1)}
             
             Expected: 
-            {int_to_float(expected, 1, 15)}
+            {int_to_float(expected, 1, width-1)}
 
             Error:
             {error}
             """
 
 if __name__ == "__main__":
-    mase_runner()
+    def full_sweep():
+        parameter_list = []
+        for width in range(1, 17):
+            parameter_list.append({"WIDTH": width})
+        return parameter_list
+
+    parameter_list = full_sweep()
+    #parameter_list = [{"WIDTH": 3}]
+
+    mase_runner(module_param_list=parameter_list)
