732649ef100eff0a77cd0f5ea192a2586ecca262
Jared Joselowitz <jaredjoss123@gmail.com>
2024-03-11 22:58:52 +0000

    Refactor

--- machop/chop/actions/search/search_space/zero_cost_proxy/graph.py
+++ machop/chop/actions/search/search_space/zero_cost_proxy/graph.py
@@ -1,53 +1,14 @@
-# This is the search space for mixed-precision post-training-quantization quantization search on mase graph.
-from copy import deepcopy
-from torch import nn
-from ..base import SearchSpaceBase
-from .....passes.graph.transforms.quantize import (
-    QUANTIZEABLE_OP,
-    quantize_transform_pass,
-)
-from .....ir.graph.mase_graph import MaseGraph
-from .....passes.graph import (
-    init_metadata_analysis_pass,
-    add_common_metadata_analysis_pass,
-)
-from .....passes.graph.utils import get_mase_op, get_mase_type
-from ..utils import flatten_dict, unflatten_dict
-from collections import defaultdict
+# This is the search space for Zero Cost Proxies
 
-# naslib imports
+from ..base import SearchSpaceBase
+
 from naslib.search_spaces import NasBench201SearchSpace
 from naslib.utils import get_dataset_api
-from naslib.predictors import ZeroCost
 from naslib.utils import get_train_val_loaders, get_project_root
 from fvcore.common.config import CfgNode
 from tqdm import tqdm
-from naslib.utils.encodings import EncodingType
 
-import random
-from tqdm import tqdm
-from naslib.search_spaces.core.query_metrics import Metric
-from naslib.search_spaces.nasbench201.conversions import convert_naslib_to_str
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-from naslib.utils import compute_scores  # computes more metrics than just correlation
-from scipy.stats import kendalltau, spearmanr
-
-from .utils import sample_arch_dataset, evaluate_predictions, iterate_whole_searchspace, encode_archs, eval_zcp
-
-DEFAULT_QUANTIZATION_CONFIG = {
-    "config": {
-        "name": "integer",
-        "bypass": True,
-        "bias_frac_width": 5,
-        "bias_width": 8,
-        "data_in_frac_width": 5,
-        "data_in_width": 8,
-        "weight_frac_width": 3,
-        "weight_width": 8,
-    }
-}
+from .utils import sample_arch_dataset, evaluate_predictions, eval_zcp
 
 DEFAULT_ZERO_COST_PROXY_CONFIG = {
     "config": {
@@ -68,59 +29,14 @@ class ZeroCostProxy(SearchSpaceBase):
         self.model.to("cpu")  # save this copy of the model to cpu
         self.mg = None
         self._node_info = None
-        self.default_config = DEFAULT_QUANTIZATION_CONFIG
+        self.default_config = DEFAULT_ZERO_COST_PROXY_CONFIG
         self.scores = {}
-        # self.spearman_metrics = {}
         self.zcp_results = []
-
-        # quantize the model by type or name
-        assert (
-            "by" in self.config["setup"]
-        ), "Must specify entry `by` (config['setup']['by] = 'name' or 'type')"
-
-    def rebuild_model(self, sampled_config, is_eval_mode: bool = True):
-        # set train/eval mode before creating mase graph
-
-        print("sampled_config:  ", sampled_config)
-
-        self.model.to(self.accelerator)
-        if is_eval_mode:
-            self.model.eval()
-        else:
-            self.model.train()
-
-        if self.mg is None:
-            assert self.model_info.is_fx_traceable, "Model must be fx traceable"
-            mg = MaseGraph(self.model)
-            mg, _ = init_metadata_analysis_pass(mg, None)
-            mg, _ = add_common_metadata_analysis_pass(
-                mg, {"dummy_in": self.dummy_input, "force_device_meta": False}
-            )
-            self.mg = mg
-        if sampled_config is not None:
-            mg, _ = quantize_transform_pass(self.mg, sampled_config)
-        mg.model.to(self.accelerator)
-        return mg
-
-    def _build_node_info(self):
-        """
-        Build a mapping from node name to mase_type and mase_op.
-        """
 
     def build_search_space(self):
         """
         Build the search space for the mase graph (only quantizeable ops)
         """
-
-        # Build a mapping from node name to mase_type and mase_op.
-        mase_graph = self.rebuild_model(sampled_config=None, is_eval_mode=True)
-        node_info = {}
-        for node in mase_graph.fx_graph.nodes:
-            node_info[node.name] = {
-                "mase_type": get_mase_type(node),
-                "mase_op": get_mase_op(node),
-            }
-
 
         # Create configs required for get_train_val_loaders
         config_dict = {
@@ -136,23 +52,6 @@ class ZeroCostProxy(SearchSpaceBase):
 
         # Get the dataloaders
         train_loader, val_loader, test_loader, train_transform, valid_transform = get_train_val_loaders(config)
-
-        # Sample a random NB201 graph and instantiate it
-        # if self.config["zc"]["benchmark"] == 'nasbench201':
-        #     graph = NasBench201SearchSpace()
-        #     graph.sample_random_architecture()
-        #     graph.parse()
-        # else:
-        #     raise ValueError(f"Unknown benchmark: {self.config['zc']['benchmark']}")
-
-        # print('Scores of the model: ')
-        # for zc_proxy in self.config["zc"]["zc_proxies"]:
-        #     zc_predictor = ZeroCost(method_type=zc_proxy)
-        #     score = zc_predictor.query(graph=graph, dataloader=train_loader)
-        #     self.scores[zc_proxy] = score
-
-        # print("self.scores: ", self.scores)
-
 
         seed = self.config["zc"]["seed"]
         
@@ -199,80 +98,3 @@ class ZeroCostProxy(SearchSpaceBase):
             # pdb.set_trace()
 
         print("zcp_results: ", self.zcp_results)
-
-        # Build the search space
-        choices = {}
-        seed = self.config["seed"]
-
-        match self.config["setup"]["by"]:
-            case "name":
-                # iterate through all the quantizeable nodes in the graph
-                # if the node_name is in the seed, use the node seed search space
-                # else use the default search space for the node
-                for n_name, n_info in node_info.items():
-                    if n_info["mase_op"] in QUANTIZEABLE_OP:
-                        if n_name in seed:
-                            choices[n_name] = deepcopy(seed[n_name])
-                        else:
-                            choices[n_name] = deepcopy(seed["default"])
-            case "type":
-                # iterate through all the quantizeable nodes in the graph
-                # if the node mase_op is in the seed, use the node seed search space
-                # else use the default search space for the node
-                for n_name, n_info in node_info.items():
-                    n_op = n_info["mase_op"]
-                    if n_op in QUANTIZEABLE_OP:
-                        if n_op in seed:
-                            choices[n_name] = deepcopy(seed[n_op])
-                        else:
-                            choices[n_name] = deepcopy(seed["default"])
-            case _:
-                raise ValueError(
-                    f"Unknown quantization by: {self.config['setup']['by']}"
-                )
-
-        # flatten the choices and choice_lengths
-        flatten_dict(choices, flattened=self.choices_flattened)
-        self.choice_lengths_flattened = {
-            k: len(v) for k, v in self.choices_flattened.items()
-        }
-
-    def flattened_indexes_to_config(self, indexes: dict[str, int]):
-        """
-        Convert sampled flattened indexes to a nested config which will be passed to `rebuild_model`.
-
-        ---
-        For example:
-        ```python
-        >>> indexes = {
-            "conv1/config/name": 0,
-            "conv1/config/bias_frac_width": 1,
-            "conv1/config/bias_width": 3,
-            ...
-        }
-        >>> choices_flattened = {
-            "conv1/config/name": ["integer", ],
-            "conv1/config/bias_frac_width": [5, 6, 7, 8],
-            "conv1/config/bias_width": [3, 4, 5, 6, 7, 8],
-            ...
-        }
-        >>> flattened_indexes_to_config(indexes)
-        {
-            "conv1": {
-                "config": {
-                    "name": "integer",
-                    "bias_frac_width": 6,
-                    "bias_width": 6,
-                    ...
-                }
-            }
-        }
-        """
-        flattened_config = {}
-        for k, v in indexes.items():
-            flattened_config[k] = self.choices_flattened[k][v]
-
-        config = unflatten_dict(flattened_config)
-        config["default"] = self.default_config
-        config["by"] = self.config["setup"]["by"]
-        return config
--- machop/chop/actions/search/strategies/naslib.py
+++ machop/chop/actions/search/strategies/naslib.py
@@ -60,65 +60,76 @@ class SearchStrategyNaslib(SearchStrategyBase):
                 raise ValueError(f"Unknown sampler name: {name}")
         return sampler
 
-    def compute_software_metrics(self, model, sampled_config: dict, is_eval_mode: bool):
-        # note that model can be mase_graph or nn.Module
-        metrics = {}
-        if is_eval_mode:
-            with torch.no_grad():
-                for runner in self.sw_runner:
-                    metrics |= runner(self.data_module, model, sampled_config)
-        else:
-            for runner in self.sw_runner:
-                metrics |= runner(self.data_module, model, sampled_config)
-        return metrics
+    # def objective(trial):
+    #     # Suggesting weights for each metric
+    #     weight_epe_nas = trial.suggest_float("weight_epe_nas", -50, 50)
+    #     weight_fisher = trial.suggest_float("weight_fisher", -50, 50)
+    #     weight_grad_norm = trial.suggest_float("weight_grad_norm", -50, 50)
+        
+    #     total_loss = 0
+    #     penalty = 0  # Initialize penalty
+        
+    #     for item in combined_data_list:
+    #         # Calculate predicted accuracy based on weights and zc metrics
+    #         predicted_accuracy = (item['metrics']['epe_nas'] * weight_epe_nas +
+    #                             item['metrics']['fisher'] * weight_fisher +
+    #                             item['metrics']['grad_norm'] * weight_grad_norm)
+            
+    #         # Assume 'train_accuracy' is your target
+    #         actual_accuracy = item['train_accuracy']
+            
+    #         # Calculating squared error loss
+    #         loss = (predicted_accuracy - actual_accuracy) ** 2
+    #         total_loss += loss
+            
+    #         # Apply penalties for predictions outside the [0, 100] range
+    #         if predicted_accuracy > 100:
+    #             penalty += (predicted_accuracy - 100) ** 2  # Penalty for exceeding 100%
+    #         elif predicted_accuracy < 0:
+    #             penalty += (predicted_accuracy - 0) ** 2  # Penalty for going below 0%
+        
+    #     # Incorporate penalties into the average loss
+    #     average_loss_with_penalty = (total_loss + penalty) / len(combined_data_list)
+        
+    #     return average_loss_with_penalty
 
-    def compute_hardware_metrics(self, model, sampled_config, is_eval_mode: bool):
-        metrics = {}
-        if is_eval_mode:
-            with torch.no_grad():
-                for runner in self.hw_runner:
-                    metrics |= runner(self.data_module, model, sampled_config)
-        else:
-            for runner in self.hw_runner:
-                metrics |= runner(self.data_module, model, sampled_config)
-        return metrics
 
-    def objective(self, trial: optuna.trial.Trial, search_space):
-        sampled_indexes = {}
-        if hasattr(search_space, "optuna_sampler"):
-            sampled_config = search_space.optuna_sampler(trial)
-        else:
-            for name, length in search_space.choice_lengths_flattened.items():
-                sampled_indexes[name] = trial.suggest_int(name, 0, length - 1)
-            sampled_config = search_space.flattened_indexes_to_config(sampled_indexes)
+    # def objective(self, trial: optuna.trial.Trial, search_space):
+    #     sampled_indexes = {}
+    #     if hasattr(search_space, "optuna_sampler"):
+    #         sampled_config = search_space.optuna_sampler(trial)
+    #     else:
+    #         for name, length in search_space.choice_lengths_flattened.items():
+    #             sampled_indexes[name] = trial.suggest_int(name, 0, length - 1)
+    #         sampled_config = search_space.flattened_indexes_to_config(sampled_indexes)
 
-        is_eval_mode = self.config.get("eval_mode", True)
-        model = search_space.rebuild_model(sampled_config, is_eval_mode)
+    #     is_eval_mode = self.config.get("eval_mode", True)
+    #     model = search_space.rebuild_model(sampled_config, is_eval_mode)
 
-        software_metrics = self.compute_software_metrics(
-            model, sampled_config, is_eval_mode
-        )
-        hardware_metrics = self.compute_hardware_metrics(
-            model, sampled_config, is_eval_mode
-        )
-        metrics = software_metrics | hardware_metrics
-        scaled_metrics = {}
-        for metric_name in self.metric_names:
-            scaled_metrics[metric_name] = (
-                self.config["metrics"][metric_name]["scale"] * metrics[metric_name]
-            )
+    #     software_metrics = self.compute_software_metrics(
+    #         model, sampled_config, is_eval_mode
+    #     )
+    #     hardware_metrics = self.compute_hardware_metrics(
+    #         model, sampled_config, is_eval_mode
+    #     )
+    #     metrics = software_metrics | hardware_metrics
+    #     scaled_metrics = {}
+    #     for metric_name in self.metric_names:
+    #         scaled_metrics[metric_name] = (
+    #             self.config["metrics"][metric_name]["scale"] * metrics[metric_name]
+    #         )
 
-        trial.set_user_attr("software_metrics", software_metrics)
-        trial.set_user_attr("hardware_metrics", hardware_metrics)
-        trial.set_user_attr("scaled_metrics", scaled_metrics)
-        trial.set_user_attr("sampled_config", sampled_config)
+    #     trial.set_user_attr("software_metrics", software_metrics)
+    #     trial.set_user_attr("hardware_metrics", hardware_metrics)
+    #     trial.set_user_attr("scaled_metrics", scaled_metrics)
+    #     trial.set_user_attr("sampled_config", sampled_config)
 
-        self.visualizer.log_metrics(metrics=scaled_metrics, step=trial.number)
+    #     self.visualizer.log_metrics(metrics=scaled_metrics, step=trial.number)
 
-        if not self.sum_scaled_metrics:
-            return list(scaled_metrics.values())
-        else:
-            return sum(scaled_metrics.values())
+    #     if not self.sum_scaled_metrics:
+    #         return list(scaled_metrics.values())
+    #     else:
+    #         return sum(scaled_metrics.values())
 
     def search(self, search_space) -> optuna.study.Study:
         print("search_space:  ", search_space)
@@ -127,33 +138,35 @@ class SearchStrategyNaslib(SearchStrategyBase):
         study_kwargs = {
             "sampler": self.sampler_map(self.config["setup"]["sampler"]),
         }
-        if not self.sum_scaled_metrics:
-            study_kwargs["directions"] = self.directions
-        else:
-            study_kwargs["direction"] = self.direction
+        # if not self.sum_scaled_metrics:
+        #     study_kwargs["directions"] = self.directions
+        # else:
+        #     study_kwargs["direction"] = self.direction
 
-        if isinstance(self.config["setup"].get("pkl_ckpt", None), str):
-            study = joblib.load(self.config["setup"]["pkl_ckpt"])
-            logger.info(f"Loaded study from {self.config['setup']['pkl_ckpt']}")
-        else:
-            study = optuna.create_study(**study_kwargs)
+        # if isinstance(self.config["setup"].get("pkl_ckpt", None), str):
+        #     study = joblib.load(self.config["setup"]["pkl_ckpt"])
+        #     logger.info(f"Loaded study from {self.config['setup']['pkl_ckpt']}")
+        # else:
+        
+        study = optuna.create_study(**study_kwargs)
 
-        study.optimize(
-            func=partial(self.objective, search_space=search_space),
-            n_jobs=self.config["setup"]["n_jobs"],
-            n_trials=self.config["setup"]["n_trials"],
-            timeout=self.config["setup"]["timeout"],
-            callbacks=[
-                partial(
-                    callback_save_study,
-                    save_dir=self.save_dir,
-                    save_every_n_trials=self.config["setup"].get(
-                        "save_every_n_trials", 10
-                    ),
-                )
-            ],
-            show_progress_bar=True,
-        )
+        # study.optimize(
+        #     func=partial(self.objective, search_space=search_space),
+        #     n_jobs=self.config["setup"]["n_jobs"],
+        #     n_trials=self.config["setup"]["n_trials"],
+        #     timeout=self.config["setup"]["timeout"],
+        #     callbacks=[
+        #         partial(
+        #             callback_save_study,
+        #             save_dir=self.save_dir,
+        #             save_every_n_trials=self.config["setup"].get(
+        #                 "save_every_n_trials", 10
+        #             ),
+        #         )
+        #     ],
+        #     show_progress_bar=True,
+        # )
+
         self._save_study(study, self.save_dir / "study.pkl")
         self._save_search_dataframe(study, search_space, self.save_dir / "log.json")
         # self._save_best(study, self.save_dir / "best.json")
@@ -232,78 +245,6 @@ class SearchStrategyNaslib(SearchStrategyBase):
             :, ["result_dict"]
         ] = df_truncated.loc[
             :, ["result_dict"]
-        ].map(
-            beautify_metric
-        )
-        txt += tabulate(
-            df_truncated,
-            headers="keys",
-            tablefmt="orgtbl",
-        )
-        logger.info(f"Best trial(s):\n{txt}")
-        return df
-
-    @staticmethod
-    def _save_best(study: optuna.study.Study, save_path):
-        df = pd.DataFrame(
-            columns=[
-                "number",
-                "value",
-                "software_metrics",
-                "hardware_metrics",
-                "scaled_metrics",
-                "sampled_config",
-            ]
-        )
-        if study._is_multi_objective:
-            best_trials = study.best_trials
-            for trial in best_trials:
-                row = [
-                    trial.number,
-                    trial.values,
-                    trial.user_attrs["software_metrics"],
-                    trial.user_attrs["hardware_metrics"],
-                    trial.user_attrs["scaled_metrics"],
-                    trial.user_attrs["sampled_config"],
-                ]
-                df.loc[len(df)] = row
-        else:
-            best_trial = study.best_trial
-            row = [
-                best_trial.number,
-                best_trial.value,
-                best_trial.user_attrs["software_metrics"],
-                best_trial.user_attrs["hardware_metrics"],
-                best_trial.user_attrs["scaled_metrics"],
-                best_trial.user_attrs["sampled_config"],
-            ]
-            df.loc[len(df)] = row
-        df.to_json(save_path, orient="index", indent=4)
-
-        txt = "Best trial(s):\n"
-        print("df:  ", df)
-        df_truncated = df.loc[
-            :, ["number", "software_metrics", "hardware_metrics", "scaled_metrics"]
-        ]
-
-        def beautify_metric(metric: dict):
-            beautified = {}
-            for k, v in metric.items():
-                if isinstance(v, (float, int)):
-                    beautified[k] = round(v, 3)
-                else:
-                    txt = str(v)
-                    if len(txt) > 20:
-                        txt = txt[:20] + "..."
-                    else:
-                        txt = txt[:20]
-                    beautified[k] = txt
-            return beautified
-
-        df_truncated.loc[
-            :, ["software_metrics", "hardware_metrics", "scaled_metrics"]
-        ] = df_truncated.loc[
-            :, ["software_metrics", "hardware_metrics", "scaled_metrics"]
         ].map(
             beautify_metric
         )
