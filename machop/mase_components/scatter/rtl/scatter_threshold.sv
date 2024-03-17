7f0b2c3f976c9fb67fa3f1ddb877c423c04628fc
alexfrater <whitealez@hotmail.com>
2024-03-17 10:36:05 +0000

    scatter threshold commit - 13 march

--- machop/mase_components/scatter/rtl/scatter_threshold.sv
+++ machop/mase_components/scatter/rtl/scatter_threshold.sv
@@ -3,7 +3,7 @@
 
 module scatter_threshold #(
     parameter DATA_IN_0_PRECISION_0 = 4,
-    
+
     // parameter DATA_IN_0_PRECISION_1 = 3,
     parameter DATA_IN_0_TENSOR_SIZE_DIM_0 = 4,
     // parameter DATA_IN_0_PRECISION_0 = 32,
@@ -16,20 +16,12 @@ module scatter_threshold #(
     // parameter DATA_OUT_0_PARALLELISM_DIM_0 = 16
     // parameter DATA_OUT_0_PARALLELISM_DIM_1 = 1,
 ) (
-    input clk,
-    input rst,
     input  logic signed [DATA_IN_0_PRECISION_0-1:0] data_in[DATA_IN_0_TENSOR_SIZE_DIM_0-1:0],
     output  [DATA_OUT_0_PRECISION_0-1:0] data_out[DATA_IN_0_TENSOR_SIZE_DIM_0-1:0],
     output [DATA_IN_0_PRECISION_0-1:0] o_high_precision [DATA_IN_0_TENSOR_SIZE_DIM_0-1:0], 
     output [DATA_IN_0_PRECISION_0-1:0] o_low_precision [DATA_IN_0_TENSOR_SIZE_DIM_0-1:0] 
 );
 
-
-
-
-    //Logic to indentify values larger than x (2MSB check) //Can parameterise
-    // logic [DATA_IN_0_PRECISION_0-2:0] mantisssa [DATA_IN_0_TENSOR_SIZE_DIM_0-1:0];
-    // logic [DATA_IN_0_TENSOR_SIZE_DIM_0-1:0] sign;
 
     logic [DATA_IN_0_TENSOR_SIZE_DIM_0-1:0] high_precision_req_vec;
     for (genvar i = 0; i < DATA_IN_0_TENSOR_SIZE_DIM_0; i = i + 1) begin
@@ -39,11 +31,10 @@ module scatter_threshold #(
     end
 
 
-    
     //Logic to assign indicies based on priority
+
     //Pack array first
     wire [3:0] output_mask;
-
     logic [$clog2(DATA_IN_0_TENSOR_SIZE_DIM_0)-1:0] address_outliers[HIGH_SLOTS-1:0];
     priority_encoder #(
         .NUM_INPUT_CHANNELS(DATA_IN_0_TENSOR_SIZE_DIM_0),
@@ -55,20 +46,6 @@ module scatter_threshold #(
         // .output_channels(address_outliers)
         .mask(output_mask)
     );
-
-    //Logic to turn indicies to mask
-
-    // index_to_mask #(
-    //     .NUM_INPUT_CHANNELS(DATA_IN_0_TENSOR_SIZE_DIM_0),
-    //     .NUM_OUPUT_CHANNELS(DATA_IN_0_TENSOR_SIZE_DIM_0),
-    //     .NO_INDICIES(HIGH_SLOTS),
-    //     .OUTPUT_WIDTH(DATA_IN_0_TENSOR_SIZE_DIM_0)
-    // )mask_gen(
-    //     .indicies(address_outliers),
-    //     .output_mask(output_mask)
-    // );
-
-
 
     //Logic to apply mask
     array_zero_mask#(
