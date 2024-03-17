690ddaf51a053181eb60b939988f39a1567ec4d5
Thomas <thomas.koc92@gmail.com>
2024-03-17 17:13:35 +0000

    temporary commit

--- machop/mase_components/llmint/rtl/LLMint.sv
+++ machop/mase_components/llmint/rtl/LLMint.sv
@@ -8,35 +8,49 @@ module LLMint #(
     parameter HIGH_SLOTS = 2,
     parameter THRESHOLD = 6
 ) (
-    input [ORIGINAL_PRECISION-1:0] data_in[TENSOR_SIZE_DIM-1:0],
-    output logic [ORIGINAL_PRECISION-1:0] data_out[TENSOR_SIZE_DIM-1:0]
+    input logic signed [ORIGINAL_PRECISION-1:0] data_in[TENSOR_SIZE_DIM-1:0],
+    output logic signed [ORIGINAL_PRECISION-1:0] data_out[TENSOR_SIZE_DIM-1:0]
 );
+
+    wire [TENSOR_SIZE_DIM-1:0] low_precision_masked;
+    wire [TENSOR_SIZE_DIM-1:0] high_precision_masked;
+    wire [TENSOR_SIZE_DIM-1:0] output_linear_low_precision;
+    wire [TENSOR_SIZE_DIM-1:0] output_linear_high_precision;
 
     scatter_threshold#(
         .PRECISION(ORIGINAL_PRECISION),
         .TENSOR_SIZE_DIM(TENSOR_SIZE_DIM),
         .HIGH_SLOTS(HIGH_SLOTS),
         .THRESHOLD(THRESHOLD)
-    )masker(
+    )scatter(
         .data_in(data_in),
-        .o_high_precision(),
-        .o_low_precision()
+        .o_high_precision(high_precision_masked),
+        .o_low_precision(low_precision_masked)
     );
 
-    linear#(
+    fixed_linear#(
         .PRECISION(ORIGINAL_PRECISION),
         .REDUCED_PRECISION(REDUCED_PRECISION_0),
         .TENSOR_SIZE_DIM(TENSOR_SIZE_DIM)
-    )reducer(
-        .data_in(data_in),
-        .data_out(data_out)
+    )low_precision_linear(
+        .data_in(),
+        .data_out_0(output_linear_low_precision)
+    );
+
+    fixed_linear#(
+        .PRECISION(ORIGINAL_PRECISION),
+        .REDUCED_PRECISION(REDUCED_PRECISION_0),
+        .TENSOR_SIZE_DIM(TENSOR_SIZE_DIM)
+    )high_precision_linear(
+        .data_in(),
+        .data_out_0(output_linear_high_precision)
     );
 
 
     gather#(
         .PRECISION(ORIGINAL_PRECISION),
         .TENSOR_SIZE_DIM(TENSOR_SIZE_DIM),
-    )masker(
+    )gather(
         .mat_a(),
         .mat_b(),
         .mat_sum(data_out) 
