1e71b79ab7871eb8581a1940a5eff878010bcf49
Miguel <m.pl.braganca@gmail.com>
2024-03-26 14:14:53 +0000

    corrected paralellization

--- machop/chop/actions/search/strategies/rl/core_algorithm.py
+++ machop/chop/actions/search/strategies/rl/core_algorithm.py
@@ -1,88 +1,80 @@
 from ..base import SearchStrategyBase
-from .env import env_map
+from .env import env_map, registered_env_map
 from pprint import pprint
 from stable_baselines3 import A2C, PPO
+from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.callbacks import (
     CallbackList,
     CheckpointCallback,
     EvalCallback,
 )
-from stable_baselines3.common.vec_env import SubprocVecEnv, DummyVecEnv
 
 algorithm_map = {
     "ppo": PPO,
     "a2c": A2C,
 }
-
 class StrategyRL(SearchStrategyBase):
     iterative = True
-
     def _post_init_setup(self):
         self.algorithm_name = self.config["algorithm"]
         self.algorithm = algorithm_map[self.algorithm_name]
         self.total_timesteps = self.config["total_timesteps"]
         self.save_name = self.config["save_name"]
+        self.env = env_map[self.config["env"]]
         self.device = self.config["device"]
-        self.episode_max_len = 10  # This might be adjusted based on trials
-        
-    def make_env(self, env_id, rank, seed=0):
-        def _init():
-            env = env_map[env_id](config=self.config, search_space=self.search_space, sw_runner=self.sw_runner, hw_runner=self.hw_runner, data_module=self.data_module, episode_max_len=self.episode_max_len)
-            env.seed(seed + rank)
-            return env
-        return _init
+        self.episode_max_len = 10  # TODO: Try and change this
+        self.registered_env_name = registered_env_map[self.config["env"]]
 
     def search(self, search_space):
-        self.search_space = search_space  # Storing search_space for use in make_env
-        
+        # Check if load_name is specified in config and load the model if so
         if 'load_name' in self.config:
-            # Assuming that loading the model correctly handles a vectorized environment
-            model = self.algorithm.load(self.config['load_name'])
+            model = self.algorithm.load(self.config['load_name'], env=self.env(config=self.config, search_space=search_space, sw_runner=self.sw_runner, hw_runner=self.hw_runner, data_module=self.data_module, episode_max_len=self.episode_max_len))
             print(f"Model loaded from {self.config['load_name']}. Skipping training.")
         else:
-            num_envs = 4  # Define the number of environments to run in parallel
-            env_id = self.config["env"]
-            #envs = SubprocVecEnv([self.make_env(env_id, i) for i in range(num_envs)])
-            envs = envs = SubprocVecEnv([self.make_env(env_id, i) for i in range(num_envs)], start_method='spawn')
-            
+            #env =self.env(config=self.config, search_space=search_space, sw_runner=self.sw_runner, hw_runner=self.hw_runner, data_module=self.data_module, episode_max_len=self.episode_max_len)
+            env = make_vec_env(
+                self.registered_env_name, n_envs=256, seed=0, 
+                env_kwargs={"config": self.config, "search_space": search_space , 
+                "sw_runner": self.sw_runner, "hw_runner": self.hw_runner, 
+                "data_module": self.data_module, "episode_max_len":self.episode_max_len}
+                )
+
             checkpoint_callback = CheckpointCallback(save_freq=1000, save_path="./logs/")
             eval_callback = EvalCallback(
-                envs,
+                env,
                 best_model_save_path="./logs/best_model",
                 log_path="./logs/results",
                 eval_freq=100,
             )
             callback = CallbackList([checkpoint_callback, eval_callback])
-            
+
             model = self.algorithm(
                 "MultiInputPolicy",
-                envs,
+                env,
                 verbose=1,
                 device=self.device,
                 tensorboard_log="./logs/",
-                n_steps=256,  # This might be adjusted based on trials
+                n_steps=64,  # TODO: Try and change this
             )
-            
+
+            vec_env = model.get_env()
             model.learn(
                 total_timesteps=int(self.total_timesteps),
                 progress_bar=True,
                 callback=callback,
             )
-            
+
+            # Save the model after training
             model.save(self.save_name)
             print(f"Model trained and saved as {self.save_name}.")
 
-        # Evaluating the model using the same vectorized environment
+        # Post-training or post-loading actions
         vec_env = model.get_env()
         obs = vec_env.reset()
         for _ in range(self.episode_max_len):
-            action, _states = model.predict(obs, deterministic=True)
-            obs, rewards, dones, infos = vec_env.step(action)
-        
-        # Assuming the observation has a "cost" field for the purpose of this example
-        costs = [ob["cost"] for ob in obs]  # You might need to adjust this based on your environment's observations
+            action, _state = model.predict(obs, deterministic=True)
+            obs, reward, done, info = vec_env.step(action)
+
         pprint(obs)
-        
-        # Return average cost and the last observations and model as an example
-        avg_cost = sum(costs) / len(costs)
-        return avg_cost, obs, model
+
+        return obs["cost"], obs, model
--- machop/chop/actions/search/strategies/rl/env.py
+++ machop/chop/actions/search/strategies/rl/env.py
@@ -3,106 +3,10 @@ import copy
 import torch
 import numpy as np
 import math
-from pprint import pprint 
+from pprint import pprint
+import random
 from gymnasium.spaces import Box, Dict, Discrete, MultiBinary, MultiDiscrete
 from stable_baselines3.common.callbacks import BaseCallback
-
-
-################################################ LEGACY CODE - NOT FOR USE ##############################################
-class LLMMixedPrecisionEnv(gym.Env): 
-    def __init__(self, config):
-        # Make the space (for actions and observations) configurable.
-        # Since actions should repeat observations, their spaces must be the
-        # same.
-        search_space = config.get("search_space", None)
-        runner = config.get("runner", None)
-        self.search_space, self.runner = search_space, runner
-        if search_space is None:
-            raise ValueError(f"search_space cannot be None")
-
-        indexes, self.config_seed = search_space.build_opt_seed_and_indexes()
-
-        # observation space definition
-        self.observation_space = Dict({})
-        for k, v in search_space.index_ranges.items():
-            self.observation_space[k] = Discrete(v)
-        per_layer_space = search_space.per_layer_search_space
-        flattend_per_layer_space = search_space.transform_nested_dict_to_flat_dict(
-            per_layer_space
-        )
-        my_list = list(flattend_per_layer_space.values())
-
-        # action space definition
-        self.action_space = MultiDiscrete((search_space.get_num_layers(), *my_list))
-
-        self.cur_obs = None
-        self.episode_len = 0
-
-    def sample(self, indexes, quant_config_seed, search_space):
-        sampled_config = search_space.config_sampler(
-            indexes=indexes, config_seed=quant_config_seed
-        )
-        sampled_config = search_space.config_parser(
-            sampled_config, search_space.get_num_layers()
-        )
-        model = search_space.rebuild_model(sampled_config)
-        return self.compute_software_metric(model)
-
-    def build_sample_based_on_action(self, action, indexes):
-        layer_id = action[0]
-        layer_config = action[1:]
-
-        layer_index = copy.deepcopy(indexes[f"model_layer_{layer_id}"])
-        flat_layer = self.search_space.transform_nested_dict_to_flat_dict(layer_index)
-        i = 0
-        for k, v in flat_layer.items():
-            flat_layer[k] = layer_config[i]
-            i += 1
-        nested_layer = self.search_space.transform_flat_dict_to_nested_dict(flat_layer)
-        indexes[f"model_layer_{layer_id}"] = nested_layer
-        return indexes
-
-    def compute_software_metric(self, model):
-        loss, _ = self.runner(model)
-        cost = 10 # TODO: fix
-        return loss + cost["avg_bit"]
-
-    def reset(self, *, seed=None, options=None):
-        """Resets the episode and returns the initial observation of the new one."""
-        # Reset the episode len.
-        self.episode_len = 0
-        # Sample a random number from our observation space.
-        self.cur_obs = self.observation_space.sample()
-        # Return initial observation.
-        return self.cur_obs, {}
-
-    def step(self, action):
-        """Takes a single step in the episode given `action`
-
-        Returns:
-            New observation, reward, done-flag, info-dict (empty).
-        """
-        # loss = self.cur_obs.pop('loss', None)
-        cur_obs = self.cur_obs
-        obs = self.search_space.transform_flat_dict_to_nested_dict(cur_obs)
-        indexes = self.build_sample_based_on_action(action, obs)
-        metric = self.sample(indexes, self.config_seed, self.search_space)
-        metric = metric.cpu().detach().numpy()
-
-        # Set `truncated` flag after 10 steps.
-        self.episode_len += 1
-        terminated = False
-        truncated = self.episode_len >= 10
-
-        # TODO: this reward needs redesign, apparently
-        reward = -metric
-        indexes_flat = self.search_space.transform_nested_dict_to_flat_dict(indexes)
-        # indexes_flat['loss'] = metric
-        return indexes_flat, reward, terminated, truncated, {"loss": metric}
-
-##############################################################################################################################
-
-
 
 # TODO:
 # 1 - Discuss the following design decisions:
@@ -118,7 +22,6 @@ class LLMMixedPrecisionEnv(gym.Env):
 # Meeting:
 # - Plot accuracy for same bitwidth
     
-
 class MixedPrecisionEnv(gym.Env):
     def __init__(self, config, search_space, sw_runner, hw_runner, data_module, episode_max_len):
         if search_space is None:
@@ -138,25 +41,13 @@ class MixedPrecisionEnv(gym.Env):
             metric: (-1 if self.config["metrics"][metric]["direction"] == "maximize" else 1)
             for metric in self.metric_names
         }
-
         self._define_observation_space()
         self._define_action_space()
         self.cur_obs = None
         self.episode_len = 0
         self.episode_max_len = episode_max_len
 
-        self.seed()
 
-    def seed(self, seed=None):
-        if seed is None or seed < 0 or seed >= 2**32:
-            seed = np.random.randint(0, 2**32)
-        
-        self.rng, seed = gym.utils.seeding.np_random(seed)
-        np.random.seed(seed)
-        torch.manual_seed(seed)
-        
-        return [seed]
-    
     def _define_observation_space(self):
         """Defines the observation space based on the search space."""
         self.observation_space = Dict({
@@ -174,21 +65,29 @@ class MixedPrecisionEnv(gym.Env):
         # Create a MultiDiscrete space with these options
         self.action_space = MultiDiscrete(num_options_per_key)
     
+    def seed(self, seed=None):
+        self.np_random, seed = gym.utils.seeding.np_random(seed)
+        # Use self.np_random instead of np.random for random number generation within the environment
+        return [seed]
+
     def reset(self, *, seed=None, options=None):
         """Resets the episode and returns the initial observation of the new one."""
+        if seed is None:
+            seed = self.seed()
+        else:
+            self.seed(seed)
+
         self.episode_len = 0
         self.cur_obs = self.observation_space.sample()
         return self.cur_obs, {}
-
+    
     def step(self, action):
         # Map action to model configuration changes
         flattened_sampled_indexes = self._action_to_config(action)
         sampled_config = self.search_space.flattened_indexes_to_config(flattened_sampled_indexes)
         model = self.search_space.rebuild_model(sampled_config)
-
         software_metrics = self.compute_software_metrics(model, sampled_config)
         hardware_metrics = self.compute_hardware_metrics(model, sampled_config)
-
         metrics = software_metrics | hardware_metrics
         scaled_metrics = {}
         cost = 0
@@ -197,15 +96,13 @@ class MixedPrecisionEnv(gym.Env):
             scaled_metric_value = self.config["metrics"][metric_name]["scale"] * metrics[metric_name] * self.direction_multipliers[metric_name]
             scaled_metrics[metric_name] = scaled_metric_value
             cost += scaled_metric_value
-
         self.cur_obs['accuracy'] = np.array([software_metrics['accuracy']], dtype=np.float32)
         self.cur_obs['average_bitwidth'] = np.array([hardware_metrics['average_bitwidth']], dtype=np.float32)
         self.cur_obs['cost'] = np.array([cost], dtype=np.float32)
-        self.cur_obs.update(sampled_config)
+        self.cur_obs.update(flattened_sampled_indexes)
         
         # Adjust reward calculation based on your scenario
         reward = -cost
-
         # Determine if the episode is done
         self.episode_len += 1
         done = self.episode_len >= self.episode_max_len
@@ -217,9 +114,8 @@ class MixedPrecisionEnv(gym.Env):
             print(f'reward: {reward}')
             pprint(info)
             print("\n")
-
         return self.cur_obs, reward, done, truncated, info
-
+    
 
     def _action_to_config(self, action):
         config = {}
@@ -230,7 +126,7 @@ class MixedPrecisionEnv(gym.Env):
             config[key] = choice_idx
             action_idx += 1
         return config
-
+    
     def compute_software_metrics(self, model, sampled_config: dict, is_eval_mode: bool = True): # TODO: these functions should be moved to the runner
         # note that model can be mase_graph or nn.Module
         metrics = {}
@@ -242,7 +138,7 @@ class MixedPrecisionEnv(gym.Env):
             for runner in self.sw_runner:
                 metrics |= runner(self.data_module, model, sampled_config)
         return metrics
-
+    
     def compute_hardware_metrics(self, model, sampled_config, is_eval_mode: bool = True):
         metrics = {}
         if is_eval_mode:
@@ -253,8 +149,7 @@ class MixedPrecisionEnv(gym.Env):
             for runner in self.hw_runner:
                 metrics |= runner(self.data_module, model, sampled_config)
         return metrics
-
-
+    
 
 class MixedPrecisionEnvHiLo(gym.Env):
     def __init__(self, config, search_space, sw_runner, hw_runner, data_module, episode_max_len):
@@ -296,21 +191,29 @@ class MixedPrecisionEnvHiLo(gym.Env):
         num_actions = len(self.search_space.choices_flattened)
         # Each action can be -1, 0, or 1
         self.action_space = MultiDiscrete([3] * num_actions)
+
+    def seed(self, seed=None):
+        self.np_random, seed = gym.utils.seeding.np_random(seed)
+        # Use self.np_random instead of np.random for random number generation within the environment
+        return [seed]
     
     def reset(self, *, seed=None, options=None):
+        if seed is None:
+            seed = self.seed()
+        else:
+            self.seed(seed)
+
         self.episode_len = 0
         self.cur_obs = self.observation_space.sample()
         # Reset model configuration to the lowest precision settings
         #self.model_config = {key: 0 for key in self.search_space.choices_flattened.keys()}
-
         # Another option is init on median values: 
         # Initialize model configuration to the middle precision setting for each parameter
         self.model_config = {
         key: len(options) // 2 for key, options in self.search_space.choices_flattened.items()
     }
-
         return self.cur_obs, {}
-
+    
     def step(self, action):
         # Update model configuration based on action
         self._action_to_config(action)
@@ -319,7 +222,6 @@ class MixedPrecisionEnvHiLo(gym.Env):
     
         software_metrics = self.compute_software_metrics(model, sampled_config)
         hardware_metrics = self.compute_hardware_metrics(model, sampled_config)
-
         metrics = software_metrics | hardware_metrics
         scaled_metrics = {}
         cost = 0
@@ -329,15 +231,13 @@ class MixedPrecisionEnvHiLo(gym.Env):
             scaled_metrics[metric_name] = scaled_metric_value
             cost += scaled_metric_value
         
-
         self.cur_obs['accuracy'] = np.array([software_metrics['accuracy']], dtype=np.float32)
         self.cur_obs['average_bitwidth'] = np.array([hardware_metrics['average_bitwidth']], dtype=np.float32)
         self.cur_obs['cost'] = np.array([cost], dtype=np.float32)
-        self.cur_obs.update(sampled_config)
+        self.cur_obs.update(self.model_config)
         
         # Adjust reward calculation based on your scenario
         reward = -cost
-
         # Determine if the episode is done
         self.episode_len += 1
         done = self.episode_len >= self.episode_max_len
@@ -345,13 +245,12 @@ class MixedPrecisionEnvHiLo(gym.Env):
         
         # Optional additional info about the step
         info = {"loss": software_metrics['loss'], "average_bitwidth": hardware_metrics['average_bitwidth'], "accuracy": software_metrics['accuracy'], "memory density": hardware_metrics['memory_density']}
-
         if done:
             print(f'reward: {reward}')
             pprint(info)
             print("\n")
         return self.cur_obs, reward, done, truncated, info
-
+    
     def _action_to_config(self, action):
         action_idx = 0
         for key, choices in self.search_space.choices_flattened.items():
@@ -372,7 +271,7 @@ class MixedPrecisionEnvHiLo(gym.Env):
             for runner in self.sw_runner:
                 metrics |= runner(self.data_module, model, sampled_config)
         return metrics
-
+    
     def compute_hardware_metrics(self, model, sampled_config, is_eval_mode: bool = True):
         metrics = {}
         if is_eval_mode:
@@ -384,9 +283,30 @@ class MixedPrecisionEnvHiLo(gym.Env):
                 metrics |= runner(self.data_module, model, sampled_config)
         return metrics
 
+#################### Register the environments ####################
+
+Env_id = 'RL/MixedPrecisionEnv-v0'
+gym.envs.registration.register(
+    id=Env_id,
+    entry_point=MixedPrecisionEnv,
+    max_episode_steps=10,
+    reward_threshold=500
+)
+
+Env_id_Hi_Lo = 'RL/MixedPrecisionEnvHiLo-v0'
+gym.envs.registration.register(
+    id=Env_id_Hi_Lo,
+    entry_point=MixedPrecisionEnvHiLo,
+    max_episode_steps=10,
+    reward_threshold=500
+)
 
 env_map = {
-    "mixed_precision": MixedPrecisionEnv,
+    'mixed_precision': MixedPrecisionEnv,
     'mixed_precision_hi_lo': MixedPrecisionEnvHiLo,
-    #"llm_mixed_precision": LLMMixedPrecisionEnv,
+}
+
+registered_env_map = {
+    'mixed_precision': 'RL/MixedPrecisionEnv-v0',
+    'mixed_precision_hi_lo': "RL/MixedPrecisionEnvHiLo-v0",
 }
