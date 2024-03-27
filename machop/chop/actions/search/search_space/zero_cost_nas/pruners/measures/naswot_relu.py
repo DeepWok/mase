aa089d51602cd71154b7575152b1aa0ccc08835a
zniihgnexy <xz2723@ic.ac.uk>
2024-03-27 13:47:12 +0000

    final mase commit

---
+++ machop/chop/actions/search/search_space/zero_cost_nas/pruners/measures/naswot_relu.py
@@ -0,0 +1,67 @@
+'''
+Author: ViolinSolo
+Date: 2023-04-26 12:23:01
+LastEditTime: 2023-04-28 21:44:16
+LastEditors: ViolinSolo
+Description: original RELU based NASWOT implementation.
+FilePath: /zero-cost-proxies/alethiometer/zero_cost_metrics/naswot_relu.py
+'''
+import torch
+import torch.nn as nn
+import numpy as np
+from . import measure
+
+
+def hooklogdet(K, labels=None):
+    s, ld = np.linalg.slogdet(K)
+    return ld
+
+def safe_hooklogdet(K):
+    s, ld = np.linalg.slogdet(K)
+    return 0 if (np.isneginf(ld) and s==0) else ld
+
+@measure('naswot_relu', bn=True)
+@measure('nwot_relu_Kmats', bn=True, return_Kmats=True)
+def compute_naswot(net, inputs, targets, loss_fn, split_data=1, 
+                   return_Kmats=False):
+    """
+    This is the original RELU based NASWOT implementation.
+    Based on v2 paper, and its repo link: 
+    https://github.com/BayesWatch/nas-without-training/blob/master/score_networks.py
+    """
+    net.eval()
+
+
+    net.K = 0. # **naswot matrix**, NONE-layer-wise | e (mat,) ===> using torch broadcasting tech to init zero-like matrix
+    def counting_forward_hook(module, inp, out):
+        # try:
+        #    if not module.visited_backwards:
+        #        return
+        if isinstance(inp, tuple):
+            inp = inp[0]
+        inp = inp.view(inp.size(0), -1)
+        x = (inp > 0).float()
+        K = x @ x.t()
+        K2 = (1.-x) @ (1.-x.t())
+        net.K = net.K + K.cpu().numpy() + K2.cpu().numpy()  # remove this original code, since we want use broadcasting tech
+        # except:
+        #     pass
+
+
+    def counting_backward_hook(module, inp, out):
+        module.visited_backwards = True
+
+
+    for name, module in net.named_modules():
+        if 'ReLU' in str(type(module)):
+            #hooks[name] = module.register_forward_hook(counting_hook)
+            module.register_forward_hook(counting_forward_hook)
+            # module.register_backward_hook(counting_backward_hook)
+
+
+    with torch.no_grad():
+        net(inputs)
+
+    K_mat = net.K
+    K_mat_logdet = safe_hooklogdet(K_mat)
+    return (K_mat, K_mat_logdet) if return_Kmats else K_mat_logdet
---
+++ machop/chop/actions/search/search_space/zero_cost_nas/pruners/measures/t_cet.py
@@ -0,0 +1,124 @@
+'''
+Author: ViolinSolo
+Date: 2023-04-29 17:33:33
+LastEditTime: 2023-05-05 17:10:54
+LastEditors: ViolinSolo
+Description: t_cet metric
+FilePath: /zero-cost-proxies/alethiometer/zero_cost_metrics/t_cet.py
+'''
+
+import gc
+import numpy as np
+import torch
+
+from . import measure
+from .naswot import compute_naswot
+from .synflow import compute_synflow_per_weight
+from .snip import compute_snip_per_weight
+
+
+@measure('tcet_syn_none', bn=True, mode='none', mt='synflow')  # change bn from Fasle to True, in order to avoid lnwot changes. We will manually remove bn in compute_tcet_score set synflow WITHOUT bn.
+@measure('tcet_syn_log', bn=True, mode='log', mt='synflow')  # change bn from Fasle to True, in order to avoid lnwot changes. We will manually remove bn in compute_tcet_score set synflow WITHOUT bn.
+@measure('tcet_syn_log1p', bn=True, mode='log1p', mt='synflow')  # change bn from Fasle to True, in order to avoid lnwot changes. We will manually remove bn in compute_tcet_score set synflow WITHOUT bn.
+@measure('tcet_syn_norm', bn=True, mode='norm', mt='synflow')  # change bn from Fasle to True, in order to avoid lnwot changes. We will manually remove bn in compute_tcet_score set synflow WITHOUT bn.
+@measure('tcet_snip_none', bn=True, mode='none', mt='snip')
+@measure('tcet_snip_log', bn=True, mode='log', mt='snip')
+@measure('tcet_snip_log1p', bn=True, mode='log1p', mt='snip')
+@measure('tcet_snip_norm', bn=True, mode='norm', mt='snip')
+@measure('t_cet', bn=True, mode='none', mt='synflow')  # change bn from Fasle to True, in order to avoid lnwot changes. We will manually remove bn in compute_tcet_score set synflow WITHOUT bn.
+def compute_tcet_score(net, inputs, targets, loss_fn=None, split_data=1,
+                      mode='none', mt='synflow'):
+    
+    # if mode not in ['none', 'log', 'log1p', 'norm']:
+    #     raise ValueError('mode {} not supported for t_cet'.format(mode))
+    
+    # if mt not in ['synflow', 'snip']:
+    #     raise ValueError('mt {} not supported for t_cet'.format(mt))
+    
+    # print('---------')
+    # print('mt: ', mt, ', mode: ', mode)
+    # print('+++inputs: ',  np.sum(inputs.cpu().numpy()))
+    net1 = net.get_prunable_copy(bn=True)  # manually keep bn in lnwot, and remove bn in synflow
+    # compute layerwise nwot scores
+    lnwot_scores = compute_naswot(net1, inputs, targets, loss_fn, split_data, layerwise=True, return_Kmats=False)
+    # print('lnwot_scores: ', np.sum(lnwot_scores), ", len:", len(lnwot_scores), 'type: ', type(np.sum(lnwot_scores)))
+    # print('lnwot_scores: \n', lnwot_scores)
+    del net1
+    torch.cuda.empty_cache()
+    gc.collect()
+
+    # compute layerwise mt scores
+    # print('---inputs: ', np.sum(inputs.cpu().numpy()))
+    net2 = net.get_prunable_copy(bn=True if mt == 'snip' else False)  # manually keep bn in lnwot, and remove bn in synflow
+    mt_scores = compute_snr_score(net2, inputs, targets, loss_fn, split_data, mode=mode, mt=mt)
+    # print(f'mt_scores: {mt}: ', np.sum(mt_scores), ", len:", len(mt_scores))
+    del net2
+    torch.cuda.empty_cache()
+    gc.collect()
+
+    # compute layerwise t_cet scores
+    tcet_scores = []
+    for lnwot_score, mt_score in zip(lnwot_scores, mt_scores):
+        tcet_scores.append(lnwot_score * mt_score)
+    # print('+++ tcet_scores: ', np.sum(tcet_scores), ", len:", len(tcet_scores))
+    # tcet_scores = [(l*s) for l, s in zip(lnwot_scores, mt_scores)]
+    # print('--- tcet_scores: ', np.sum(tcet_scores), ", len:", len(tcet_scores))
+    return tcet_scores
+
+
+
+@measure('snr_syn_none', bn=False, mode='none', mt='synflow')
+@measure('snr_syn_log', bn=False, mode='log', mt='synflow')
+@measure('snr_syn_log1p', bn=False, mode='log1p', mt='synflow')
+@measure('snr_syn_norm', bn=False, mode='norm', mt='synflow')
+@measure('snr_snip_none', bn=True, mode='none', mt='snip')
+@measure('snr_snip_log', bn=True, mode='log', mt='snip')
+@measure('snr_snip_log1p', bn=True, mode='log1p', mt='snip')
+@measure('snr_snip_norm', bn=True, mode='norm', mt='snip')
+@measure('snr_syn', bn=False, mode='none', mt='synflow')
+@measure('snr_snip', bn=True, mode='none', mt='snip')
+def compute_snr_score(net, inputs, targets, loss_fn=None, split_data=1,
+                      mode='none', mt='synflow'):
+    
+    if mode not in ['none', 'log', 'log1p', 'norm']:
+        raise ValueError('mode {} not supported for t_cet'.format(mode))
+    
+    if mt not in ['synflow', 'snip']:
+        raise ValueError('mt {} not supported for t_cet'.format(mt))
+    
+    # compute layerwise mt scores
+    mt_scores = None
+    if mt == 'synflow':
+        synflow_scores = compute_synflow_per_weight(net, inputs, targets, mode='param', split_data=split_data, loss_fn=loss_fn)
+        mt_scores = synflow_scores
+    elif mt == 'snip':
+        snip_scores = compute_snip_per_weight(net, inputs, targets, mode='param', split_data=split_data, loss_fn=loss_fn)
+        mt_scores = snip_scores
+    
+    # compute snr mt scores
+    snr_mt_scores = []
+    for mt_score in mt_scores:
+        s = mt_score.detach().view(-1)
+
+        sigma = torch.std(s)
+        sigma = torch.tensor(0.) if torch.isnan(sigma) else sigma  ###TODO: Notice, torch.std(torch.tensor([2.2]))==tensor(nan), np.std([2.2])==0.
+        
+        if sigma == 0 or torch.isnan(sigma):
+            s = torch.sum(s)
+        else:
+            s = torch.sum(s)/sigma
+            
+        s_val = s.cpu().item()
+        snr_mt_scores.append(s_val)
+
+    if mode == 'none':
+        return snr_mt_scores
+    elif mode == 'log':
+        log_sc = np.log(snr_mt_scores)
+        return np.nan_to_num(log_sc, nan=0.0, posinf=0.0, neginf=0.0)
+    elif mode == 'log1p':
+        log_sc = np.log1p(snr_mt_scores)
+        return np.nan_to_num(log_sc, nan=0.0, posinf=0.0, neginf=0.0)
+    elif mode == 'norm':
+        _mt_std = np.std(snr_mt_scores)
+        return [s/_mt_std if _mt_std>0 else s for s in snr_mt_scores]
