47f5acd50c43a35defaab99a633ce456519a3686
alexfrater <whitealez@hotmail.com>
2024-03-28 15:16:43 +0000

    Verilog Formatting

--- machop/mase_components/gather/rtl/gather.sv
+++ machop/mase_components/gather/rtl/gather.sv
@@ -5,15 +5,14 @@ module gather #(
     parameter TENSOR_SIZE_DIM = 1,
     parameter PRECISION = 1
 ) (
-    input logic signed [PRECISION-1:0] mat_a[TENSOR_SIZE_DIM-1:0],
-    input logic signed [PRECISION-1:0] mat_b[TENSOR_SIZE_DIM-1:0],
-    output logic signed [PRECISION-1:0] mat_sum[TENSOR_SIZE_DIM-1:0]
+    input logic signed [PRECISION-1:0] mat_a  [TENSOR_SIZE_DIM-1:0],
+    input logic signed [PRECISION-1:0] mat_b  [TENSOR_SIZE_DIM-1:0],
+    output logic signed [PRECISION-1:0] mat_sum [TENSOR_SIZE_DIM-1:0]
 );
 
   always_comb begin
-    
     integer i;
-    for(i=0; i<TENSOR_SIZE_DIM; i=i+1) begin
+    for(i = 0; i < TENSOR_SIZE_DIM; i  = i + 1) begin
       mat_sum[i] = mat_a[i] + mat_b[i];
     end
 
