fee7cd75836c3dd7dc2a00381cee368fc04e7239
Gargi-Sunil <145966255+Gargi-Sunil@users.noreply.github.com>
2024-03-28 17:04:51 +0000

    Update exp.sv

--- machop/mase_components/activations/rtl/exp.sv
+++ machop/mase_components/activations/rtl/exp.sv
@@ -27,32 +27,32 @@ module exp #(
     logic [DATA_IN_0_PRECISION_INT-1:0] a_sat;  //store integer value greater than 15
     logic [3:0] a_precise_1;  //store integer value upto 15
     logic [2:0] a_precise_2;  //store most significant 3 fractional bits
-    logic [DATA_IN_0_PRECISION_1-1:0] a_imprecise;  //store least significant fractional bits other than the MSB 3 bits
+    logic [DATA_IN_0_PRECISION_1-1:0] a_imprecise;  //store LSB fractional bits other than the MSB 3 bits
     logic [LUT_1_PRECISION_0-1:0] exp_precise_1;  //negative exponential of a_precise_1
     logic [LUT_2_PRECISION_0-1:0] exp_precise_2;  //negative exponential of a_precise_2
     logic [IMPRECISE_PRECISION_0-1:0] exp_imprecise;   //negative exponential of a_imprecise
     logic [PDT_WIDTH-1:0] product;  //final product of all part exponentials
 	
     always_comb begin
-        if (DATA_IN_0_PRECISION_INT > 4) begin //there is possibility for saturation 
+	if (DATA_IN_0_PRECISION_INT > 4) begin  //there is possibility for saturation 
             a_sat[DATA_IN_0_PRECISION_INT-4-1:0] = data_in_0[DATA_IN_0_PRECISION_0-1:DATA_IN_0_PRECISION_1+4];
             a_sat[DATA_IN_0_PRECISION_INT-1:DATA_IN_0_PRECISION_INT-4] = 0;
-            if (a_sat > 0) begin //saturation condition. All bits of all parts are assigned to one.
+	    if (a_sat > 0) begin  //saturation condition. All bits of all parts are assigned to one.
                 a_precise_1 = 4'b1111;
-                if (DATA_IN_0_PRECISION_1 > 3) begin//both a_precise_2 and a_imprecise are non-zero and assigned to ones				
+		if (DATA_IN_0_PRECISION_1 > 3) begin  //both a_precise_2 and a_imprecise are non-zero and assigned to ones				
                     a_precise_2 = 3'b111;
 		            a_imprecise[DATA_IN_0_PRECISION_1-3-1:0] = {(DATA_IN_0_PRECISION_1-3){1'b1}};
 		            a_imprecise[DATA_IN_0_PRECISION_1-1:DATA_IN_0_PRECISION_1-3] = 0;
 		        end 
-				else if (DATA_IN_0_PRECISION_1 == 3) begin//a_precise_2 is 3-bits of ones and a_imprecise is zero
+		    	else if (DATA_IN_0_PRECISION_1 == 3) begin  //a_precise_2 is 3-bits of ones and a_imprecise is zero
 		            a_precise_2 = {(DATA_IN_0_PRECISION_1){1'b1}};
 		            a_imprecise = 0;
                 end
-                else if (DATA_IN_0_PRECISION_1 == 0) begin//both a_precise_2 and a_imprecise are zero
+		else if (DATA_IN_0_PRECISION_1 == 0) begin  //both a_precise_2 and a_imprecise are zero
                     a_precise_2 = 0;
                     a_imprecise = 0;
                 end
-                else begin//a_precise_2 is less than 3 bits  and a_imprecise are zero
+                else begin  //a_precise_2 is less than 3 bits  and a_imprecise are zero
                     a_precise_2[2:2-DATA_IN_0_PRECISION_1+1]={(DATA_IN_0_PRECISION_1){1'b1}};
                     a_precise_2[2-DATA_IN_0_PRECISION_1:0]= 0;
                     a_imprecise = 0;
