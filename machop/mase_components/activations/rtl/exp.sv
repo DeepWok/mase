6e49ab01321a977306cc571b9691b2538fa9bc15
Gargi-Sunil <145966255+Gargi-Sunil@users.noreply.github.com>
2024-03-28 18:18:05 +0000

    Update exp.sv

--- machop/mase_components/activations/rtl/exp.sv
+++ machop/mase_components/activations/rtl/exp.sv
@@ -39,19 +39,19 @@ module exp #(
       a_sat[DATA_IN_0_PRECISION_INT-1:DATA_IN_0_PRECISION_INT-4] = 0;
       if (a_sat > 0) begin  //saturation condition. All bits of all parts are assigned to one.
         a_precise_1 = 4'b1111;
-	if (DATA_IN_0_PRECISION_1 > 3) begin  //both a_precise_2 and a_imprecise are non-zero and assigned to ones				
+	  if (DATA_IN_0_PRECISION_1 > 3) begin  //both a_precise_2 and a_imprecise are non-zero and assigned to ones				
           a_precise_2 = 3'b111;
-	  a_imprecise[DATA_IN_0_PRECISION_1-3-1:0] = {(DATA_IN_0_PRECISION_1 - 3) {1'b1}};
-	  a_imprecise[DATA_IN_0_PRECISION_1-1:DATA_IN_0_PRECISION_1-3] = 0;
-	end else if (DATA_IN_0_PRECISION_1 == 3) begin  //a_precise_2 is 3-bits of ones and a_imprecise is zero
-	  a_precise_2 = {(DATA_IN_0_PRECISION_1) {1'b1}};
-	  a_imprecise = 0;
+	    a_imprecise[DATA_IN_0_PRECISION_1-3-1:0] = {(DATA_IN_0_PRECISION_1 - 3) {1'b1}};
+	    a_imprecise[DATA_IN_0_PRECISION_1-1:DATA_IN_0_PRECISION_1-3] = 0;
+	  end else if (DATA_IN_0_PRECISION_1 == 3) begin  //a_precise_2 is 3-bits of ones and a_imprecise is zero
+	    a_precise_2 = {(DATA_IN_0_PRECISION_1) {1'b1}};
+	    a_imprecise = 0;
         end else if (DATA_IN_0_PRECISION_1 == 0) begin  //both a_precise_2 and a_imprecise are zero
           a_precise_2 = 0;
           a_imprecise = 0;
         end else begin  //a_precise_2 is less than 3 bits  and a_imprecise are zero
-	  a_precise_2[2:2-DATA_IN_0_PRECISION_1+1] = {(DATA_IN_0_PRECISION_1) {1'b1}};
-	  a_precise_2[2-DATA_IN_0_PRECISION_1:0] = 0;
+	    a_precise_2[2:2-DATA_IN_0_PRECISION_1+1] = {(DATA_IN_0_PRECISION_1) {1'b1}};
+	    a_precise_2[2-DATA_IN_0_PRECISION_1:0] = 0;
           a_imprecise = 0;
         end 
       end else begin  //unsaturated condition
@@ -86,7 +86,7 @@ module exp #(
         a_precise_2 = 0;
 	  a_imprecise = 0;
       end else begin
-	a_precise_2[2:2-DATA_IN_0_PRECISION_1+1] = data_in_0[DATA_IN_0_PRECISION_1-1:0];
+	  a_precise_2[2:2-DATA_IN_0_PRECISION_1+1] = data_in_0[DATA_IN_0_PRECISION_1-1:0];
         a_precise_2[2-DATA_IN_0_PRECISION_1:0] = 0;
 	a_imprecise = 0;
       end 			
