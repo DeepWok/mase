6bc4716e7490ff47661a75840f2ba84ee6ea9754
Lycho556 <liyichen1215@gmail.com>
2024-03-28 00:31:03 +0000

    black

--- machop/chop/actions/transform.py
+++ machop/chop/actions/transform.py
@@ -33,9 +33,9 @@ from chop.passes.graph.transforms import metadata_value_type_cast_transform_pass
 from chop.passes.module import PASSES as MODULE_PASSES
 
 
-
 logger = logging.getLogger(__name__)
 pp = pprint.PrettyPrinter(indent=4)
+
 
 def pre_transform_load(load_name: str, load_type: str, model: torch.nn.Module):
     if load_name is not None and load_type in ["pt", "pl"]:
@@ -54,15 +54,14 @@ def transform(
     save_dir: str = None,
     load_name: str = None,
     load_type: str = None,
-
-):  
-    model = pre_transform_load(load_name=load_name, load_type=load_type, model=model.to('cuda'))
-    accelerator: str = "auto",
+):
+    model = pre_transform_load(
+        load_name=load_name, load_type=load_type, model=model.to("cuda")
+    )
+    accelerator: str = ("auto",)
     accelerator = parse_accelerator(accelerator)
     model = pre_transform_load(load_name=load_name, load_type=load_type, model=model)
     model.to(accelerator)
-
-
     config = load_config(config)
     transform_config = config["transform"]
     style = transform_config.get("style", "graph")
@@ -91,29 +90,28 @@ def transform(
             load_name=load_name,
             load_type=load_type,
             accelerator=accelerator,
-    )
+        )
     else:
         raise ValueError(f"Style {style} is not supported!")
 
-
     def transform_module(
-    model: torch.nn.Module,
-    model_info,
-    model_name,
-    data_module,
-    task: str,
-    config: str,
-    save_dir: str = None,
-    load_name: str = None,
-    load_type: str = None,
-    accelerator: str = "auto",
-):
+        model: torch.nn.Module,
+        model_info,
+        model_name,
+        data_module,
+        task: str,
+        config: str,
+        save_dir: str = None,
+        load_name: str = None,
+        load_type: str = None,
+        accelerator: str = "auto",
+    ):
         accelerator = parse_accelerator(accelerator)
+
     model = pre_transform_load(load_name=load_name, load_type=load_type, model=model)
     model.to(accelerator)
     save_dir = Path(save_dir)
     save_dir.mkdir(parents=True, exist_ok=True)
-
     if load_name is not None:
         model = load_model(load_name, load_type=load_type, model=model)
         logger.info(f"'{load_type}' checkpoint loaded before training")
@@ -161,13 +159,9 @@ def transform_graph(
     else:
         cf_args = config["cf_args"]
 
-    # graph generation
     graph = MaseGraph(model=model, cf_args=cf_args)
-    # graph_metadata = Mase
     graph, _ = init_metadata_analysis_pass(graph, pass_args=None)
-    # logger.info(f"graph: {graph.fx_graph}")
 
-    # create or load metadata.parameters and mase_graph.model
     if load_name is not None and load_type == "mz":
         graph = load_mase_graph_interface_pass(graph, pass_args=load_name)
     else:
@@ -190,14 +184,6 @@ def transform_graph(
         pass_name: str
         pass_config: dict
         match pass_name:
-            # TODO: fix this later!
-            # case "quantize":
-            #     pass_save_dir = save_dir / "quantize"
-            #     ori_graph = deepcopy_mase_graph(graph)
-            #     graph = PASSES["quantize"](graph, pass_args=pass_config)
-            #     PASSES["summarize_quantization"](
-            #         ori_graph, graph, save_dir=pass_save_dir
-            #     )
             case "profile_statistics":
                 input_generator = InputGenerator(
                     model_info=model_info,
@@ -215,7 +201,6 @@ def transform_graph(
             case "report_node_type":
                 graph = PASSES[pass_name](graph, pass_args=None)
             case "report_node_meta_param":
-                # {"save_path": ..., "which": "all"|["common", "hardware", "software"]}
                 pass_save_path = pass_config.get("save_path", save_dir / "report")
                 pass_config["save_path"] = pass_save_path
                 graph = PASSES[pass_name](graph, pass_args=pass_config)
@@ -246,34 +231,28 @@ def transform_graph(
                 )
                 graph = PASSES[pass_name](graph, pass_args=pass_save_path)
             case "prune":
-                # NOTE: The input generator is only used for when the user wants to
-                # enforce or observe activation sparsity. Otherwise, it's ignored.
-                # We use the validation dataloader as that doesn't shuffle the input
-                # data. This determinism helps establish a fair ground in draw
-                # layer-wise comparisons between activation pruning strategies.
                 input_generator = InputGenerator(
                     model_info=model_info,
                     data_module=data_module,
                     task=task,
                     which_dataloader="val",
                 )
-                dummy_input = get_dummy_input(model_info, data_module, task=task, device='cpu')
+                dummy_input = get_dummy_input(
+                    model_info, data_module, task=task, device="cpu"
+                )
                 pass_config["model_name"] = model_name
                 pass_config["input_generator"] = input_generator
                 pass_config["dummy_in"] = dummy_input
-
-
                 graph = PASSES[pass_name](
                     graph,
                     pass_args=pass_config,
                 )
-                
-                
-                graph.model.to('cuda')
-                graph, sparsity_info = add_pruning_metadata_analysis_pass(graph,
-                                                                               {"dummy_in": dummy_input, "add_value": False})
+                graph.model.to("cuda")
+                graph, sparsity_info = add_pruning_metadata_analysis_pass(
+                    graph, {"dummy_in": dummy_input, "add_value": False}
+                )
                 pp.pprint(sparsity_info)
-                '''
+                """
                 print('Start Encoding')
                 # Apply Huffman Encoding to the pruned weights
                 class HuffmanEncoder:
@@ -327,14 +306,17 @@ def transform_graph(
                         'encoded_weights': encoded_weights
                     }, os.path.join(transformed_ckpt, "state_dict_with_huffman.pt"))
                 print('End Encoding')
-                '''
+                """
 
                 if save_dir is not None:
                     transformed_ckpt = save_dir / f"{pass_name}_ckpt"
                     transformed_ckpt.mkdir(parents=True, exist_ok=True)
-                    torch.save(model.state_dict(), os.path.join(transformed_ckpt, "state_dict.pt"))
+                    torch.save(
+                        model.state_dict(),
+                        os.path.join(transformed_ckpt, "state_dict.pt"),
+                    )
                 return graph
-            
+
             case "retrain":
                 input_generator = InputGenerator(
                     model_info=model_info,
@@ -342,62 +324,84 @@ def transform_graph(
                     task=task,
                     which_dataloader="val",
                 )
-                dummy_input = get_dummy_input(model_info, data_module, task=task, device='cpu')
+                dummy_input = get_dummy_input(
+                    model_info, data_module, task=task, device="cpu"
+                )
 
                 pass_config["model_name"] = model_name
                 pass_config["input_generator"] = input_generator
                 pass_config["dummy_in"] = dummy_input
 
-                max_epoch = pass_config['config']['epoch']
-                batch_size = pass_config['config']['batch_size']
-                lr = pass_config['config']['learning_rate']
+                max_epoch = pass_config["config"]["epoch"]
+                batch_size = pass_config["config"]["batch_size"]
+                lr = pass_config["config"]["learning_rate"]
 
                 module = torch.load(load_name)
 
                 def get_parametrized_layers(module):
                     parametrized_layers = []
                     for name, weights in module.items():
-                        if 'original' in name:
+                        if "original" in name:
                             parametrized_layers.append(weights)
                     return parametrized_layers
 
                 def load_pruned_weights(model, layers, index):
                     for _, layer in model.named_children():
-                        if isinstance(layer, (torch.nn.modules.conv.Conv2d, torch.nn.modules.linear.Linear)):
+                        if isinstance(
+                            layer,
+                            (
+                                torch.nn.modules.conv.Conv2d,
+                                torch.nn.modules.linear.Linear,
+                            ),
+                        ):
                             layer.weight.data.copy_(layers[index[0]])
                             index[0] += 1
                         elif isinstance(layer, torch.nn.Module):
                             load_pruned_weights(layer, layers, index)
                     return model
-                
-                if dataset_info.name == 'cifar10':
-                    transform = transforms.Compose([
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                    ])
-                    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-                    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
+                if dataset_info.name == "cifar10":
+                    transform = transforms.Compose(
+                        [
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                        ]
+                    )
+                    trainset = torchvision.datasets.CIFAR10(
+                        root="./data", train=True, download=True, transform=transform
+                    )
+                    trainloader = torch.utils.data.DataLoader(
+                        trainset, batch_size=batch_size, shuffle=True, num_workers=2
+                    )
 
-                elif dataset_info.name == 'imagenet':
-                    transform = transforms.Compose([
-                        transforms.RandomResizedCrop(224),
-                        transforms.RandomHorizontalFlip(),
-                        transforms.ToTensor(),
-                        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-                    ])
-                    trainset = torchvision.datasets.ImageNet(root='./data', train=True, download=True, transform=transform)
-                    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+                elif dataset_info.name == "imagenet":
+                    transform = transforms.Compose(
+                        [
+                            transforms.RandomResizedCrop(224),
+                            transforms.RandomHorizontalFlip(),
+                            transforms.ToTensor(),
+                            transforms.Normalize(
+                                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                            ),
+                        ]
+                    )
+                    trainset = torchvision.datasets.ImageNet(
+                        root="./data", train=True, download=True, transform=transform
+                    )
+                    trainloader = torch.utils.data.DataLoader(
+                        trainset, batch_size=batch_size, shuffle=True, num_workers=2
+                    )
 
-                ## Add more dataset loading method here
                 else:
                     raise ValueError(
-                        "Dataset {} is not supported for pruning yet".format(dataset_info.name)
+                        "Dataset {} is not supported for pruning yet".format(
+                            dataset_info.name
+                        )
                     )
-                
+
                 criterion = torch.nn.CrossEntropyLoss()
                 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-                
+
                 parametrized_layers = get_parametrized_layers(module)
                 index = [0]
                 pruned_model = load_pruned_weights(model, parametrized_layers, index)
@@ -410,22 +414,27 @@ def transform_graph(
                         inputs, labels = data
                         optimizer.zero_grad()
 
-                        outputs = pruned_model(inputs.to('cuda'))
-                        loss = criterion(outputs, labels.to('cuda'))
+                        outputs = pruned_model(inputs.to("cuda"))
+                        loss = criterion(outputs, labels.to("cuda"))
                         loss.backward()
                         optimizer.step()
 
                         running_loss += loss.item()
                         if i % 100 == 99:  # Print every 100 mini-batches
-                            print('[Num of epoch:%d, %5dth batch] loss: %.3f' %
-                                (epoch + 1, i + 1, running_loss / 100))
+                            print(
+                                "[Num of epoch:%d, %5dth batch] loss: %.3f"
+                                % (epoch + 1, i + 1, running_loss / 100)
+                            )
                             running_loss = 0.0
                 print("Retrain complete")
                 if save_dir is not None:
                     transformed_ckpt = save_dir / f"{pass_name}_ckpt"
                     transformed_ckpt.mkdir(parents=True, exist_ok=True)
-                    torch.save(pruned_model, os.path.join(transformed_ckpt, "retrain_model.ckpt"))
-                
+                    torch.save(
+                        pruned_model,
+                        os.path.join(transformed_ckpt, "retrain_model.ckpt"),
+                    )
+
                 return graph
             case "remove_prune_wrappers":
                 # Removes the pruning-related hooks and makes pruning permanent
