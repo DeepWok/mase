bdcb0929dc263d6be729ea546f3c709a96da3159
Thomas <thomas.koc92@gmail.com>
2024-03-29 00:24:15 +0100

    PR 8

--- machop/mase_cocotb/interfaces/streaming.py
+++ machop/mase_cocotb/interfaces/streaming.py
@@ -51,6 +51,8 @@ class StreamMonitor(Monitor):
         self.check = check
 
     def _trigger(self):
+        print('-----TRIGGER----')
+        print(self.valid.value == 1 and self.ready.value == 1)
         return self.valid.value == 1 and self.ready.value == 1
 
     def _recv(self):
@@ -61,9 +63,7 @@ class StreamMonitor(Monitor):
 
     def _check(self, got, exp):
         if self.check:
-            print('-----fail--------')
-            print("\nGot \n%s, \nExpected \n%s" % (got, exp))
-            # assert np.equal(got, exp).all()
+            assert np.equal(got, exp).all()
             # if not np.equal(got, exp).all():
                 # print('------FAILED---------')
                 # raise TestFailure("\nGot \n%s, \nExpected \n%s" % (got, exp))
