07f53f93a40745d582fd8c49d387d4189de6b0df
Ruiqi-Shen <ruiqi.shen23@imperial.ac.uk>
2024-03-30 08:24:28 +0000

    fix60

--- scripts/test-machop.sh
+++ scripts/test-machop.sh
@@ -7,7 +7,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-The absolute path to the directory of this script.
+# The absolute path to the directory of this script.
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 MASE=$SCRIPT_DIR/..
 
