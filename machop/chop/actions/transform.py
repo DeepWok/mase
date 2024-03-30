2a850366479a51bb9478decafd5f4458ac6c0991
Lycho556 <liyichen1215@gmail.com>
2024-03-30 14:20:36 +0000

    p6

--- machop/chop/actions/transform.py
+++ machop/chop/actions/transform.py
@@ -2,15 +2,13 @@ import os
 from copy import deepcopy
 from pathlib import Path
 import logging
-import pprint
+
 import torch
 from chop.passes.graph import PASSES
 from chop.passes.graph.analysis import (
     add_common_metadata_analysis_pass,
     add_software_metadata_analysis_pass,
     init_metadata_analysis_pass,
-    profile_statistics_analysis_pass,
-    add_pruning_metadata_analysis_pass,
 )
 from chop.ir.graph.mase_graph import MaseGraph
 from chop.passes.graph.interface import (
@@ -21,20 +19,12 @@ from chop.passes.graph.utils import deepcopy_mase_graph
 from chop.tools.checkpoint_load import load_model
 from chop.tools.config_load import load_config
 from chop.tools.get_input import InputGenerator, get_cf_args, get_dummy_input
-from chop.tools.utils import device
-
-from chop.actions import train, test
-import torchvision
-import torchvision.transforms as transforms
-import heapq
-import collections
+from chop.tools.utils import parse_accelerator, to_numpy_if_tensor
 
 from chop.passes.graph.transforms import metadata_value_type_cast_transform_pass
 from chop.passes.module import PASSES as MODULE_PASSES
 
-
 logger = logging.getLogger(__name__)
-pp = pprint.PrettyPrinter(indent=4)
 
 
 def pre_transform_load(load_name: str, load_type: str, model: torch.nn.Module):
@@ -91,24 +81,25 @@ def transform(
     else:
         raise ValueError(f"Style {style} is not supported!")
 
-    def transform_module(
-        model: torch.nn.Module,
-        model_info,
-        model_name,
-        data_module,
-        task: str,
-        config: str,
-        save_dir: str = None,
-        load_name: str = None,
-        load_type: str = None,
-        accelerator: str = "auto",
-    ):
-        accelerator = parse_accelerator(accelerator)
 
+def transform_module(
+    model: torch.nn.Module,
+    model_info,
+    model_name,
+    data_module,
+    task: str,
+    config: str,
+    save_dir: str = None,
+    load_name: str = None,
+    load_type: str = None,
+    accelerator: str = "auto",
+):
+    accelerator = parse_accelerator(accelerator)
     model = pre_transform_load(load_name=load_name, load_type=load_type, model=model)
     model.to(accelerator)
     save_dir = Path(save_dir)
     save_dir.mkdir(parents=True, exist_ok=True)
+
     if load_name is not None:
         model = load_model(load_name, load_type=load_type, model=model)
         logger.info(f"'{load_type}' checkpoint loaded before training")
@@ -142,7 +133,6 @@ def transform_graph(
     config: str,
     save_dir: str = None,
     load_name: str = None,
-    dataset_info=None,
     load_type: str = None,
     accelerator: str = "auto",
 ):
@@ -157,22 +147,26 @@ def transform_graph(
     else:
         cf_args = config["cf_args"]
 
+    # graph generation
     graph = MaseGraph(model=model, cf_args=cf_args)
+    # graph_metadata = Mase
     graph, _ = init_metadata_analysis_pass(graph, pass_args=None)
+    # logger.info(f"graph: {graph.fx_graph}")
 
+    # create or load metadata.parameters and mase_graph.model
     if load_name is not None and load_type == "mz":
-        graph = load_mase_graph_interface_pass(graph, pass_args=load_name)
+        graph, _ = load_mase_graph_interface_pass(graph, pass_args=load_name)
     else:
         dummy_in = get_dummy_input(
             model_info=model_info,
             data_module=data_module,
             task=task,
-            device=device,
+            device=accelerator,
         )
         if len(graph.model.additional_inputs) > 0:
             dummy_in = dummy_in | graph.model.additional_inputs
         graph, _ = add_common_metadata_analysis_pass(
-            graph, pass_args={"dummy_in": dummy_in, "force_device_meta": False}
+            graph, pass_args={"dummy_in": dummy_in}
         )
         graph, _ = add_software_metadata_analysis_pass(graph, pass_args=None)
 
@@ -182,6 +176,16 @@ def transform_graph(
         pass_name: str
         pass_config: dict
         match pass_name:
+            case "quantize":
+                pass_save_dir = save_dir / "quantize"
+                graph, _ = metadata_value_type_cast_transform_pass(
+                    graph, pass_args={"fn": to_numpy_if_tensor}
+                )
+                ori_graph = deepcopy_mase_graph(graph)
+                graph, _ = PASSES["quantize"](graph, pass_args=pass_config)
+                PASSES["summarize_quantization"](
+                    ori_graph, graph, save_dir=pass_save_dir
+                )
             case "profile_statistics":
                 input_generator = InputGenerator(
                     model_info=model_info,
@@ -190,44 +194,45 @@ def transform_graph(
                     which_dataloader="train",
                 )
                 pass_config["input_generator"] = input_generator
-                graph = PASSES[pass_name](graph, pass_args=pass_config)
+                graph, _ = PASSES[pass_name](graph, pass_args=pass_config)
             case "report_graph":
                 pass_file_name = pass_config.get(
                     "file_name", save_dir / "report_graph.txt"
                 )
-                graph = PASSES[pass_name](graph, file_name=pass_file_name)
+                graph, _ = PASSES[pass_name](graph, file_name=pass_file_name)
             case "report_node_type":
-                graph = PASSES[pass_name](graph, pass_args=None)
+                graph, _ = PASSES[pass_name](graph, pass_args=None)
             case "report_node_meta_param":
+                # {"save_path": ..., "which": "all"|["common", "hardware", "software"]}
                 pass_save_path = pass_config.get("save_path", save_dir / "report")
                 pass_config["save_path"] = pass_save_path
-                graph = PASSES[pass_name](graph, pass_args=pass_config)
+                graph, _ = PASSES[pass_name](graph, pass_args=pass_config)
             case "report_node_shape":
-                graph = PASSES[pass_name](graph, pass_args=None)
+                graph, _ = PASSES[pass_name](graph, pass_args=None)
             case "report_node_type":
-                graph = PASSES[pass_name](graph, pass_args=None)
+                graph, _ = PASSES[pass_name](graph, pass_args=None)
             case "report_node_hardware_type":
-                graph = PASSES[pass_name](graph, pass_args=None)
+                graph, _ = PASSES[pass_name](graph, pass_args=None)
             case "report_node_shape":
-                graph = PASSES[pass_name](graph, pass_args=None)
+                graph, _ = PASSES[pass_name](graph, pass_args=None)
             case "report_node_type":
-                graph = PASSES[pass_name](graph, pass_args=None)
+                graph, _ = PASSES[pass_name](graph, pass_args=None)
             case "load_mase_graph":
                 pass_load_dir = pass_config["load_dir"]
-                graph = PASSES[pass_name](graph, pass_args=pass_load_dir)
+                graph, _ = PASSES[pass_name](graph, pass_args=pass_load_dir)
             case "load_node_meta_param":
                 pass_load_path = pass_config["load_path"]
-                graph = PASSES[pass_name](graph, pass_args=pass_load_path)
+                graph, _ = PASSES[pass_name](graph, pass_args=pass_load_path)
             case "save_mase_graph":
                 pass_save_dir = pass_config.get(
                     "save_dir", save_dir / "saved_mase_graph"
                 )
-                graph = PASSES[pass_name](graph, pass_args=pass_save_dir)
+                graph, _ = PASSES[pass_name](graph, pass_args=pass_save_dir)
             case "save_node_meta_param":
                 pass_save_path = pass_config.get(
                     "save_path", save_dir / "saved_node_meta_param"
                 )
-                graph = PASSES[pass_name](graph, pass_args=pass_save_path)
+                graph, _ = PASSES[pass_name](graph, pass_args=pass_save_path)
             case "prune":
                 input_generator = InputGenerator(
                     model_info=model_info,
