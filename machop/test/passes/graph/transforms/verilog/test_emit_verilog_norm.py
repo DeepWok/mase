ac8fced5bce4b6aacbba40008ede1cc8c411f3ff
pgimenes <pgimenes@outlook.com>
2024-04-26 17:32:56 +0200

    missing filepath updates for emit norm

--- machop/test/passes/graph/transforms/verilog/test_emit_verilog_norm.py
+++ machop/test/passes/graph/transforms/verilog/test_emit_verilog_norm.py
@@ -17,6 +17,18 @@ from chop.passes.graph.transforms.quantize.quantizers.quantizers_for_hw import (
 
 import chop.models.manual.rms_norm as rms
 
+import sys, pdb, traceback
+
+
+def excepthook(exc_type, exc_value, exc_traceback):
+    traceback.print_exception(exc_type, exc_value, exc_traceback)
+    print("\nEntering debugger...")
+    pdb.post_mortem(exc_traceback)
+
+
+# Set the custom exception hook
+# sys.excepthook = excepthook
+
 
 def _debug_mase_metadata(mg):
     print("#### GRAPH METADATA ####")
