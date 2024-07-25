439d2b7896c93465623a4dc40b5983430581b095
pgimenes <pgimenes@outlook.com>
2024-07-25 15:54:28 +0100

    Refactor models directory

--- src/chop/models/manual/opt_plain/__init__.py
+++
@@ -1,3 +0,0 @@
-from transformers.models.gpt2.tokenization_gpt2 import GPT2Tokenizer
-from .configuration_opt import OPTConfig
-from .modeling_opt import OPTForCausalLM, OPTForSequenceClassification, OPTModel
