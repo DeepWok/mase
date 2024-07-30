e2fc9bf176c809c8aa2665cd2055b293e7a76711
cx922 <cx922@ic.ac.uk>
2024-07-30 08:47:25 +0000

    finish testing

---
+++ src/mase_components/vision_models/vit/test/fixed_self_attention_head_tb.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python3
+
+import os
+
+import torch
+import logging
+from functools import partial
+
+import cocotb
+from cocotb.log import SimLog
+from cocotb.triggers import Timer, RisingEdge
+from pathlib import Path
+
+from mase_cocotb.testbench import Testbench
+from mase_cocotb.interfaces.streaming import StreamDriver, StreamMonitor
+from mase_cocotb.runner import mase_runner
+
+# from mase_cocotb import Testbench, StreamDriver, StreamMonitor, mase_runner
+from chop.nn.quantized import ViTSelfAttentionHeadInteger
+from chop.nn.quantizers import integer_quantizer
+
+from mase_components.helper import generate_memory
+
+import pytest
+import math
+class FixedSelfAttentionHeadTB(Testbench):
+    def __init__(self, dut) -> None:
+        super().__init__(dut, dut.clk, dut.rst)
+
+        if not hasattr(self, "log"):
+            self.log = SimLog("%s" % (type(self).__qualname__))
+            self.log.setLevel(logging.DEBUG)
+
+        # * QKV drivers
+        self.query_driver = StreamDriver(
+            dut.clk, dut.query, dut.query_valid, dut.query_ready
+        )
+        self.key_driver = StreamDriver(dut.clk, dut.key, dut.key_valid, dut.key_ready)
+        self.value_driver = StreamDriver(
+            dut.clk, dut.value, dut.value_valid, dut.value_ready
+        )
+
+        self.out_monitor = StreamMonitor(
+            dut.clk,
+            dut.out,
+            dut.out_valid,
+            dut.out_ready,
+            check=True,
+        )
+
+        # Model
+        self.head_size = self.get_parameter("IN_DATA_TENSOR_SIZE_DIM_0")
+
+        
+        self.q_config = {
+            "query_width":self.get_parameter("IN_DATA_PRECISION_0"),
+            "query_frac_width":self.get_parameter("IN_DATA_PRECISION_1"),
+            "key_width":self.get_parameter("IN_DATA_PRECISION_0"),
+            "key_frac_width":self.get_parameter("IN_DATA_PRECISION_1"),
+            "value_width":self.get_parameter("IN_DATA_PRECISION_0"),
+            "value_frac_width":self.get_parameter("IN_DATA_PRECISION_1"),
+            "qkmm_out_width":self.get_parameter("QKMM_OUT_PRECISION_0"),
+            "qkmm_out_frac_width":self.get_parameter("QKMM_OUT_PRECISION_1"),
+            "softmax_exp_width":self.get_parameter("SOFTMAX_EXP_PRECISION_0"),
+            "softmax_exp_frac_width":self.get_parameter("SOFTMAX_EXP_PRECISION_1"),
+            "softmax_out_frac_width":self.get_parameter("SOFTMAX_OUT_DATA_PRECISION_1"),
+            "svmm_out_width":self.get_parameter("OUT_DATA_PRECISION_0"),
+            "svmm_out_frac_width":self.get_parameter("OUT_DATA_PRECISION_1"),
+        }
+        self.model = ViTSelfAttentionHeadInteger(
+            dim=self.get_parameter("IN_DATA_TENSOR_SIZE_DIM_0"),
+            num_heads=1,
+            q_config=self.q_config,
+        )
+        # assert self.model.mult_data == torch.tensor(MULT_DATA), f"running set mult data {self.model.mult_data} != {MULT_DATA}"
+        # Set verbosity of driver and monitor loggers to debug
+        self.query_driver.log.setLevel(logging.DEBUG)
+        self.key_driver.log.setLevel(logging.DEBUG)
+        self.value_driver.log.setLevel(logging.DEBUG)
+        self.out_monitor.log.setLevel(logging.DEBUG)
+
+    def generate_inputs(self, seq_len=1):
+        return {
+            "query_layer": torch.randn((seq_len, self.head_size)),
+            "key_layer": torch.randn((seq_len, self.head_size)),
+            "value_layer": torch.randn((seq_len, self.head_size)),
+        }
+
+    def preprocess_tensor(self, tensor, config, parallelism):
+        if len(tensor.shape) == 1:
+            tensor = tensor.unsqueeze(0)
+
+        # Quantize
+        quantizer = partial(integer_quantizer, **config)
+        q_tensor = quantizer(tensor)
+        self.log.debug(f"Quantized tensor: {q_tensor}")
+
+        # Convert to integer format
+        q_tensor = (q_tensor * 2 ** config["frac_width"]).int()
+        self.log.debug(f"Tensor in integer format: {q_tensor}")
+
+        # Split into chunks according to parallelism in each dimension
+        # parallelism[0]: along rows, parallelism[1]: along columns
+        dim_0_split = q_tensor.split(parallelism[0], dim=0)
+        dim_1_split = [x.split(parallelism[1], dim=1) for x in dim_0_split]
+        blocks = []
+        # Flatten the list of blocks
+        for i in range(len(dim_1_split)):
+            for j in range(len(dim_1_split[i])):
+                blocks.append(dim_1_split[i][j].flatten().tolist())
+        return blocks
+
+    async def run_test(self):
+        await self.reset()
+        self.log.info(f"Reset finished")
+        self.out_monitor.ready.value = 1
+        inputs = self.generate_inputs(
+            seq_len=self.get_parameter("IN_DATA_TENSOR_SIZE_DIM_1")
+        )
+        exp_out = self.model(**inputs)
+        # breakpoint()
+        parallelism = [
+            self.get_parameter("IN_DATA_PARALLELISM_DIM_1"),
+            self.get_parameter("IN_DATA_PARALLELISM_DIM_0"),
+        ]
+
+        # * Load the query driver
+        self.log.info(f"Processing query inputs: {inputs['query_layer']}")
+        query_inputs = self.preprocess_tensor(
+            tensor=inputs["query_layer"],
+            config={
+                "width":self.q_config["query_width"],
+                "frac_width":self.q_config["query_frac_width"]
+            },
+            parallelism=parallelism,
+        )
+        self.query_driver.load_driver(query_inputs)
+
+        # * Load the key driver
+        self.log.info(f"Processing key inputs: {inputs['key_layer']}")
+        key_inputs = self.preprocess_tensor(
+            tensor=inputs["key_layer"],
+            config={
+                "width":self.q_config["key_width"],
+                "frac_width":self.q_config["key_frac_width"]
+            },
+            parallelism=parallelism,
+        )
+        self.key_driver.load_driver(key_inputs)
+
+        # * Load the value driver
+        self.log.info(f"Processing value inputs: {inputs['value_layer']}")
+        value_inputs = self.preprocess_tensor(
+            tensor=inputs["value_layer"],
+            config={
+                "width":self.q_config["value_width"],
+                "frac_width":self.q_config["value_frac_width"]
+            },
+            parallelism=parallelism,
+        )
+        self.value_driver.load_driver(value_inputs)
+
+        # * Load the output monitor
+        self.log.info(f"Processing outputs: {exp_out}")
+        outs = self.preprocess_tensor(
+            tensor=exp_out,
+            config={
+                "width": self.get_parameter("OUT_DATA_PRECISION_0"),
+                "frac_width": self.get_parameter("OUT_DATA_PRECISION_1"),
+            },
+            parallelism=[
+                self.get_parameter("OUT_DATA_PARALLELISM_DIM_1"),
+                self.get_parameter("OUT_DATA_PARALLELISM_DIM_0"),
+            ],
+        )
+        self.out_monitor.load_monitor(outs)
+
+        cocotb.start_soon(check_signal(self.dut, self.log))
+        await Timer(1, units="ms")
+        if not self.out_monitor.exp_queue.empty():
+            raise RuntimeError(
+                "Reached the end of the test, but the output monitor is not empty."
+            )
+
+
+@cocotb.test()
+async def cocotb_test(dut):
+    tb = FixedSelfAttentionHeadTB(dut)
+    await tb.run_test()
+
+default_config = {
+        "IN_DATA_TENSOR_SIZE_DIM_0": 128,
+        "IN_DATA_TENSOR_SIZE_DIM_1": 64,
+        "IN_DATA_PARALLELISM_DIM_0": 4,
+        "IN_DATA_PARALLELISM_DIM_1": 2,
+        "IN_DATA_PRECISION_0": 12,
+        "IN_DATA_PRECISION_1": 4,
+        "ACTIVATION": 1,
+        "QKMM_OUT_PRECISION_0": 12,
+        "QKMM_OUT_PRECISION_1": 4,
+        "SOFTMAX_EXP_PRECISION_0": 16,
+        "SOFTMAX_EXP_PRECISION_1": 4,
+
+        "SOFTMAX_OUT_DATA_PRECISION_1": 7,
+
+        "OUT_DATA_PRECISION_0": 12,
+        "OUT_DATA_PRECISION_1": 4,
+    }
+def get_fixed_self_attention_head_config(kwargs={}):
+    config = default_config
+    config.update(kwargs)
+    return config
+
+torch.manual_seed(1)
+async def check_signal(dut, log):
+    while True:
+        await RisingEdge(dut.clk)
+        handshake_signal_check(
+            dut.attention_scores_valid, 
+            dut.attention_scores_ready, 
+            dut.attention_scores, log)
+        # handshake_signal_check(dut.rolled_k_valid, dut.rolled_k_ready, dut.rolled_k, log)
+        # handshake_signal_check(dut.bias_valid,
+        #                        dut.bias_ready,
+        #                        dut.bias, log)
+
+
+def handshake_signal_check(valid, ready, signal, log):
+    svalue = [i.signed_integer for i in signal.value]
+    if valid.value & ready.value:
+        log.debug(f"handshake {signal} = {svalue}")
+
+
+MULT_DATA = 1 / math.sqrt(default_config["IN_DATA_TENSOR_SIZE_DIM_0"])
+@pytest.mark.dev
+def test_fixed_self_attention_head_smoke():
+    """
+    Some quick tests to check if the module is working.
+    """
+
+    # * Generate exponential LUT for softmax
+    generate_memory.generate_sv_lut(
+        "exp",
+        default_config["QKMM_OUT_PRECISION_0"],
+        default_config["QKMM_OUT_PRECISION_1"],
+        default_config["SOFTMAX_EXP_PRECISION_0"],
+        default_config["SOFTMAX_EXP_PRECISION_1"],
+        path=Path(__file__).parents[1] / "rtl",
+        constant_mult=MULT_DATA,
+    )
+    mase_runner(
+        trace=True,
+        module_param_list=[
+            get_fixed_self_attention_head_config(),
+        ],
+        skip_build=False,
+    )
+
+
+if __name__ == "__main__":
+    test_fixed_self_attention_head_smoke()
---
+++ src/mase_components/vision_models/vit/test/fixed_self_attention_tb.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python3
+
+import os
+
+import torch
+import logging
+from functools import partial
+
+import cocotb
+from cocotb.log import SimLog
+from cocotb.triggers import Timer
+
+import math
+
+from mase_components.helper import generate_memory
+from mase_cocotb.testbench import Testbench
+from mase_cocotb.interfaces.streaming import StreamDriver, StreamMonitor
+from mase_cocotb.runner import mase_runner
+
+# from mase_cocotb import Testbench, StreamDriver, StreamMonitor, mase_runner
+from chop.nn.quantized import (
+    ViTAttentionInteger
+)
+from mase_cocotb.utils import fixed_preprocess_tensor
+
+
+class FixedSelfAttentionTB(Testbench):
+    def __init__(self, dut) -> None:
+        super().__init__(dut, dut.clk, dut.rst)
+
+        if not hasattr(self, "log"):
+            self.log = SimLog("%s" % (type(self).__qualname__))
+            self.log.setLevel(logging.DEBUG)
+
+        self.data_in_0_driver = StreamDriver(
+            dut.clk, dut.data_in_0, dut.data_in_0_valid, dut.data_in_0_ready
+        )
+
+        # * Weight drivers
+        self.weight_query_driver = StreamDriver(
+            dut.clk, dut.weight_query, dut.weight_query_valid, dut.weight_query_ready
+        )
+        self.weight_key_driver = StreamDriver(
+            dut.clk, dut.weight_key, dut.weight_key_valid, dut.weight_key_ready
+        )
+        self.weight_value_driver = StreamDriver(
+            dut.clk, dut.weight_value, dut.weight_value_valid, dut.weight_value_ready
+        )
+
+        if self.get_parameter("HAS_BIAS") == 1:
+            self.bias_query_driver = StreamDriver(
+                dut.clk, dut.bias_query, dut.bias_query_valid, dut.bias_query_ready
+            )
+            self.bias_key_driver = StreamDriver(
+                dut.clk, dut.bias_key, dut.bias_key_valid, dut.bias_key_ready
+            )
+            self.bias_value_driver = StreamDriver(
+                dut.clk, dut.bias_value, dut.bias_value_valid, dut.bias_value_ready
+            )
+            self.bias_query_driver.log.setLevel(logging.DEBUG)
+            self.bias_key_driver.log.setLevel(logging.DEBUG)
+            self.bias_value_driver.log.setLevel(logging.DEBUG)
+
+        self.data_out_0_monitor = StreamMonitor(
+            dut.clk,
+            dut.data_out_0,
+            dut.data_out_0_valid,
+            dut.data_out_0_ready,
+            check=True,
+        )
+
+        # Model
+        self.q_config = {
+            "data_in_width":self.get_parameter("DATA_IN_0_PRECISION_0"),
+            "data_in_frac_width":self.get_parameter("DATA_IN_0_PRECISION_1"),
+            "qkv_weight_width":self.get_parameter("WEIGHT_PRECISION_0"),
+            "qkv_weight_frac_width":self.get_parameter("WEIGHT_PRECISION_1"),
+            "qkv_bias_width":self.get_parameter("BIAS_PRECISION_0"),
+            "qkv_bias_frac_width":self.get_parameter("BIAS_PRECISION_1"),
+            "qkv_width":self.get_parameter("DATA_OUT_0_PRECISION_0"),
+            "qkv_frac_width":self.get_parameter("DATA_OUT_0_PRECISION_1"),
+            "qkmm_out_width":self.get_parameter("DATA_OUT_0_PRECISION_0"),
+            "qkmm_out_frac_width":self.get_parameter("DATA_OUT_0_PRECISION_1"),
+            "softmax_exp_width":self.get_parameter("DATA_OUT_0_PRECISION_0"),
+            "softmax_exp_frac_width":self.get_parameter("DATA_OUT_0_PRECISION_1"),
+            "softmax_out_frac_width":self.get_parameter("DATA_OUT_0_PRECISION_1"),
+            "svmm_out_width":self.get_parameter("DATA_OUT_0_PRECISION_0"),
+            "svmm_out_frac_width":self.get_parameter("DATA_OUT_0_PRECISION_1"),
+        }
+        self.model = ViTAttentionInteger(
+                dim=self.get_parameter("DATA_IN_0_TENSOR_SIZE_DIM_0"),
+                num_heads=self.get_parameter("NUM_HEADS"),
+                qkv_bias=True if self.get_parameter("HAS_BIAS") else False,
+                q_config=self.q_config
+            )
+
+
+        # Set verbosity of driver and monitor loggers to debug
+        self.data_in_0_driver.log.setLevel(logging.DEBUG)
+        self.weight_query_driver.log.setLevel(logging.DEBUG)
+        self.weight_key_driver.log.setLevel(logging.DEBUG)
+        self.weight_value_driver.log.setLevel(logging.DEBUG)
+        self.data_out_0_monitor.log.setLevel(logging.DEBUG)
+
+    def generate_inputs(self, batch_size=1):
+        return torch.randn(
+            (
+                batch_size,
+                self.get_parameter("DATA_IN_0_TENSOR_SIZE_DIM_1"),
+                self.get_parameter("DATA_IN_0_TENSOR_SIZE_DIM_0"),
+            )
+        )
+
+    async def run_test(self, us=100):
+        await self.reset()
+        self.log.info(f"Reset finished") 
+        self.data_out_0_monitor.ready.value = 1
+
+        inputs = self.generate_inputs()
+        exp_out = self.model(inputs)[0]
+
+        # * Load the inputs driver
+        self.log.info(f"Processing inputs: {inputs}")
+        inputs = fixed_preprocess_tensor(
+            tensor=inputs,
+            q_config={
+                "width": self.get_parameter("DATA_IN_0_PRECISION_0"),
+                "frac_width": self.get_parameter("DATA_IN_0_PRECISION_1"),
+            },
+            parallelism=[
+                self.get_parameter("DATA_IN_0_PARALLELISM_DIM_1"),
+                self.get_parameter("DATA_IN_0_PARALLELISM_DIM_0"),
+            ],
+        )
+        self.data_in_0_driver.load_driver(inputs)
+
+        qkv_weight = self.model.qkv.weight.reshape(
+            3, self.get_parameter("WEIGHT_TENSOR_SIZE_DIM_1"), self.get_parameter("WEIGHT_TENSOR_SIZE_DIM_0"))
+        qkv_bias = self.model.qkv.bias.reshape(
+            3, self.get_parameter("BIAS_TENSOR_SIZE_DIM_1"), self.get_parameter("BIAS_TENSOR_SIZE_DIM_0"))
+        # * Load the weights driver
+        i = 0
+        for projection in ["query", "key", "value"]:
+            
+            if self.get_parameter("WEIGHTS_PRE_TRANSPOSED") == 1:
+                weights = qkv_weight[i].transpose(0, 1)
+            else:
+                weights = qkv_weight[i]
+
+            self.log.info(f"Processing {projection} weights: {weights}")
+            weights = fixed_preprocess_tensor(
+                tensor=weights,
+                q_config={
+                    "width": self.get_parameter("WEIGHT_PRECISION_0"),
+                    "frac_width": self.get_parameter("WEIGHT_PRECISION_1"),
+                },
+                parallelism=[
+                    self.get_parameter("WEIGHT_PARALLELISM_DIM_1"),
+                    self.get_parameter("WEIGHT_PARALLELISM_DIM_0"),
+                ],
+            )
+            getattr(self, f"weight_{projection}_driver").load_driver(weights)
+
+            # * Load the bias driver
+            if self.get_parameter("HAS_BIAS") == 1:
+                bias = qkv_bias[i]
+                self.log.info(f"Processing {projection} bias: {bias}")
+                bias = fixed_preprocess_tensor(
+                    tensor=bias,
+                    q_config={
+                        "width": self.get_parameter("BIAS_PRECISION_0"),
+                        "frac_width": self.get_parameter("BIAS_PRECISION_1"),
+                    },
+                    parallelism=[
+                        self.get_parameter("BIAS_PARALLELISM_DIM_1"),
+                        self.get_parameter("BIAS_PARALLELISM_DIM_0"),
+                    ],
+                )
+                getattr(self, f"bias_{projection}_driver").load_driver(bias)
+            i = i+1
+        # * Load the output monitor
+        self.log.info(f"Processing outputs: {exp_out}")
+        outs = fixed_preprocess_tensor(
+            tensor=exp_out,
+            q_config={
+                "width": self.get_parameter("DATA_OUT_0_PRECISION_0"),
+                "frac_width": self.get_parameter("DATA_OUT_0_PRECISION_1"),
+            },
+            parallelism=[
+                self.get_parameter("DATA_OUT_0_PARALLELISM_DIM_1"),
+                self.get_parameter("DATA_OUT_0_PARALLELISM_DIM_0"),
+            ],
+        )
+        self.data_out_0_monitor.load_monitor(outs)
+
+        await Timer(us, units="us")
+        assert self.data_out_0_monitor.exp_queue.empty()
+
+
+@cocotb.test()
+async def cocotb_test(dut):
+    tb = FixedSelfAttentionTB(dut)
+    await tb.run_test(us=100)
+
+default_config = {
+    "NUM_HEADS": 2,
+    "ACTIVATION": 1,
+    "DATA_IN_0_TENSOR_SIZE_DIM_0": 4,
+    "DATA_IN_0_TENSOR_SIZE_DIM_1": 2,
+    "DATA_IN_0_PARALLELISM_DIM_0": 1,
+    "DATA_IN_0_PARALLELISM_DIM_1": 1,
+    "DATA_IN_0_PRECISION_0": 8,
+    "DATA_IN_0_PRECISION_1": 4,
+    "WEIGHTS_PRE_TRANSPOSED": 1,
+    "WEIGHT_TENSOR_SIZE_DIM_0": 4,
+    "WEIGHT_TENSOR_SIZE_DIM_1": 4,
+    "WEIGHT_PARALLELISM_DIM_0": 1,
+    "WEIGHT_PARALLELISM_DIM_1": 1,
+    "WEIGHT_PRECISION_0": 16,
+    "WEIGHT_PRECISION_1": 8,
+    "HAS_BIAS": 1,
+    "BIAS_TENSOR_SIZE_DIM_0": 4,
+    "BIAS_TENSOR_SIZE_DIM_1": 1,
+    "BIAS_PARALLELISM_DIM_0": 1,
+    "BIAS_PARALLELISM_DIM_1": 1,
+    "BIAS_PRECISION_0": 16,
+    "BIAS_PRECISION_1": 8,
+    "DATA_OUT_0_TENSOR_SIZE_DIM_0": 4,
+    "DATA_OUT_0_TENSOR_SIZE_DIM_1": 2,
+    "DATA_OUT_0_PARALLELISM_DIM_0": 1,
+    "DATA_OUT_0_PARALLELISM_DIM_1": 1,
+    "DATA_OUT_0_PRECISION_0": 10,
+    "DATA_OUT_0_PRECISION_1": 4,
+}
+MULT_DATA = 1 / math.sqrt(default_config["DATA_IN_0_TENSOR_SIZE_DIM_0"] // default_config["NUM_HEADS"])
+def get_config(kwargs={}):
+    config = default_config
+    config.update(kwargs)
+    return config
+
+
+def test_fixed_linear_smoke():
+    """
+    Some quick tests to check if the module is working.
+    """
+    generate_memory.generate_sv_lut(
+        "exp",
+        default_config["DATA_OUT_0_PRECISION_0"],
+        default_config["DATA_OUT_0_PRECISION_1"],
+        default_config["DATA_OUT_0_PRECISION_0"],
+        default_config["DATA_OUT_0_PRECISION_1"],
+        constant_mult=MULT_DATA,
+    )
+    mase_runner(trace=True, module_param_list=[get_config()], skip_build=False)
+
+torch.manual_seed(0)
+if __name__ == "__main__":
+    test_fixed_linear_smoke()
