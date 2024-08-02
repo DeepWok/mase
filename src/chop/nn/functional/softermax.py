f7f7933bd116d712b5964b26760c9a68c894e3e2
Derek Lai <medereklai@gmail.com>
2024-08-02 12:04:49 +0100

    revert softermax

--- src/chop/nn/functional/softermax.py
+++ src/chop/nn/functional/softermax.py
@@ -1,12 +1,11 @@
-import torch
 from torch import Tensor
 
 
-def softermax(x: Tensor, dim: int) -> Tensor:
+def softermax(input: Tensor, dim: int) -> Tensor:
     """Softermax implementation, according to "Softermax: Hardware/Software Co-Design of an Efficient Softmax for Transformers" paper (https://arxiv.org/abs/2103.09301).
 
     Args:
-        x (Tensor): Input tensor
+        input (Tensor): Input tensor
 
     Returns:
         Tensor: Output tensor
@@ -16,14 +15,5 @@ def softermax(x: Tensor, dim: int) -> Tensor:
     out = 2**out
     row_sum = out.sum(dim=1).reshape((-1, 1)).expand(input.shape)
     # Elementwise division
-    out = out / row_sum
-    return out
-
-
-# Derek: Temp!
-def softermax2(x: Tensor, dim: int) -> Tensor:
-    out = x - torch.floor(x.max(dim=dim, keepdim=True).values)
-    out = 2**out
-    row_sum = out.sum(dim=dim, keepdim=True)
     out = out / row_sum
     return out
