fd029ae001e2d4cf9382ac5c95ad913025b0dd05
pgimenes <pgimenes@outlook.com>
2024-08-02 12:05:54 +0100

    change sphinx theme and remove static path warnings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -78,14 +78,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 # check the options here https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
 html_theme_options = {}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
 
 autodoc_default_options = {
     "members": True,
