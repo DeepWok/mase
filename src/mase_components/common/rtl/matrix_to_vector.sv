6eb65c4f03bec1b25261f3d3077c7960bcc09e69
przemekff <p.forys2000@gmail.com>
2024-08-06 16:47:32 +0000

    fixed matrix to vector for single mult

--- src/mase_components/common/rtl/matrix_to_vector.sv
+++ src/mase_components/common/rtl/matrix_to_vector.sv
@@ -52,12 +52,12 @@ enum int unsigned{
 state, next;
 
 logic [COUNTER_WIDTH:0] counter;
-logic [DATA_OUT_0_PRECISION_0-1:0] fifo_out [DATA_IN_0_PARALLELISM_DIM_0-1:0];
+logic [DATA_OUT_0_PRECISION_0-1:0] fifo_out [DATA_IN_0_PARALLELISM_DIM_0 * (DATA_IN_0_PARALLELISM_DIM_1-1)-1:0];
 //logic [DATA_OUT_0_PRECISION_0-1:0] fifo_in [DATA_IN_0_PARALLELISM_DIM_0 * DATA_IN_0_PARALLELISM_DIM_1-1:0];
 
 logic [DATA_OUT_0_PRECISION_0-1:0] data_in_0_out [DATA_IN_0_PARALLELISM_DIM_0-1:0];
-logic [DATA_OUT_0_PRECISION_0-1:0] data_in_0_fifo [DATA_IN_0_PARALLELISM_DIM_0-1:0];
-logic [DATA_OUT_0_PRECISION_0-1:0] fifo_in [DATA_IN_0_PARALLELISM_DIM_0-1:0];
+logic [DATA_OUT_0_PRECISION_0-1:0] data_in_0_fifo [DATA_IN_0_PARALLELISM_DIM_0 * (DATA_IN_0_PARALLELISM_DIM_1-1)-1:0];
+logic [DATA_OUT_0_PRECISION_0-1:0] fifo_in [DATA_IN_0_PARALLELISM_DIM_0 * (DATA_IN_0_PARALLELISM_DIM_1-1)-1:0];
 
 logic fifo_in_ready;
 logic fifo_in_valid;
@@ -66,13 +66,17 @@ logic fifo_out_ready;
 logic ctrl_output_fifo;
 logic ctrl_run_counter;
 logic ctrl_input_fifo;
+logic ctrl_data_out_valid;
+logic data_in_0_valid_delayed; 
+logic data_in_0_ready_delayed;
+logic fifo_out_valid_delayed;
 
 
 matrix_fifo #(
     .DATA_WIDTH(DATA_IN_0_PRECISION_0),
     .DIM0(DATA_IN_0_PARALLELISM_DIM_0),
-    .DIM1(1),
-    .FIFO_SIZE(DATA_IN_0_MAX_DEPTH_DIM_0)
+    .DIM1(DATA_IN_0_PARALLELISM_DIM_1-1),
+    .FIFO_SIZE(DATA_IN_0_MAX_DEPTH_DIM_0+1)
 ) m_fifo
 (
     .clk(clk),
@@ -107,7 +111,6 @@ always_comb begin
     end
     else
         for (int i=0; i<DATA_IN_0_PARALLELISM_DIM_0*DATA_IN_0_PARALLELISM_DIM_1; i++) begin
-            if(i<12)
                 fifo_in[i] = fifo_out[i+DATA_IN_0_PARALLELISM_DIM_0];
         end
 end
@@ -124,20 +127,45 @@ always_ff@(posedge clk) begin
         counter <= counter;
 end
 
+always_ff@(posedge clk) begin
+    fifo_out_valid_delayed <= fifo_out_valid;
+end
+
+always_ff@(posedge clk) begin
+    data_in_0_valid_delayed <= data_in_0_valid;
+    data_in_0_ready_delayed <= data_in_0_ready;
+end
+
+
+always_comb begin
+    if(data_in_0_ready_delayed && data_in_0_valid_delayed)
+        data_out_0_valid = 1;
+    else if(ctrl_data_out_valid)
+        data_out_0_valid = fifo_out_valid_delayed;
+    else
+        data_out_0_valid = 0;
+end
+
 
 //state machine blocks
 
 always_comb begin
     next = state;
     case(state)
-        IDLE:     if (data_in_0_valid && data_in_0_ready)                                                      next = LOAD;
+        IDLE:     if (data_in_0_valid && data_in_0_ready) begin
+                        if (data_in_0_depth_dim0 == 1)                                                         next = PROCESS;
+                        else                                                                                   next = LOAD; 
+                  end
         LOAD:     if (counter == (counter_max - data_in_0_depth_dim0 - 1) && fifo_activity)                    next = LAST;     
                   else if (counter >= (data_in_0_depth_dim0-1) && fifo_activity)                               next = PROCESS;
                   
         PROCESS:  if (counter == counter_max - data_in_0_depth_dim0)                                           next = LAST;
         LAST:     if (counter == counter_max)                                                                  next = FINISHED;
-        FINISHED: if (data_in_0_valid && data_in_0_ready)                                                      next = LOAD;
-                  else                                                                                         next = IDLE;
+        FINISHED: if (data_in_0_valid && data_in_0_ready) begin
+                        if (data_in_0_depth_dim0 == 1)                                                         next = PROCESS;
+                        else                                                                                   next = LOAD;
+                  end
+                  else                                                                                         next = IDLE;                                                                               
     endcase
 end
 
@@ -145,40 +173,45 @@ end
 always_comb  begin
     case(state)
         IDLE: begin       data_in_0_ready  = fifo_in_ready;
-                          fifo_out_ready   = 0;
-                          ctrl_output_fifo = 0;
-                          ctrl_run_counter = 1;
-                          ctrl_input_fifo  = 1;
-                          fifo_in_valid = data_in_0_valid;
+                          fifo_out_ready      = 0;
+                          ctrl_output_fifo    = 0;
+                          ctrl_run_counter    = 1;
+                          ctrl_input_fifo     = 1;
+                          ctrl_data_out_valid = 0;
+                          fifo_in_valid       = data_in_0_valid;
         end
-        LOAD: begin       data_in_0_ready  = fifo_in_ready;
-                          fifo_out_ready   = 0;
-                          ctrl_output_fifo = 0;
-                          ctrl_run_counter = 1;
-                          ctrl_input_fifo  = 1;
-                          fifo_in_valid = data_in_0_valid;
+        LOAD: begin       data_in_0_ready     = fifo_in_ready;
+                          fifo_out_ready      = 0;
+                          ctrl_output_fifo    = 0;
+                          ctrl_run_counter    = 1;
+                          ctrl_input_fifo     = 1;
+                          ctrl_data_out_valid = 0;
+                          fifo_in_valid       = data_in_0_valid;
                         
         end
-        PROCESS: begin    data_in_0_ready  = 0;
-                          fifo_out_ready   = 1;
-                          ctrl_output_fifo = 1;
-                          ctrl_run_counter = 1;
-                          ctrl_input_fifo  = 0;
-                          fifo_in_valid = fifo_out_valid;
+        PROCESS: begin    data_in_0_ready     = 0;
+                          fifo_out_ready      = fifo_in_ready;
+                          ctrl_output_fifo    = 1;
+                          ctrl_run_counter    = 1;
+                          ctrl_input_fifo     = 0;
+                          ctrl_data_out_valid = 1;
+                          fifo_in_valid       = fifo_out_valid;
         end
-        LAST: begin       data_in_0_ready  = 0;
-                          fifo_out_ready   = 1;
-                          ctrl_output_fifo = 1;
-                          ctrl_run_counter = 1;
-                          ctrl_input_fifo  = 1;
-                          fifo_in_valid    = 0;
+        LAST: begin       data_in_0_ready     = 0;
+                          fifo_out_ready      = fifo_in_ready;
+                          ctrl_output_fifo    = 1;
+                          ctrl_run_counter    = 1;
+                          ctrl_input_fifo     = 1;
+                          ctrl_data_out_valid = 1;
+                          fifo_in_valid       = 0;
         end
-        FINISHED: begin   data_in_0_ready  = 1;
-                          fifo_out_ready   = 1;
-                          ctrl_output_fifo = 0;
-                          ctrl_run_counter = 1;
-                          ctrl_input_fifo  = 1;
-                          fifo_in_valid    = data_in_0_valid;
+        FINISHED: begin   data_in_0_ready     = 1;
+                          fifo_out_ready      = 1;
+                          ctrl_output_fifo    = 0;
+                          ctrl_run_counter    = 1;
+                          ctrl_input_fifo     = 1;
+                          ctrl_data_out_valid = 1;
+                          fifo_in_valid       = data_in_0_valid;
                           
         end
     endcase
@@ -188,6 +221,8 @@ always_ff @(posedge clk) begin
     if(rst) state <= IDLE;
     else    state <= next;
 end
+
+
 
 assign fifo_activity = (fifo_in_ready && fifo_in_valid) || (fifo_out_ready && fifo_out_valid);
 
--- src/mase_components/common/test/matrix_to_vector_tb.py
+++ src/mase_components/common/test/matrix_to_vector_tb.py
@@ -1,5 +1,6 @@
 import logging
 import random
+from random import choice
 
 import cocotb
 from cocotb.triggers import *
@@ -8,7 +9,11 @@ from mase_cocotb.testbench import Testbench
 from mase_cocotb.interfaces.streaming import StreamDriver, StreamMonitor
 from mase_cocotb.runner import mase_runner
 from mase_cocotb.utils import bit_driver, batched
-from mase_cocotb.matrix_tools import gen_random_matrix_input
+from mase_cocotb.matrix_tools import (
+    gen_random_matrix_input,
+    rebuild_matrix,
+    split_matrix,
+)
 
 logger = logging.getLogger("testbench")
 logger.setLevel(logging.INFO)
@@ -28,29 +33,62 @@ class MatrixToVectorTB(Testbench):
             ]
         )
 
-        dut.counter_max.value = 4
-        dut.data_in_0_depth_dim0.value = 2
+        self.DIM0 = choice(range(self.DATA_IN_0_PARALLELISM_DIM_0, self.DATA_IN_0_MAX_TENSOR_SIZE_DIM_0, self.DATA_IN_0_PARALLELISM_DIM_0))
+        self.DIM1 = choice(range(self.DATA_IN_0_PARALLELISM_DIM_1, self.DATA_IN_0_MAX_TENSOR_SIZE_DIM_1, self.DATA_IN_0_PARALLELISM_DIM_1))
+        
+        #self.DIM0 = 6
+        #elf.DIM1 = 6
+
+        dut.data_in_0_depth_dim0.value = self.DIM0 // self.DATA_IN_0_PARALLELISM_DIM_0
+        print(self.DIM0 // self.DATA_IN_0_PARALLELISM_DIM_0)
+
+        # should be set to depth0 * parallelism1
+        dut.counter_max.value = self.DIM0 // self.DATA_IN_0_PARALLELISM_DIM_0 * self.DATA_IN_0_PARALLELISM_DIM_1
+
+
         # Driver/Monitor
         self.in_driver = StreamDriver(dut.clk, dut.data_in_0, dut.data_in_0_valid, dut.data_in_0_ready)
         self.output_monitor = StreamMonitor(
-            dut.clk, dut.data_out_0, dut.data_out_0_valid, dut.data_out_0_ready
+            dut.clk, dut.data_out_0, dut.data_out_0_valid, dut.data_out_0_ready, unsigned = True
         )
+
+        self.output_monitor.log.setLevel(logging.DEBUG)
 
 
     async def run_test(self, us):
         await self.reset()
+
+        self.output_monitor.ready.value = 1
+
         inputs = self.generate_inputs()
-        #exp_out = self.model(A_inputs, B_inputs)
+        print(inputs)
+        exp_out = self.model(inputs)
+        print(exp_out)
+
         # Setup drivers and monitors
         self.in_driver.load_driver(inputs)
-        #self.output_monitor.load_monitor(exp_out)
+        self.output_monitor.load_monitor(exp_out)
+
         await Timer(us, units="us")
         assert self.output_monitor.exp_queue.empty()
 
+    def model(self, X):
+        X_matrix = rebuild_matrix(
+            X, self.DIM0, self.DIM1, self.DATA_IN_0_PARALLELISM_DIM_0, self.DATA_IN_0_PARALLELISM_DIM_1
+        )
+        return split_matrix(
+            X_matrix,
+            self.DIM0,
+            self.DIM1,
+            self.DATA_IN_0_PARALLELISM_DIM_0,
+            1
+        )
+
+
     def generate_inputs(self):
         inputs = gen_random_matrix_input(
-            self.DATA_IN_0_MAX_TENSOR_SIZE_DIM_0,
-            self.DATA_IN_0_MAX_TENSOR_SIZE_DIM_1,
+            self.DIM0,
+            self.DIM1,
             self.DATA_IN_0_PARALLELISM_DIM_0,
             self.DATA_IN_0_PARALLELISM_DIM_1,
             self.DATA_IN_0_PRECISION_0,
@@ -61,16 +99,32 @@ class MatrixToVectorTB(Testbench):
 def test_matrix_to_vector():
     # Default is a square matrix mult
     # 4x4 4x4 matrix multiplication done using 2x2 window
-    DEFAULT_CONFIG = {
+    P2X2 = {
         "DATA_IN_0_PARALLELISM_DIM_0": 2,
         "DATA_IN_0_PARALLELISM_DIM_1": 2,
-        "DATA_IN_0_MAX_TENSOR_SIZE_DIM_0": 4,
-        "DATA_IN_0_MAX_TENSOR_SIZE_DIM_1": 4,
+        "DATA_IN_0_MAX_TENSOR_SIZE_DIM_0": 8,
+        "DATA_IN_0_MAX_TENSOR_SIZE_DIM_1": 8,
+        "DATA_IN_0_PRECISION_0": 8,
+        "DATA_IN_0_PRECISION_1": 8,
+    }
+    P2X4 = {
+        "DATA_IN_0_PARALLELISM_DIM_0": 2,
+        "DATA_IN_0_PARALLELISM_DIM_1": 4,
+        "DATA_IN_0_MAX_TENSOR_SIZE_DIM_0": 8,
+        "DATA_IN_0_MAX_TENSOR_SIZE_DIM_1": 8,
+        "DATA_IN_0_PRECISION_0": 8,
+        "DATA_IN_0_PRECISION_1": 8,
+    }
+    P4X2 = {
+        "DATA_IN_0_PARALLELISM_DIM_0": 4,
+        "DATA_IN_0_PARALLELISM_DIM_1": 2,
+        "DATA_IN_0_MAX_TENSOR_SIZE_DIM_0": 8,
+        "DATA_IN_0_MAX_TENSOR_SIZE_DIM_1": 8,
         "DATA_IN_0_PRECISION_0": 8,
         "DATA_IN_0_PRECISION_1": 8,
     }
     mase_runner(
-        module_param_list=[DEFAULT_CONFIG],
+        module_param_list=[P2X2, P2X4, P4X2],
         trace=True,
         jobs=12,
         skip_build = False
