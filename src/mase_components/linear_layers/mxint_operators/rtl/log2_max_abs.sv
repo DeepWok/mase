4da5752f7a9c2215be05f32c92999125de0e03cb
cx922 <cx922@ic.ac.uk>
2024-10-04 14:16:40 +0000

    optimize block max

--- src/mase_components/linear_layers/mxint_operators/rtl/log2_max_abs.sv
+++ src/mase_components/linear_layers/mxint_operators/rtl/log2_max_abs.sv
@@ -16,24 +16,33 @@ module log2_max_abs #(
     output logic                 data_out_valid,
     input  logic                 data_out_ready
 );
-  logic [IN_WIDTH - 1:0] max_abs_value;
-  max_abs_tree #(
+  logic [IN_WIDTH - 1:0] or_result;
+  logic [IN_WIDTH - 1:0] abs_data_in [IN_SIZE - 1:0];
+  for (genvar i=0; i< IN_SIZE; i++) begin
+    abs #(
+      .IN_WIDTH(IN_WIDTH)
+    ) abs_i (
+      .data_in(data_in[i]),
+      .data_out(abs_data_in[i])
+    );
+  end
+  or_tree #(
       .IN_SIZE (IN_SIZE),
       .IN_WIDTH(IN_WIDTH),
   ) max_bas_i (
       .clk,
       .rst,
-      .data_in(data_in),
+      .data_in(abs_data_in),
       .data_in_valid(data_in_valid),
       .data_in_ready(data_in_ready),
-      .data_out(max_abs_value),
+      .data_out(or_result),
       .data_out_valid(data_out_valid),
       .data_out_ready(data_out_ready)
   );
   log2_value #(
       .IN_WIDTH(IN_WIDTH),
   ) log2_i (
-      .data_in (max_abs_value),
+      .data_in (or_result),
       .data_out(data_out)
   );
 
@@ -51,11 +60,22 @@ module log2_value #(
   logic [$clog2(IN_WIDTH) - 1:0] unsigned_log_out;
   always_comb begin
     for (i = IN_WIDTH - 1; i >= 0; i = i - 1) begin
-      if ((data_in > (1 << i))) begin
+      if (data_in[i] == 1) begin
         unsigned_log_out = i + 1;
         break;
       end
     end
   end
   assign data_out = {1'b0, unsigned_log_out};
+endmodule
+
+module abs #(
+    parameter IN_WIDTH = 8  // Parameter for bit-width, can be adjusted
+) (
+    input  wire [IN_WIDTH-1:0] data_in,  // N-bit input number
+    output wire [IN_WIDTH-1:0] data_out  // N-bit output representing 2's complement
+);
+
+  // 2's complement calculation
+  assign data_out = data_in[IN_WIDTH-1] ? ~data_in + 1 : data_in;
 endmodule
