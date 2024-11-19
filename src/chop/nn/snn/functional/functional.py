90c09f62faf163c860f699a9d1ffae88f9fc5508
Jeffrey <jefferywongtszhang@hotmail.com>
2024-11-19 13:05:28 +0000

    feat: add reset function for modules

--- src/chop/nn/snn/functional/functional.py
+++ src/chop/nn/snn/functional/functional.py
@@ -8,12 +8,45 @@
 
 import logging
 import copy
+from chop.nn.snn import base
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
 from typing import Callable, Union
 from torch import Tensor
+
+
+def reset_net(net: nn.Module):
+    """
+    * :ref:`API in English <reset_net-en>`
+
+    .. _reset_net-cn:
+
+    :param net: 任何属于 ``nn.Module`` 子类的网络
+
+    :return: None
+
+    将网络的状态重置。做法是遍历网络中的所有 ``Module``，若 ``m `` 为 ``base.MemoryModule`` 函数或者是拥有 ``reset()`` 方法，则调用 ``m.reset()``。
+
+    * :ref:`中文API <reset_net-cn>`
+
+    .. _reset_net-en:
+
+    :param net: Any network inherits from ``nn.Module``
+
+    :return: None
+
+    Reset the whole network.  Walk through every ``Module`` as ``m``, and call ``m.reset()`` if this ``m`` is ``base.MemoryModule`` or ``m`` has ``reset()``.
+    """
+    for m in net.modules():
+        if hasattr(m, "reset"):
+            if not isinstance(m, base.MemoryModule):
+                logging.warning(
+                    f"Trying to call `reset()` of {m}, which is not spikingjelly.activation_based.base"
+                    f".MemoryModule"
+                )
+            m.reset()
 
 
 def multi_step_forward(
