46b39ad074d02688271001f99b109aa498d5c599
Cano Xiao <cx922@ee-kraken.ee.ic.ac.uk>
2024-11-28 16:22:16 +0000

    ViT

--- src/chop/models/vision/vit/__init__.py
+++ src/chop/models/vision/vit/__init__.py
@@ -1,1 +1,5 @@
-from .vit import get_vit_tiny_patch16, get_vit_base_patch16
+from .vit import (
+    get_vit_tiny_patch16_224, 
+    get_vit_small_patch16_224,
+    get_vit_base_patch16_224
+)
--- src/chop/models/vision/vit/vit.py
+++ src/chop/models/vision/vit/vit.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-from chop.models.vision.pvt.pvt import PatchEmbed, Mlp, DropPath
 from functools import partial
 from logging import getLogger
 from timm.layers import (
@@ -8,8 +7,9 @@ from timm.layers import (
     get_norm_layer,
     LayerType,
 )
-from timm.models._hub import load_state_dict_from_hf
 import numpy as np
+from torchvision.models import ViT_B_16_Weights
+from torchvision.models import VisionTransformer
 from .utils import load_weights_from_npz
 logger = getLogger(__name__)
 
@@ -30,301 +30,58 @@ from typing import (
     List,
 )
 
-class Attention(nn.Module):
-
-    def __init__(
-        self,
-        dim: int,
-        num_heads: int = 8,
-        qkv_bias: bool = False,
-        qk_norm: bool = False,
-        attn_drop: float = 0.0,
-        proj_drop: float = 0.0,
-        norm_layer: nn.Module = nn.LayerNorm,
-    ) -> None:
-        super().__init__()
-        assert dim % num_heads == 0, "dim should be divisible by num_heads"
-        self.num_heads = num_heads
-        self.head_dim = dim // num_heads
-        self.scale = torch.tensor(self.head_dim**-0.5)
-
-        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
-        self.q_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
-        self.k_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
-        self.attn_drop = nn.Dropout(attn_drop)
-        self.proj = nn.Linear(dim, dim)
-        self.proj_drop = nn.Dropout(proj_drop)
-
-    def __name__(self):
-        return "vit_attention"
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        B, N, C = x.shape
-        qkv = (
-            self.qkv(x)
-            .reshape(B, N, 3, self.num_heads, self.head_dim)
-            .permute(2, 0, 3, 1, 4)
-        )
-        q, k, v = qkv[0], qkv[1], qkv[2]
-        q, k = self.q_norm(q), self.k_norm(k)
-
-        q = q * self.scale
-        attn = q @ k.transpose(-2, -1)
-        attn = torch.softmax(attn,dim=-1)
-        attn = self.attn_drop(attn)
-        x = attn @ v
-
-        x = x.transpose(1, 2).reshape(B, N, C)
-        x = self.proj(x)
-        x = self.proj_drop(x)
-        return x
-
-
-class Block(nn.Module):
-    def __init__(
-        self,
-        dim: int,
-        num_heads: int,
-        mlp_ratio: float = 4.0,
-        qkv_bias: bool = False,
-        proj_drop: float = 0.0,
-        attn_drop: float = 0.0,
-        drop_path: float = 0.0,
-        act_layer: nn.Module = nn.GELU,
-        norm_layer: nn.Module = nn.LayerNorm,
-        mlp_layer: nn.Module = Mlp,
-    ) -> None:
-        super().__init__()
-        self.norm1 = norm_layer(dim)
-        self.attn = Attention(
-            dim,
-            num_heads=num_heads,
-            qkv_bias=qkv_bias,
-            attn_drop=attn_drop,
-            proj_drop=proj_drop,
-        )
-        self.drop_path1 = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
-
-        self.norm2 = norm_layer(dim)
-        self.mlp = mlp_layer(
-            in_features=dim,
-            hidden_features=int(dim * mlp_ratio),
-            act_layer=act_layer,
-            drop=proj_drop,
-        )
-        self.drop_path2 = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = x + self.drop_path1(self.attn(self.norm1(x)))
-        x = x + self.drop_path2(self.mlp(self.norm2(x)))
-        return x
-
-
-class VisionTransformer(nn.Module):
-    """Vision Transformer
-
-    A PyTorch impl of : `An Image is Worth 16x16 Words: Transformers for Image Recognition at Scale`
-        - https://arxiv.org/abs/2010.11929
-    """
-
-    def __init__(
-        self,
-        img_size: Union[int, Tuple[int, int]] = 224,
-        patch_size: Union[int, Tuple[int, int]] = 16,
-        in_chans: int = 3,
-        num_classes: int = 1000,
-        global_pool: Literal["", "avg", "token"] = "token",
-        embed_dim: int = 768,
-        depth: int = 12,
-        num_heads: int = 12,
-        mlp_ratio: float = 4.0,
-        qkv_bias: bool = True,
-        class_token: bool = True,
-        fc_norm: Optional[bool] = None,
-        drop_rate: float = 0.0,
-        pos_drop_rate: float = 0.0,
-        proj_drop_rate: float = 0.0,
-        attn_drop_rate: float = 0.0,
-        drop_path_rate: float = 0.0,
-        embed_layer: Callable = PatchEmbed,
-        norm_layer: Optional[LayerType] = None,
-        act_layer: Optional[LayerType] = None,
-    ) -> None:
-        """
-        Args:
-            img_size: Input image size.
-            patch_size: Patch size.
-            in_chans: Number of image input channels.
-            num_classes: Mumber of classes for classification head.
-            global_pool: Type of global pooling for final sequence (default: 'token').
-            embed_dim: Transformer embedding dimension.
-            depth: Depth of transformer.
-            num_heads: Number of attention heads.
-            mlp_ratio: Ratio of mlp hidden dim to embedding dim.
-            qkv_bias: Enable bias for qkv projections if True.
-            init_values: Layer-scale init values (layer-scale enabled if not None).
-            class_token: Use class token.
-            no_embed_class: Don't include position embeddings for class (or reg) tokens.
-            reg_tokens: Number of register tokens.
-            fc_norm: Pre head norm after pool (instead of before), if None, enabled when global_pool == 'avg'.
-            drop_rate: Head dropout rate.
-            pos_drop_rate: Position embedding dropout rate.
-            attn_drop_rate: Attention dropout rate.
-            drop_path_rate: Stochastic depth rate.
-            embed_layer: Patch embedding layer.
-            norm_layer: Normalization layer.
-            act_layer: MLP activation layer.
-            block_fn: Transformer block layer.
-        """
-        super().__init__()
-        assert global_pool in ("", "avg", "token")
-        assert class_token or global_pool != "token"
-        use_fc_norm = global_pool == "avg" if fc_norm is None else fc_norm
-        norm_layer = get_norm_layer(norm_layer) or partial(nn.LayerNorm, eps=1e-6)
-        act_layer = get_act_layer(act_layer) or nn.GELU
-
-        # self.patched_nodes = {
-        #         "layers": (Attention,),
-        #         "functions": (),
-        #         "additional_inputs": ()
-        #     }
-        self.num_classes = num_classes
-        self.global_pool = global_pool
-        self.num_features = self.embed_dim = (
-            embed_dim  # num_features for consistency with other models
-        )
-        self.num_prefix_tokens = 1 if class_token else 0
-        self.has_class_token = class_token
-        self.patch_embed = embed_layer(
-            img_size=img_size,
-            patch_size=patch_size,
-            in_chans=in_chans,
-            embed_dim=embed_dim
-        )
-        num_patches = self.patch_embed.num_patches
-
-        self.cls_token = (
-            nn.Parameter(torch.zeros(1, 1, embed_dim)) if class_token else None
-        )
-        num_patches = num_patches + 1 if class_token else num_patches
-        self.pos_embed = nn.Parameter(torch.randn(1, num_patches, embed_dim) * 0.02)
-        self.pos_drop = nn.Dropout(p=pos_drop_rate)
-
-        dpr = [
-            x.item() for x in torch.linspace(0, drop_path_rate, depth)
-        ]  # stochastic depth decay rule
-        self.blocks = nn.Sequential(
-            *[
-                Block(
-                    dim=embed_dim,
-                    num_heads=num_heads,
-                    mlp_ratio=mlp_ratio,
-                    qkv_bias=qkv_bias,
-                    proj_drop=proj_drop_rate,
-                    attn_drop=attn_drop_rate,
-                    drop_path=dpr[i],
-                    norm_layer=norm_layer,
-                    act_layer=act_layer,
-                )
-                for i in range(depth)
-            ]
-        )
-        self.norm = norm_layer(embed_dim) if not use_fc_norm else nn.Identity()
-
-        # Classifier Head
-        self.fc_norm = norm_layer(embed_dim) if use_fc_norm else nn.Identity()
-        self.head_drop = nn.Dropout(drop_rate)
-        self.head = (
-            nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
-        )
-    def _pos_embed(self, x: torch.Tensor) -> torch.Tensor:
-        pos_embed = self.pos_embed
-        x = torch.cat([self.cls_token.expand(x.shape[0], -1, -1), x], dim=1)
-        x = x + pos_embed
-
-        return self.pos_drop(x)
-
-    def forward_features(self, x: torch.Tensor) -> torch.Tensor:
-        x, _ = self.patch_embed(x)
-        x = self._pos_embed(x)
-        x = self.blocks(x)
-        x = self.norm(x)
-        return x
-
-    def forward_head(self, x: torch.Tensor, pre_logits: bool = False) -> torch.Tensor:
-        if self.global_pool == "avg":
-            x = x[:, self.num_prefix_tokens :].mean(dim=1)
-        elif self.global_pool:
-            x = x[:, 0]  # class token
-        x = self.fc_norm(x)
-        x = self.head_drop(x)
-        return x if pre_logits else self.head(x)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.forward_features(x)
-        x = self.forward_head(x)
-        return x
-
-
-def load_pretrained(pretrained, num_classes, model, url, model_name):
-    if pretrained:
-        checkpoint = np.load(
-            "/root/work/mase-tools/machop/.machop_cache/.machop_cache.ff0e4f759408437a93630130d36308e8.partial"
-        )
-        model.load_state_dict(checkpoint, strict=False)
-        logger.info("Pretrained weights loaded into {}".format(model_name))
-    else:
-        logger.info("{} randomly initialized".format(model_name))
-
-
-def get_vit_tiny_patch16(info, pretrained=False, **kwargs):
+def get_vit_tiny_patch16_224(info, pretrained=False, **kwargs):
     """ViT-Tiny (Vit-Ti/16)"""
     num_classes = info.num_classes
     img_size = info.image_size[-1]
     model = VisionTransformer(
-        img_size=img_size,
-        num_classes=num_classes,
+        image_size=img_size,
         patch_size=16,
-        embed_dim=192,
-        num_heads=3,
-        depth=12,
+        num_layers=12,
+        num_heads=12,
+        hidden_dim=192,
+        mlp_dim=768,
         **kwargs,
     )
-    if img_size == 224:
-        url = "https://storage.googleapis.com/vit_models/augreg/Ti_16-i21k-300ep-lr_0.001-aug_none-wd_0.03-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.03-res_224.npz"
-    elif img_size == 384:
-        url = "https://storage.googleapis.com/vit_models/augreg/Ti_16-i21k-300ep-lr_0.001-aug_none-wd_0.03-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.03-res_384.npz"
-    else:
-        pretrained = False
-        logger.warning("this image_size is not supported rightnow")
-
-    load_weights_from_npz(model, url, check_hash=True)
+    if pretrained:
+        raise NotImplementedError("Pretrained weights not available for vit_tiny_patch16")
     return model
 
-
-def get_vit_base_patch16(info, pretrained=False, **kwargs):
-    """ViT-Base (Vit-B/16)"""
+def get_vit_small_patch16_224(info, pretrained=False, **kwargs):
+    """ViT-Tiny (Vit-Ti/16)"""
     num_classes = info.num_classes
     img_size = info.image_size[-1]
     model = VisionTransformer(
-        img_size=img_size,
-        num_classes=num_classes,
+        image_size=img_size,
         patch_size=16,
-        embed_dim=768,
+        num_layers=12,
         num_heads=12,
-        depth=12,
+        hidden_dim=384,
+        mlp_dim=1536,
         **kwargs,
     )
-    if img_size == 224:
-        url = 'https://storage.googleapis.com/vit_models/augreg/' + \
-            'B_16-i21k-300ep-lr_0.001-aug_medium1-wd_0.1-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.01-res_224.npz'
-    elif img_size == 384:
-        url = "https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-vitjx/jx_vit_base_p16_384-83fb41ba.pth"
-    else:
-        logger.warning("this image_size is not supported rightnow")
-        pretrained = False
-
     if pretrained:
-        load_weights_from_npz(model, url, check_hash=True)
+        raise NotImplementedError("Pretrained weights not available for vit_small_patch16")
+    return model
+
+def get_vit_base_patch16_224(info, pretrained=False, **kwargs):
+    """ViT-Base (Vit-B/16)"""
+    num_classes = info.num_classes
+    img_size = info.image_size[-1]
+
+    model = VisionTransformer(
+        image_size=img_size,
+        patch_size=16,
+        num_layers=12,
+        num_heads=12,
+        hidden_dim=768,
+        mlp_dim=3072,
+        **kwargs,
+    )
+    if pretrained:
+        pretrained_weight_cls = ViT_B_16_Weights.IMAGENET1K_V1
+        pretrained_weight = pretrained_weight_cls.get_state_dict(progress=True)
+        model.load_state_dict(pretrained_weight, strict=True)
         logger.info("Pretrained weights loaded into vit_base_patch16")
     else:
         logger.info("vit_base_patch16 randomly initialized")
