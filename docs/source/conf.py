31ee00410d7c41b56c2b285d7d546554bdbb59ab
Aaron Zhao <Aaron-Zhao123@users.noreply.github.com>
2025-01-02 23:59:02 +0800

    update sphinx to try (#242)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,9 +43,7 @@ extensions = [
     "sphinx_needs",
     "sphinxcontrib.plantuml",
     "myst_nb",
-    # TODO: rebuild
-    # this causes a problem on github ci
-    # "sphinxcontrib.test_reports",
+    "sphinxcontrib.test_reports",
 ]
 
 myst_enable_extensions = [
