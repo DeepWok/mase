5ac60362f4dac7e241ef741f5634a3d649dba4a7
Johnny1882 <jw3621@ic.ac.uk>
2025-02-27 12:58:25 +0000

    use bert instead of dummy model

--- src/chop/passes/module/__init__.py
+++ src/chop/passes/module/__init__.py
@@ -1,6 +1,6 @@
 from .analysis import calculate_avg_bits_module_analysis_pass
 from .analysis.report import report_trainable_parameters_analysis_pass
-from .transforms import quantize_module_transform_pass, resharding_transform_pass
+from .transforms import quantize_module_transform_pass, resharding_transform_pass, mla_transform_pass
 
 ANALYSIS_PASSES = ["calculate_avg_bits_module_analysis_pass"]
 
--- test/passes/module/transforms/mla/test_mla_transform.py
+++ test/passes/module/transforms/mla/test_mla_transform.py
@@ -6,46 +6,27 @@ import sys
 
 import torch
 import torch.nn as nn
+from transformers import AutoModel, AutoTokenizer
 
 from pathlib import Path
 
 sys.path.append(Path(__file__).resolve().parents[5].as_posix())
 
-
+from chop.passes.module.transforms import quantize_module_transform_pass
 from chop.passes.module.transforms import mla_transform_pass
 
 
 # --------------------------------------------------
 #   Model specifications
 # --------------------------------------------------
-class SimpleTransformerEncoder(nn.Module):
-    """
-    A simple Transformer encoder with a few layers.
-    """
-    def __init__(self, d_model=128, nhead=4, num_layers=2, dim_feedforward=256):
-        super(SimpleTransformerEncoder, self).__init__()
-        encoder_layer = nn.TransformerEncoderLayer(
-            d_model=d_model,
-            nhead=nhead,
-            dim_feedforward=dim_feedforward,
-            batch_first=True  # Use batch_first=True for convenience
-        )
-        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
-        
-        # final linear just for demonstration
-        self.fc_out = nn.Linear(d_model, 10)
-        
-    def forward(self, x):
-        """
-        x is expected to be of shape [batch_size, seq_len, d_model]
-        """
-        enc_output = self.transformer_encoder(x)  # (B, S, D)
-        output = self.fc_out(enc_output[:, 0, :]) # classify just first token
-        return output
+model_name = "prajjwal1/bert-mini"  # or "prajjwal1/bert-tiny"
+model = AutoModel.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+print(model)
 
 
 def test_mla_transform_pass():
-    mlp = SimpleTransformerEncoder()
+    mlp = model()
     # Sanity check and report
     # mg = verify_common_metadata_analysis_pass(mg)
     pass_args = {
