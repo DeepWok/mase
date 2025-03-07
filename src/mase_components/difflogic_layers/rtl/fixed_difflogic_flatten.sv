423e29192298ce7cd2c45052b84acc74c16886d7
Johan Jino <johanjinoj@gmail.com>
2025-03-07 16:01:31 +0000

    fix: update flatten verilog

--- src/mase_components/difflogic_layers/rtl/fixed_difflogic_flatten.sv
+++ src/mase_components/difflogic_layers/rtl/fixed_difflogic_flatten.sv
@@ -6,24 +6,22 @@ module fixed_difflogic_flatten #(
     input wire clk,
     input wire rst,
 
-    input wire [DATA_IN_0_TENSOR_SIZE_DIM_0-1:0] data_in_0 [0:DATA_IN_0_TENSOR_SIZE_DIM_1-1];
+    input wire [DATA_IN_0_TENSOR_SIZE_DIM_0-1:0] data_in_0 [0:DATA_IN_0_TENSOR_SIZE_DIM_1-1],
     input  wire data_in_0_valid,
     output wire data_in_0_ready,
 
-    output reg [(DATA_IN_0_TENSOR_SIZE_DIM_0*DATA_IN_0_TENSOR_SIZE_DIM_1-1):0] data_out_0;
+    output reg [(DATA_IN_0_TENSOR_SIZE_DIM_0*DATA_IN_0_TENSOR_SIZE_DIM_1-1):0] data_out_0,
     output reg data_out_0_valid,
     input  reg data_out_0_ready
-)
+);
 
 genvar i;
+genvar j;
 generate
-    for (integer i = 0; i < DATA_IN_0_TENSOR_SIZE_DIM_1; i = i+1) begin: ROW
-        genvar j;
-        generate
-            for (integer j = 0; j < DATA_IN_0_TENSOR_SIZE_DIM_0; i = i+1) begin: COL
-                assign data_out_0[i*DATA_IN_0_TENSOR_SIZE_DIM_1+j] = data_in_0[i][j];
-            end
-        endgenerate
+    for (i = 0; i < DATA_IN_0_TENSOR_SIZE_DIM_1; i = i+1) begin: ROW
+        for (j = 0; j < DATA_IN_0_TENSOR_SIZE_DIM_0; j = j+1) begin: COL
+            assign data_out_0[i*DATA_IN_0_TENSOR_SIZE_DIM_1+j] = data_in_0[i][j];
+        end
     end
 endgenerate
 
