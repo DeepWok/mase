caff05fa0c9fae38b4c699a2715343c646d59d90
Isabel Z Zheng <zz3521@cx3-19-5.cx3.hpc.ic.ac.uk>
2025-03-11 00:33:12 +0000

    fc_v3_debugged

--- test/passes/module/transforms/attention/test_fc_transform.py
+++ test/passes/module/transforms/attention/test_fc_transform.py
@@ -8,7 +8,7 @@ import sys
 import dill
 import torch
 import torch.nn as nn
-from transformers import AutoModel, AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset
 from chop.tools import get_tokenized_dataset, get_trainer
 from chop import MaseGraph
@@ -27,7 +27,7 @@ from chop.passes.module.transforms.attention import fc_transform_pass
 
 checkpoint = "openai-community/gpt2"
 tokenizer_checkpoint = "openai-community/gpt2"
-dataset_name = "imdb"
+dataset_name = "wikitext"  
 
 dataset, tokenizer = get_tokenized_dataset(
     dataset=dataset_name,
@@ -38,28 +38,19 @@ tokenizer.pad_token = tokenizer.eos_token
 
 # with open(f"{Path.home()}/Projects/mase/mase_output/bert-uncased-2epoch.pkl", "rb") as f:
 #     model = dill.load(f)
-model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
-model.config.problem_type = "single_label_classification"
-model.config.pad_token_id = tokenizer.eos_token_id
+model = AutoModelForCausalLM.from_pretrained(checkpoint)
 
 def test_fc_transform_pass(model):
-    module_name = "transformer.h.0.attn"
+    module_name = "transformer.h.11.attn"
+    config = {}  # hidden_size
     model = fc_transform_pass(model, module_name, config={})
     return model
 
 model = test_fc_transform_pass(model)
-print("Model after FC Transform:", model)
+print("Model after FC Output Projection Transform:", model)
 
-trainer = get_trainer(
-    model=model,
-    tokenized_dataset=dataset,
-    tokenizer=tokenizer,
-    evaluate_metric="accuracy",
-    num_train_epochs=2,
-)
-eval_results = trainer.evaluate()
-print(f"Evaluation accuracy before fintuning: {eval_results['eval_accuracy']}")
-
-trainer.train()
-eval_results = trainer.evaluate()
-print(f"Evaluation accuracy: {eval_results['eval_accuracy']}")
+input_text = "Once upon a time"
+input_ids = tokenizer.encode(input_text, return_tensors="pt")
+outputs = model.generate(input_ids, max_length=50)
+generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+print("Generated text:", generated_text)
