43920f389b52873fb6dc100f2949d4441445fd4d
Denis Fouchard <denis.fouchard@telecom-paris.fr>
2025-03-25 13:31:14 +0100

    add: coco_mase.py test script

---
+++ project/coco_mase.py
@@ -0,0 +1,19 @@
+# %%
+from chop.dataset import MaseDataModule, get_dataset_info
+
+dataset_name = "coco-detection"
+# dataset_name = "coco-segmentation"
+model_name = "yolov8"
+batch_size = 5
+
+data_module = MaseDataModule(
+    name=dataset_name,
+    batch_size=batch_size,
+    model_name=model_name,
+    num_workers=0,
+)
+data_module.prepare_data()
+data_module.setup()
+
+
+# %%
