ef066d2741433bb783082207f38e8c47c07a3fa6
Freakness109 <freakness109@gmail.com>
2025-03-27 13:23:45 +0100

    Add support for quantizing ConvTranspose2d

--- project/yolov8_mase_seg.py
+++ project/yolov8_mase_seg.py
@@ -2,6 +2,7 @@ from chop.models.yolo import get_yolo_detection_model, get_yolo_segmentation_mod
 import torch
 from chop import MaseGraph
 import chop.passes as passes
+from chop.tools.logger import set_logging_verbosity
 from chop.dataset import MaseDataModule
 import toml
 import os
@@ -49,8 +50,75 @@ mg, _ = passes.add_common_metadata_analysis_pass(
 )
 mg, _ = passes.add_software_metadata_analysis_pass(mg, None)
 
-mg, _ = passes.graph.runtime_analysis_pass(mg, pass_args={
-    "dataset_input_field": "img",
-    "data_module": data_module,
-    **pass_args
-})
+# %% Quantization
+from chop.passes.graph.utils import deepcopy_mase_graph
+import chop.passes as passes
+
+quantization_config = {
+    "by": "type",
+    "default": {
+        "config": {
+            "name": None,
+        }
+    },
+    "conv2d": {
+        "config": {
+            "name": "integer",
+            # data
+            "data_in_width": 8,
+            "data_in_frac_width": 4,
+            # weight
+            "weight_width": 8,
+            "weight_frac_width": 4,
+            # bias
+            "bias_width": 8,
+            "bias_frac_width": 4,
+        }
+    },
+    "convtranspose2d": {
+        "config": {
+            "name": "integer",
+            # data
+            "data_in_width": 8,
+            "data_in_frac_width": 4,
+            # weight
+            "weight_width": 8,
+            "weight_frac_width": 4,
+            # bias
+            "bias_width": 8,
+            "bias_frac_width": 4,
+        }
+    },
+    "linear": {
+        "config": {
+            "name": "integer",
+            # data
+            "data_in_width": 8,
+            "data_in_frac_width": 4,
+            # weight
+            "weight_width": 8,
+            "weight_frac_width": 4,
+            # bias
+            "bias_width": 8,
+            "bias_frac_width": 4,
+        }
+    },
+}
+
+set_logging_verbosity("debug")
+new_mg, _ = passes.quantize_transform_pass(
+    mg,
+    pass_args=quantization_config,
+)
+
+set_logging_verbosity("warning")
+mg, _ = passes.summarize_quantization_analysis_pass(
+    new_mg,
+    pass_args={"save_dir": "quantize_summary", "original_graph": mg},
+)
+
+# mg, _ = passes.graph.runtime_analysis_pass(mg, pass_args={
+#     "dataset_input_field": "img",
+#     "data_module": data_module,
+#     **pass_args
+# })
