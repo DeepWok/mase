8c93def02eed275808830bb25040cb6af590ea89
Cano Xiao <cx922@ic.ac.uk>
2026-02-10 19:59:48 +0000

    remove pim related stuff

--- src/chop/passes/module/transforms/__init__.py
+++ src/chop/passes/module/transforms/__init__.py
@@ -2,4 +2,3 @@ from .autosharding import resharding_transform_pass
 from .quantize import quantize_module_transform_pass
 from .snn import ann2snn_module_transform_pass
 from .attention import attention_swap_transform_pass
-from .pim import pim_matmul_transform_pass
